7bd46e5f8858032bfcbcb8477ec43d26c18115ee
Bryan Krauss <bryan@button.is>
2022-04-25 13:07:55 -0700

    feat: add new config file for rendering form

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+};
