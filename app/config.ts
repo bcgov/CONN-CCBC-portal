346cb617925b35ce0900277682566e5f0815f26d
marcellmueller <mlmueller@protonmail.com>
2022-04-26 08:08:20 -0700

    feat: add convict env config

---
+++ app/config.ts
@@ -0,0 +1,69 @@
+'use strict';
+
+import convict from 'convict';
+
+const config = convict({
+  NODE_ENV: {
+    doc: 'The application environment.',
+    format: ['production', 'development', 'test'],
+    default: 'development',
+    env: 'NODE_ENV',
+    arg: 'env',
+  },
+  ORIGIN: {
+    doc: 'Application origin',
+    format: String,
+    default: 'localhost',
+    env: 'ORIGIN',
+  },
+  PORT: {
+    doc: 'Application port',
+    format: Number,
+    default: 3000,
+    env: 'PORT',
+  },
+  PGUSER: {
+    doc: 'Postgres user',
+    format: String,
+    default: 'ccbc_app',
+    env: 'PGUSER',
+  },
+  PGPASSWORD: {
+    doc: 'Postgres password',
+    format: String,
+    default: '',
+    env: 'PGPASSWORD',
+  },
+  PGHOST: {
+    doc: 'Postgres host',
+    format: String,
+    default: 'localhost',
+    env: 'PGHOST',
+  },
+  PGPORT: {
+    doc: 'Application port',
+    format: Number,
+    default: 5432,
+    env: 'PGPORT',
+  },
+  PGDATABASE: {
+    doc: 'Postgres database name',
+    format: String,
+    default: 'ccbc',
+    env: 'PGDATABASE',
+  },
+  PGSCHEMA: {
+    doc: 'Postgres database schema',
+    format: String,
+    default: 'ccbc_public',
+    env: 'PGSCHEMA',
+  },
+});
+
+// Load environment dependent configuration
+const env = config.get('NODE_ENV');
+config.loadFile('./' + env + '.json');
+
+config.validate({ allowed: 'warn' });
+
+export default config;
