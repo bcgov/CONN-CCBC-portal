d71603f047c8545133a32100135d981a45a8906c
marcellmueller <mlmueller@protonmail.com>
2022-05-05 13:02:25 -0700

    feat: add application table pgtap test

---
+++ db/test/table_applications_test.sql
@@ -0,0 +1,25 @@
+set client_min_messages to warning;
+create extension if not exists pgtap;
+reset client_min_messages;
+
+begin;
+select plan(22);
+
+-- Table exists
+select has_table(
+  'ccbc_public', 'applications',
+  'ccbc_public.applications should exist, and be a table'
+);
+
+
+-- Columns
+select has_column('ccbc_public', 'applications', 'id','The table applications has column id');
+select has_column('ccbc_public', 'applications', 'reference_number','The table applications has column reference_number');
+select has_column('ccbc_public', 'applications', 'owner','The table applications has column owner');
+select has_column('ccbc_public', 'applications', 'form_data','The table applications has column form_data');
+select has_column('ccbc_public', 'applications', 'status','The table applications has column status');
+
+
+
+select finish();
+rollback;
