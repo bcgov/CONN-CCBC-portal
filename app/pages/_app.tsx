27fc51183d3ac633a6f88c11beb4ba4b3a3ff00d
Bryan Krauss <bryan@button.is>
2022-05-06 09:54:02 -0700

    feat: creates _document.tsx
    - moves Head to _app.tsx
    - removes Head from index.tsx

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,8 +1,61 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Navigation, Footer } from '@button-inc/bcgov-theme';
+import FooterLinks from '../components/FooterLinks';
+import NavbarLinks from '../components/NavBarLinks';
+import styled from 'styled-components';
+import App from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const StyledFooter = styled(Footer)`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`;
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+          key="viewport"
+        />
+        <meta charSet="utf-8" />
+        <title>Connecting Communities BC</title>
+        <link
+          rel="apple-touch-icon"
+          href="/icons/bcid-apple-touch-icon.png"
+          sizes="180x180"
+        />
+        <link
+          rel="icon"
+          href="/icons/bcid-favicon-32x32.png"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          rel="icon"
+          href="/icons/bcid-favicon-16x16.png"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link rel="mask-icon" href="/icons/bcid-apple-icon.svg" color="#036" />
+        <link rel="icon" href="/icons/bcid-favicon-32x32.png" />
+      </Head>
+      <Navigation header="main" title="Connected Communities BC">
+        <NavbarLinks />
+      </Navigation>
+      <Component {...pageProps} />
+      <StyledFooter>
+        <FooterLinks />
+      </StyledFooter>
+    </>
+  );
 }
 
-export default MyApp;
+MyApp.getInitialProps = async (context: any) => {
+  const props = await App.getInitialProps(context);
+  return { ...props };
+};
