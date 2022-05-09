ffc70333c8e470d8b55359bc173c2c2fc71dbe8c
marcellmueller <mlmueller@protonmail.com>
2022-05-09 12:02:27 -0700

    refactor: move globalstyles into separate file

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,23 +1,8 @@
 import type { AppProps } from 'next/app';
-import { createGlobalStyle } from 'styled-components';
+import GlobalStyle from '../styles/GobalStyles';
 import BCGovTypography from '../components/BCGovTypography';
 import { Layout } from '../components';
 import App from 'next/app';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  },
-  a {
-    color: inherit;
-    text-decoration: none;
-  },
-  * {
-    box-sizing: border-box;
-  }  
-`;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ app/styles/GobalStyles.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  },
+  a {
+    color: inherit;
+    text-decoration: none;
+  },
+  * {
+    box-sizing: border-box;
+  }  
+`;
+
+export default GlobalStyle;
