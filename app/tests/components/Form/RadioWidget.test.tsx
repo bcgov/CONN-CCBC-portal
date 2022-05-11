6078c73551da8d7f9150dc2e0d75916d9088a21e
Bryan Krauss <bryan@button.is>
2022-05-11 11:45:37 -0700

    test: 3 new widget tests to be iterated on

---
+++ app/tests/components/Form/RadioWidget.test.tsx
@@ -0,0 +1,10 @@
+import RadioWidget from '../../../lib/theme/widgets/RadioWidget';
+
+describe('RadioWidget', () => {
+  xit('simple with errors');
+  xit('simple without errors');
+  xit('simple with required');
+  xit('simple without required');
+  xit('render widgets for all items in enumOptions array');
+  xit('return the correct value when selected');
+});
---
+++ app/tests/components/Form/TextAreaWidget.test.tsx
@@ -0,0 +1,10 @@
+import TextAreaWidget from '../../../lib/theme/widgets/TextAreaWidget';
+
+describe('TextareaWidget', () => {
+  xit('simple with errors');
+  xit('simple without errors');
+  xit('simple with required');
+  xit('simple without required');
+  xit('returns the value in the input');
+  xit('returns text as formatted without stripping of special chars');
+});
