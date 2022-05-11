baf1d3715829d1d7e3c7bae1cc5f71b48ddf1a3d
Bryan Krauss <bryan@button.is>
2022-05-11 10:11:40 -0700

    feat: RadioWidget working-ish

--- app/lib/theme/widgets/RadioWidget.tsx
+++ app/lib/theme/widgets/RadioWidget.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { WidgetProps } from '@rjsf/core';
 import RadioButton from '@button-inc/bcgov-theme/RadioButton';
 
@@ -7,16 +8,34 @@ const RadioWidget: React.FC<WidgetProps> = ({
   label,
   value,
   required,
+  options,
 }) => {
+  const { enumOptions = [] } = options;
+  console.log(enumOptions);
+  const formProps = {
+    onChange: (e) => {
+      let value = e.target['value'];
+      if (value === '') value = undefined;
+      if (value === 'true') value = true;
+      if (value === 'false') value = false;
+      onChange(value);
+    },
+  };
   return (
     <div>
-      <RadioButton
-        id={id}
-        onChange={(e: { target: { value: boolean } }) => {
-          onChange(e.target.value);
-        }}
-        required={required}
-      />
+      {enumOptions &&
+        enumOptions.map((option) => {
+          return (
+            <RadioButton
+              key={option.value}
+              {...formProps}
+              label={option.label}
+              value={option.value}
+              checked={option.value === value}
+              required={required}
+            />
+          );
+        })}
     </div>
   );
 };
