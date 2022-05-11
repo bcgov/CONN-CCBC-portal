b4df9daa1d37fe6d8ddc07b531586a71bfc8f0d9
marcellmueller <mlmueller@protonmail.com>
2022-05-11 11:48:08 -0700

    feat: add more targets to happo config

--- app/.happo.js
+++ app/.happo.js
@@ -1,12 +1,23 @@
 const { RemoteBrowserTarget } = require('happo.io');
 const config = require('./config.js');
 
+const VIEWPORT = '1366x768';
+
 module.exports = {
   apiKey: config.get('HAPPO_API_KEY'),
   apiSecret: config.get('HAPPO_API_SECRET'),
   targets: {
     chrome: new RemoteBrowserTarget('chrome', {
-      viewport: '1024x768',
+      viewport: VIEWPORT,
+    }),
+    edge: new RemoteBrowserTarget('edge', {
+      viewport: VIEWPORT,
+    }),
+    firefox: new RemoteBrowserTarget('firefox', {
+      viewport: VIEWPORT,
+    }),
+    safari: new RemoteBrowserTarget('safari', {
+      viewport: VIEWPORT,
     }),
   },
 };
