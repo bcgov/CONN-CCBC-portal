942cbb71d6daaac13d83553982b7cdbf175686ed
Bryan Krauss <bryan@button.is>
2022-05-12 10:01:08 -0700

    test: fix 'setup' widget tests

--- app/tests/components/Form/TextWidget.test.tsx
+++ app/tests/components/Form/TextWidget.test.tsx
@@ -1,9 +1,9 @@
 import TextWidget from '../../../lib/theme/widgets/TextWidget';
 
-describe('TextWidget', () => {
-  xit('simple without errors');
-  xit('simple with errors');
-  xit('simple with required');
-  xit('simple without required');
-  xit('returns the value in the input');
+describe.skip('TextWidget', () => {
+  xit('simple without errors', () => {});
+  xit('simple with errors', () => {});
+  xit('simple with required', () => {});
+  xit('simple without required', () => {});
+  xit('returns the value in the input', () => {});
 });
