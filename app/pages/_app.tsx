56964b632c12f95b69ae2f647eaa78c3a3c88bf6
marcellmueller <mlmueller@protonmail.com>
2022-05-16 16:02:04 -0700

    feat: app relay environment provider

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import type { AppProps } from 'next/app';
 
 import GlobalStyle from '../styles/GobalStyles';
@@ -5,16 +7,22 @@ import GlobalTheme from '../styles/GlobalTheme';
 import BCGovTypography from '../components/BCGovTypography';
 import { Layout } from '../components';
 import App from 'next/app';
+import { RelayEnvironmentProvider } from 'react-relay/hooks';
+import { relayEnvironment } from '../lib/relay';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <GlobalTheme>
-      <GlobalStyle />
-      <BCGovTypography />
-      <Layout title="Connecting Communities BC">
-        <Component {...pageProps} />
-      </Layout>
-    </GlobalTheme>
+    <RelayEnvironmentProvider environment={relayEnvironment}>
+      <Suspense fallback={'Loading...'}>
+        <GlobalTheme>
+          <GlobalStyle />
+          <BCGovTypography />
+          <Layout title="Connecting Communities BC">
+            <Component {...pageProps} />
+          </Layout>
+        </GlobalTheme>
+      </Suspense>
+    </RelayEnvironmentProvider>
   );
 }
 
