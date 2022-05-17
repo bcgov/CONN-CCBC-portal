31275e1112f8b7eb4fcf3a3b1fa016c0d3858f55
Bryan Krauss <bryan@button.is>
2022-05-17 13:04:13 -0700

    feat: add grant permissions to applications table

--- db/deploy/tables/applications.sql
+++ db/deploy/tables/applications.sql
@@ -12,4 +12,22 @@ create table if not exists ccbc_public.applications (
   unique(owner)
 );
 
+create index ccbc_owner on ccbc_public.applications(owner);
+
+do
+$grant$
+begin
+
+-- Grant ccbc_auth_user permissions
+perform ccbc_public.grant_permissions('select', 'applications', 'ccbc_auth_user');
+perform ccbc_public.grant_permissions('insert', 'applications', 'ccbc_auth_user');
+perform ccbc_public.grant_permissions('update', 'applications', 'ccbc_auth_user',
+  ARRAY['id', 'owner', 'form_data', 'status']);
+
+-- Grant  ccbc_guest persmissions
+perform ccbc_public.grant_permissions('select', 'applications', 'ccbc_guest');
+
+end
+$grant$;
+
 commit;
