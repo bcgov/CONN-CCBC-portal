d5cf9a04c6628bbbf0f93d561bd0eaf8f6dd1337
mikevespi <mike@button.is>
2022-05-20 16:30:48 -0700

    feat: handle saving logic in form pages

--- app/components/Form/ApplicationForm.tsx
+++ app/components/Form/ApplicationForm.tsx
@@ -1,23 +1,54 @@
 import FormBase from './FormBase';
 import uiSchema from '../../formSchema/uiSchema';
 import schema from '../../formSchema/schema';
+import { updateApplicationMutation } from '../../schema/mutations';
 import StyledGovButton from '../../components/StyledGovButton';
 import type { JSONSchema7 } from 'json-schema';
 import { schemaToSubschemasArray } from '../../utils/schemaUtils';
+import { useRouter } from 'next/router';
 
 interface Props {
-  onSubmit: any;
   formData: any;
   pageNumber: number;
 }
-const schemaArray = schemaToSubschemasArray(schema as JSONSchema7)
 
-const ApplicationForm: React.FC<Props> = (props) => {
-  const schemaArray = schemaToSubschemasArray(schema as JSONSchema7)
-  // pages are indexed from 1
-  const subSchema = schemaArray[props.pageNumber - 1][1]
+const ApplicationForm: React.FC<Props> = ({ formData, pageNumber }) => {
+  const router = useRouter();
+
+  const subschemaArray = schemaToSubschemasArray(schema as JSONSchema7)
+  const [sectionName, sectionSchema] = subschemaArray[pageNumber - 1]
+
+
+  const saveForm = async (incomingFormData: any, existingFormData: any) => {
+    const pageNumber = parseInt(router.query.page as string)
+    const sectionName = subschemaArray[pageNumber - 1][0]
+    let newFormData: any = {};
+
+    if (Object.keys(existingFormData).length === 0) {
+      newFormData['contactInformation'] = incomingFormData.formData
+    }
+    else if (existingFormData[sectionName]) {
+      newFormData = { ...existingFormData }
+      newFormData[sectionName] = { ...existingFormData[sectionName], ...incomingFormData.formData }
+    }
+    else {
+      newFormData = { ...existingFormData }
+      newFormData[sectionName] = { ...incomingFormData.formData }
+    }
+    await updateApplicationMutation({
+      owner: '74d2515660e6444ca177a96e67ecfc5f',
+      formData: newFormData,
+      // change status? Consider having an "editing" status or something, and switching to complete
+      // when the form actually getts finished.
+      status: 'complete',
+    }).then(() => {
+      if (pageNumber < subschemaArray.length) router.push(`/form/${pageNumber + 1}`);
+      else router.push('/form/success')
+    })
+  }
+
   return (
-    <FormBase {...props} schema={subSchema as JSONSchema7} uiSchema={uiSchema}>
+    <FormBase formData={formData[sectionName]} onSubmit={(incomingFormData: any) => saveForm(incomingFormData, formData)} schema={sectionSchema as JSONSchema7} uiSchema={uiSchema}>
       <StyledGovButton variant="primary" type="submit">
         Continue
       </StyledGovButton>
--- app/utils/schemaUtils.ts
+++ app/utils/schemaUtils.ts
@@ -2,8 +2,8 @@ import type { JSONSchema7 } from 'json-schema';
 /**
  * @param schema A valid JSON schema
  * @returns an array of the the schema properties with the following structure:
- * [ [key, value] ]
+ * [ [<section title>, <section schema>] ]
  */
-export const schemaToSubschemasArray = (schema: JSONSchema7) => {
+export const schemaToSubschemasArray = (schema: JSONSchema7): [string, unknown][] => {
   return Object.entries(schema.properties as any)
 }
