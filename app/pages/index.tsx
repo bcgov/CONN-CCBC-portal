3196322d6b996d891bc48951ecc3d2af5800ecd0
Bryan Krauss <bryan@button.is>
2022-06-01 10:16:34 -0700

    refactor: move start app button to dashboard

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,27 +1,7 @@
 import type { NextPage } from 'next';
-import StyledGovButton from '../components/StyledGovButton';
-import { useCreateApplicationMutation } from '../schema/mutations/application/createApplication';
-import { useRouter } from 'next/router';
+import { LoginForm } from '../components';
 
 const Home: NextPage = () => {
-  const router = useRouter();
-
-  const [createApplication] = useCreateApplicationMutation();
-
-  const handleCreateApplication = () => {
-    createApplication({
-      variables: {
-        input: { application: { owner: '74d2515660e6444ca177a96e67ecfc5f' } },
-      },
-      onCompleted: () => {
-        router.push('/form/1');
-      },
-      onError: () => {
-        // This needs to be removed once application dashboard implemented
-        router.push('/form/1');
-      },
-    });
-  };
   return (
     <div>
       <h1>Welcome</h1>
@@ -38,7 +18,7 @@ const Home: NextPage = () => {
         To begin the application, please log in with BCeID Business. If you do
         not have BCeID Business, please use your BCeID Basic.
       </p>
-      <StyledGovButton onClick={handleCreateApplication}>Login</StyledGovButton>
+      <LoginForm />
     </div>
   );
 };
