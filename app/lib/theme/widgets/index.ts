d0bae8abdb168a1ba2ff1f6b06a0d1d8aaa776b2
Bryan Krauss <bryan@button.is>
2022-06-09 10:18:07 -0700

    feat: adds new SelectWidget to custom theme

--- app/lib/theme/widgets/index.ts
+++ app/lib/theme/widgets/index.ts
@@ -4,3 +4,4 @@ export { default as FieldLabel } from './FieldLabel';
 export { default as RadioWidget } from './RadioWidget';
 export { default as TextAreaWidget } from './TextAreaWidget';
 export { default as TextWidget } from './TextWidget';
+export { default as SelectWidget } from './SelectWidget';
