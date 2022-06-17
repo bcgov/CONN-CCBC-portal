7d1b36e60a28687a21a0b0ef1683af07ac16d9a6
marcellmueller <mlmueller@protonmail.com>
2022-06-17 10:49:52 -0700

    feat: add calendar icon to datepicker

--- app/lib/theme/widgets/DatePickerWidget.tsx
+++ app/lib/theme/widgets/DatePickerWidget.tsx
@@ -11,7 +11,12 @@ const StyledDatePicker = styled(DatePicker)`
   border: 2px solid #606060;
   border-radius: 0.25rem;
   padding: 0.5rem 0.6rem;
+  width: 100%;
+`;
+
+const StyledDiv = styled('div')`
   width: ${(props) => props.theme.width.inputWidthSmall};
+  position: relative;
 `;
 
 function getDateString(date: SetStateAction<Date | undefined>) {
@@ -44,6 +49,15 @@ const DatePickerWidget: React.FunctionComponent<WidgetProps> = ({
     onFocus(id, getDateString(day));
   };
 
+  const CustomInput = ({ value, onClick, ...rest }: any) => {
+    return (
+      <StyledDiv>
+        <StyledDatePicker value={value} onClick={onClick} {...rest} />
+        <CalendarIcon onClick={onClick} />
+      </StyledDiv>
+    );
+  };
+
   return (
     <StyledDatePicker
       id={id}
@@ -59,8 +73,72 @@ const DatePickerWidget: React.FunctionComponent<WidgetProps> = ({
       showMonthDropdown
       showYearDropdown
       dropdownMode="select"
+      customInput={<CustomInput />}
     />
   );
 };
 
 export default DatePickerWidget;
+
+const CalendarIcon = ({ onClick }: any) => {
+  const StyledSpan = styled('span')`
+    position: absolute;
+    top: 21px;
+    right: 10px;
+    cursor: pointer;
+  `;
+  return (
+    <StyledSpan onClick={onClick}>
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="2.75"
+          y="5.75"
+          width="18.5"
+          height="16.5"
+          rx="1.25"
+          stroke="black"
+          strokeWidth="1.5"
+        />
+        <path
+          d="M4 5.5H20C20.8284 5.5 21.5 6.17157 21.5 7V9.5H2.5V7C2.5 6.17157 3.17157 5.5 4 5.5Z"
+          fill="black"
+          stroke="black"
+        />
+        <mask id="path-3-inside-1_2485_9855" fill="white">
+          <rect x="6" y="1" width="4" height="6" rx="1" />
+        </mask>
+        <rect
+          x="6"
+          y="1"
+          width="4"
+          height="6"
+          rx="1"
+          fill="white"
+          stroke="black"
+          strokeWidth="3"
+          mask="url(#path-3-inside-1_2485_9855)"
+        />
+        <mask id="path-4-inside-2_2485_9855" fill="white">
+          <rect x="14" y="1" width="4" height="6" rx="1" />
+        </mask>
+        <rect
+          x="14"
+          y="1"
+          width="4"
+          height="6"
+          rx="1"
+          fill="white"
+          stroke="black"
+          strokeWidth="3"
+          mask="url(#path-4-inside-2_2485_9855)"
+        />
+      </svg>
+    </StyledSpan>
+  );
+};
