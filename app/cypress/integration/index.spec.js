8ada2484ae5b4753f3f6b4074f373abafaaba2c3
marcellmueller <mlmueller@protonmail.com>
2022-06-21 16:08:14 -0700

    feat: add remaining current form pages to e2e test

--- app/cypress/integration/index.spec.js
+++ app/cypress/integration/index.spec.js
@@ -265,9 +265,173 @@ context('Homepage', () => {
 
     cy.get('input[id="root_POBox"]').type('test');
 
-    cy.get('input[id="root_city"]');
+    cy.get('input[id="root_city"]').type('test');
+
+    cy.get('select[id="root_province"]').select('British Columbia');
 
     cy.get('input[id="root_postalCode"]').type('test');
+
+    cy.get('input[id="root_isMailingAddress-1"]').parent().click();
+
+    cy.get('input[id="root_mailingAddress_unitNumberMailing"]').type('test');
+
+    cy.get('input[id="root_mailingAddress_streetNumberMailing"]').type('test');
+
+    cy.get('input[id="root_mailingAddress_streetNameMailing"]').type('test');
+
+    cy.get('input[id="root_mailingAddress_POBoxMailing"]').type('test');
+
+    cy.get('input[id="root_mailingAddress_cityMailing"]').type('test');
+
+    cy.get('select[id="root_mailingAddress_provinceMailing"]').select(
+      'British Columbia'
+    );
+
+    cy.get('input[id="root_mailingAddress_postalCodeMailing"]').type('test');
+
+    cy.get('button').contains('Continue').should('be.visible').click();
+
+    cy.wait(2000);
+
+    // Organization contact information page
+
+    cy.get('h1')
+      .contains('Organization contact information')
+      .should('be.visible');
+
+    cy.get('input[id="root_contactTelephoneNumber"]').type('123-4567');
+
+    cy.get('input[id="root_contactExtension"]').type('123');
+
+    cy.get('input[id="root_contactEmail"]').type('test@test.com');
+
+    cy.get('input[id="root_contactWebsite"]').type('test');
+
+    cy.get('button').contains('Continue').should('be.visible').click();
+
+    cy.wait(2000);
+
+    // Authorized contact page
+
+    cy.get('h1').contains('Authorized contact').should('be.visible');
+
+    cy.get('input[id="root_authFamilyName"]').type('test');
+
+    cy.get('input[id="root_authGivenName"]').type('test');
+
+    cy.get('input[id="root_authPostionTitle"]').type('test');
+
+    cy.get('input[id="root_authEmail"]').type('test@test.com');
+
+    cy.get('input[id="root_authTelephone"]').type('123-456');
+
+    cy.get('input[id="root_authExtension"]').type('123');
+
+    cy.get('input[id="root_isAuthContactSigningOfficer-0"]').parent().click();
+
+    cy.get('input[id="root_isFirstContact"]').parent().click();
+
+    cy.get('button').contains('Continue').should('be.visible').click();
+
+    cy.wait(2000);
+
+    // Alternate contact page
+
+    cy.get('h1').contains('Alternate contact').should('be.visible');
+
+    cy.get('input[id="root_altFamilyName"]').type('test');
+
+    cy.get('input[id="root_altGivenName"]').type('test');
+
+    cy.get('input[id="root_altPostionTitle"]').type('test');
+
+    cy.get('input[id="root_altEmail"]').type('test@test.com');
+
+    cy.get('input[id="root_altTelephone"]').type('123-456');
+
+    cy.get('input[id="root_altExtension"]').type('123');
+
+    cy.get('input[id="root_isAltContactSigningOfficer-0"]').parent().click();
+
+    cy.get('input[id="root_isAltFirstContact"]').parent().click();
+
+    cy.get('button').contains('Continue').should('be.visible').click();
+
+    cy.wait(2000);
+
+    // Declarations page
+
+    cy.get('h1').contains('Declarations').should('be.visible');
+
+    cy.get('input[id="root_declarationsList-0"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-1"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-2"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-3"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-4"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-5"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-6"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-7"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-8"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-9"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-10"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-11"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-12"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-13"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-14"]').parent().click();
+
+    cy.get('input[id="root_declarationsList-15"]').parent().click();
+
+    cy.get('button').contains('Continue').should('be.visible').click();
+
+    cy.wait(2000);
+
+    // Sign Declarations
+
+    cy.get('h1').contains('Declarations').should('be.visible');
+
+    cy.get('input[id="root_declarationsCompletedFor"]').type('test');
+
+    // Todo: fix typing
+    cy.get('[id="root_declarationsDate"]');
+
+    cy.get('input[id="root_declarationsCompletedBy"]').type('test');
+
+    cy.get('input[id="root_declarationsTitle"]').type('test');
+
+    cy.get('button').contains('Complete form').should('be.visible').click();
+
+    cy.wait(2000);
+
+    // Success page
+
+    cy.get('h2').contains('Application complete').should('be.visible');
+
+    cy.get('h3')
+      .contains('Thank you for applying to CCBC Intake 1')
+      .should('be.visible');
+
+    cy.get('body').happoScreenshot();
+
+    cy.get('button')
+      .contains('Return to dashboard')
+      .should('be.visible')
+      .click();
+
+    cy.wait(4000);
 
     // cy.get('body').happoScreenshot();
   });
