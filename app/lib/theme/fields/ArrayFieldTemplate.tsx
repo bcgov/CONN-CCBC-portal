31cabca4e5093271093f93174aabd5d90b34ba26
marcellmueller <mlmueller@protonmail.com>
2022-06-22 16:20:20 -0700

    feat: add custom array field template

---
+++ app/lib/theme/fields/ArrayFieldTemplate.tsx
@@ -0,0 +1,45 @@
+import Button from '@button-inc/bcgov-theme/Button';
+
+import styled from 'styled-components';
+const StyledDiv = styled('div')`
+  display: flex;
+  justify-content: flex-end;
+  min-width: 100%;
+`;
+
+const StyledButton = styled('button')`
+  border: none;
+  background: none;
+  cursor: pointer;
+  text-decoration: underline;
+
+  color: ${(props) => props.theme.color.links};
+`;
+
+const ArrayFieldTemplate = (props: any) => {
+  return (
+    <div>
+      {props.items.map((element: any, i) => {
+        return (
+          <div key={i}>
+            {i != 0 && (
+              <StyledDiv>
+                <StyledButton onClick={element.onDropIndexClick(element.index)}>
+                  Remove
+                </StyledButton>
+              </StyledDiv>
+            )}
+
+            {element.children}
+            <hr />
+          </div>
+        );
+      })}
+      {props.canAdd && (
+        <Button onClick={props.onAddClick}>Add another funding source</Button>
+      )}
+    </div>
+  );
+};
+
+export default ArrayFieldTemplate;
--- app/lib/theme/fields/index.ts
+++ app/lib/theme/fields/index.ts
@@ -1,1 +1,2 @@
+export { default as ArrayFieldTemplate } from './ArrayFieldTemplate';
 export { default as DescriptionField } from './DescriptionField';
