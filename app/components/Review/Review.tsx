9e34af80b112dd191745113d1c49ca291891a7f7
marcellmueller <mlmueller@protonmail.com>
2022-06-27 07:53:12 -0700

    feat: add review page confirmation checkbox

--- app/components/Review/Review.tsx
+++ app/components/Review/Review.tsx
@@ -1,6 +1,7 @@
 import Accordion from '@button-inc/bcgov-theme/Accordion';
 import styled from 'styled-components';
 import schema from '../../formSchema/schema';
+import Checkbox from '@button-inc/bcgov-theme/Checkbox';
 
 import {
   OtherFundingSourcesTable,
@@ -20,15 +21,36 @@ const StyledAccordion = styled(Accordion)`
     align-items: center;
     font-size: 24px;
   }
+
   svg {
     width: 20px;
     height: 20px;
     vertical-align: 0;
   }
+
   header {
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+`;
+
+const StyledCheckboxLabel = styled('label')`
+  padding-left: 1em;
+`;
+
+const StyledCheckboxDiv = styled('div')`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 16px 0;
+
+  & span {
+    border-radius: 2px;
+  }
+
+  & label {
+    padding-left: 1em;
   }
 `;
 
@@ -98,6 +120,13 @@ const Review = ({ formData }: Props) => {
           </StyledAccordion>
         );
       })}
+      <StyledCheckboxDiv>
+        <Checkbox id="review-confirmation-checkbox" />
+        <StyledCheckboxLabel htmlFor="review-confirmation-checkbox">
+          The Applicant acknowledges that there are unanswered fields and
+          incomplete applications may not be assessed.
+        </StyledCheckboxLabel>
+      </StyledCheckboxDiv>
     </div>
   );
 };
