1169318ec8bb871232aa010e08f98466d36d40fa
VB <vladimir@acclaim.consulting>
2022-07-16 16:49:10 -0700

    removed vpc endpoint

--- Infrastructure/TF_CCBC/modules/s3_bucket/main.tf
+++ Infrastructure/TF_CCBC/modules/s3_bucket/main.tf
@@ -52,29 +52,3 @@ resource "aws_s3_bucket_public_access_block" "this" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-resource "aws_vpc_endpoint" "endpoint_gateway_to_s3" {
-  service_name    = "com.amazonaws.${data.aws_region.current.name}.s3" 
-  vpc_id          = var.vpc_id
-
-  # https://docs.aws.amazon.com/vpc/latest/userguide/vpce-gateway.html
-  policy = <<POLICY
-  {
-    "Statement": [
-      {
-        "Sid": "Access-to-bucket-via-privatelink",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": "s3:*",
-        "Resource": ["${local.s3_buckets}"]
-      }
-    ],
-    "Version": "2008-10-17"
-  }
-POLICY
-
-  tags = {
-    Name      = "ccbc-endpoint-gateway-to-s3"
-    Terraform = true
-  }
-}
