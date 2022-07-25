a4c2f73bf58a391c2fc3f5c2b77ea8a25245c5aa
marcellmueller <mlmueller@protonmail.com>
2022-07-25 09:07:06 -0700

    feat: add is_deleted column to table attachment

--- db/deploy/tables/attachment.sql
+++ db/deploy/tables/attachment.sql
@@ -13,7 +13,8 @@ create table ccbc_public.attachment
   file_type varchar(100),
   file_size varchar(100),
   application_id integer not null references ccbc_public.applications(id),
-  application_status_id integer references ccbc_public.application_status(id)
+  application_status_id integer references ccbc_public.application_status(id),
+  is_deleted boolean not null default false
 );
 
 select ccbc_private.upsert_timestamp_columns('ccbc_public', 'attachment');
@@ -43,5 +44,6 @@ comment on column ccbc_public.attachment.file_type is 'Original uploaded file ty
 comment on column ccbc_public.attachment.file_size is 'Original uploaded file size';
 comment on column ccbc_public.attachment.application_id is 'The id of the project (ccbc_public.application.id) that the attachment was uploaded to';
 comment on column ccbc_public.attachment.application_status_id is 'The id of the application_status (ccbc_public.application_status.id) that the attachment references';
+comment on column ccbc_public.attachment.is_deleted is 'Boolean that is set to true if the file is to be soft deleted';
 
 commit;
