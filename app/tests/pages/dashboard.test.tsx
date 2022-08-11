0fba8a2c6253e77e3cc6129ee2f95d84686fa5e1
marcellmueller <mlmueller@protonmail.com>
2022-08-11 08:24:10 -0700

    chore: update dashboard page test with relay page testing helper

--- app/tests/pages/dashboard.test.tsx
+++ app/tests/pages/dashboard.test.tsx
@@ -1,4 +1,56 @@
 import { withRelayOptions } from '../../pages/dashboard';
+import { screen } from '@testing-library/react';
+import Dashboard from '../../pages/dashboard';
+import PageTestingHelper from '../helpers/pageTestingHelper';
+import compileddashboardQuery, {
+  dashboardQuery,
+} from '../../__generated__/dashboardQuery.graphql';
+
+const mockQueryPayload = {
+  Query() {
+    return {
+      allApplications: {
+        nodes: [
+          {
+            id: 'WyJhcHBsaWNhdGlvbnMiLDJd',
+            rowId: 2,
+            owner: '4e0ac88c-bf05-49ac-948f-7fd53c7a9fd6',
+            referenceNumber: 1,
+            status: 'withdrawn',
+            projectName: null,
+            ccbcId: 'CCBC-010001',
+            lastEditedPage: '',
+            intakeByIntakeId: {
+              ccbcIntakeNumber: 1,
+              closeTimestamp: '2022-09-09T13:49:23.513427-07:00',
+              openTimestamp: '2022-07-25T00:00:00-07:00',
+            },
+          },
+        ],
+      },
+      session: {
+        sub: '4e0ac88c-bf05-49ac-948f-7fd53c7a9fd6',
+      },
+    };
+  },
+};
+
+const pageTestingHelper = new PageTestingHelper<dashboardQuery>({
+  pageComponent: Dashboard,
+  compiledQuery: compileddashboardQuery,
+  defaultQueryResolver: mockQueryPayload,
+  defaultQueryVariables: {
+    formOwner: { owner: '4e0ac88c-bf05-49ac-948f-7fd53c7a9fd6' },
+  },
+});
+
+it('displays the correct nav links when user is logged in', () => {
+  pageTestingHelper.loadQuery();
+  pageTestingHelper.renderPage();
+
+  expect(screen.getByText('Logout')).toBeInTheDocument();
+  expect(screen.getByTestId('dashboard-btn-test')).toBeInTheDocument();
+});
 
 describe('The index page', () => {
   it('should redirect an unauthorized user', async () => {
