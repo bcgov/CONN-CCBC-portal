230260318f636d58417d0a4c6f469b2dc02bb800
Matthieu Foucault <matthieu@button.is>
2022-08-23 16:30:12 -0700

    chore: rename test files following team agreement

--- db/test/unit/tables/applications_test.sql
+++ db/test/unit/tables/applications_test.sql
@@ -1,7 +1,24 @@
-begin;
-select plan(4);
+set client_min_messages to warning;
+create extension if not exists pgtap;
+reset client_min_messages;
 
-select has_table('ccbc_public', 'ccbc_user', 'table ccbc_public.ccbc_user exists');
+begin;
+SELECT * FROM no_plan();
+
+-- Table exists
+select has_table(
+  'ccbc_public', 'applications',
+  'ccbc_public.applications should exist and be a table'
+);
+
+
+-- Columns
+select has_column('ccbc_public', 'applications', 'id','The table applications has column id');
+select has_column('ccbc_public', 'applications', 'ccbc_number','The table applications has column ccbc_number');
+select has_column('ccbc_public', 'applications', 'owner','The table applications has column owner');
+select has_column('ccbc_public', 'applications', 'form_data','The table applications has column form_data');
+select has_column('ccbc_public', 'applications', 'status','The table applications has column status');
+select has_column('ccbc_public', 'applications', 'last_edited_page','The table applications has column last_edited_page');
 
 insert into ccbc_public.ccbc_user
   (given_name, family_name, email_address, session_sub) values
@@ -57,6 +74,7 @@ select is_empty(
   $$,
     'user3 cannot see applications created by other users'
 );
+
 
 select finish();
 rollback;
