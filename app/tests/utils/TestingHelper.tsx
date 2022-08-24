f2528385652bb7d1108a8d2805b83b27aeb1f370
Matthieu Foucault <matthieu@button.is>
2022-08-24 16:39:03 -0700

    test: stop skipping ApplicationForm tests and improve mutation error messages

--- app/tests/utils/TestingHelper.tsx
+++ app/tests/utils/TestingHelper.tsx
@@ -18,21 +18,52 @@ class TestingHelper {
   }
 
   public expectMutationToBeCalled(mutationName: string, variables?: any) {
-    expect(this.environment.mock.getAllOperations()).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          fragment: expect.objectContaining({
-            node: expect.objectContaining({
-              type: 'Mutation',
-              name: mutationName,
+    try {
+      expect(this.environment.mock.getAllOperations()).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            fragment: expect.objectContaining({
+              node: expect.objectContaining({
+                type: 'Mutation',
+                name: mutationName,
+              }),
+            }),
+            request: expect.objectContaining({
+              variables,
             }),
           }),
-          request: expect.objectContaining({
-            variables,
-          }),
-        }),
-      ])
-    );
+        ])
+      );
+    } catch (e) {
+      const allOperations = this.environment.mock.getAllOperations();
+      const mutationsWithExpectedName = allOperations.filter(
+        ({ fragment: { node } }) => {
+          return node.type === 'Mutation' && node.name === mutationName;
+        }
+      );
+
+      if (mutationsWithExpectedName.length === 0) {
+        const allMutationNames = allOperations.map(
+          ({ fragment: { node } }) => node.name
+        );
+        e.message = `Expected to find a mutation with name "${mutationName}" but found the following mutation names: ${allMutationNames.join(
+          ', '
+        )}`;
+        throw e;
+      }
+
+      const actualMutationVariables = mutationsWithExpectedName
+        .map(({ request: { variables } }) => JSON.stringify(variables, null, 2))
+        .join('\n\n');
+
+      e.message =
+        `Expected variables for mutation "${mutationName}":\n` +
+        JSON.stringify(variables, null, 2) +
+        `\nActual variables for mutation "${mutationName}":\n` +
+        actualMutationVariables;
+
+      throw e;
+    }
   }
 }
 
