19a5051c8e4ffd8e15c8a71d3dec8f8f65dd59dc
Anthony Bushara <anthony@button.is>
2022-08-31 11:47:16 -0400

    feat: fix submit tests and withdraw tests

---
+++ db/test/unit/mutations/withdraw_application_test.sql
@@ -0,0 +1,59 @@
+begin;
+
+select plan(5);
+
+select has_function(
+  'ccbc_public', 'withdraw_application', ARRAY['int'],
+  'Function withdraw_application should exist'
+);
+
+set jwt.claims.sub to '00000000-0000-0000-0000-000000000000';
+insert into ccbc_public.application(id, form_data, owner) overriding system value
+values
+  (2, '{}', '00000000-0000-0000-0000-000000000000'),
+  (3, '{}', '00000000-0000-0000-0000-000000000000'),
+  (4, '{}', '00000000-0000-0000-0000-000000000000');
+
+
+insert into ccbc_public.application_status(application_id, status) 
+VALUES
+ (2, 'submitted'),
+ (3, 'withdrawn');
+
+select lives_ok(
+  $$
+    select ccbc_public.withdraw_application(3)
+  $$,
+  'Handles an already withdrawn application'
+);
+
+select is_empty(
+  $$
+    select * from ccbc_public.application_status where application_id=2 and status='withdrawn'
+  $$,
+  'There are no withdrawn application statuses'
+);
+
+select results_eq(
+  $$
+    select id, ccbc_number, intake_id from ccbc_public.withdraw_application(2)
+  $$,
+  $$
+    values (2, null::varchar, null::int)
+  $$,
+  'Returns the application without removing the intake number'
+);
+
+select results_eq(
+  $$
+    select application_id, status from ccbc_public.application_status where application_id = 2 and status='withdrawn';
+  $$,
+  $$
+    values (2, 'withdrawn'::varchar)
+  $$,
+  'Withdrawn status should be inserted to the application_status table'
+);
+
+select finish();
+
+rollback;
