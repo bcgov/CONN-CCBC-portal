247f9bead99ebda7489538ec22d5a7d38520288a
Anthony Bushara <anthony@button.is>
2022-08-31 16:12:05 -0400

    fix: rename create application test

--- db/test/unit/mutations/create_application_test.sql
+++ db/test/unit/mutations/create_application_test.sql
@@ -1,14 +1,16 @@
 begin;
 
-select * from no_plan();
+select plan(2);
 set jwt.claims.sub to '11111111-1111-1111-1111-111111111112';
+
+set role ccbc_auth_user;
 
 select results_eq(
   $$
     select id, owner, form_data, intake_id, ccbc_number from ccbc_public.create_application();
   $$,
   $$
-    values (1,'11111111-1111-1111-1111-111111111112', '{}'::jsonb, null::int, null:varchar)
+    values (1,'11111111-1111-1111-1111-111111111112'::varchar, '{}'::jsonb, null::int, null::varchar)
   $$,
   'Should return newly created application'
 );
@@ -21,7 +23,7 @@ select results_eq(
     values (1, 'draft'::varchar)
   $$,
   'Should create draft status'
-)
+);
 
 select finish();
 rollback;
