074e096ed0a189ecd12d8c769171fd07eae02b46
Vladimir <vladimir@button.is>
2022-09-02 08:39:36 -0700

    fix: config
    Signed-off-by: Vladimir <vladimir@button.is>

--- app/config.js
+++ app/config.js
@@ -169,7 +169,7 @@ catch(e){
   console.log(env);
 }
 config.validate({ allowed: 'warn' });
-if (config.get("namespace").endsWith("-prod") && config.get("ENABLE_MOCK_TIME")) {
+if (config.get("OPENSHIFT_APP_NAMESPACE").endsWith("-prod") && config.get("ENABLE_MOCK_TIME")) {
   throw new Error("ENABLE_MOCK_TIME cannot be true with a -prod namespace.");
 }
 module.exports = config;
