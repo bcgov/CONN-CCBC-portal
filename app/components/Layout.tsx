9fd951809f0894ffd6e18cd73ede1ce7fa4367cb
marcellmueller <mlmueller@protonmail.com>
2022-09-07 08:51:05 -0700

    fix: add flex to layout

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,20 +1,27 @@
 import Head from 'next/head';
 import Footer from '@button-inc/bcgov-theme/Footer';
 import { FooterLinks, Navigation, TimeTravel } from '.';
-import getConfig from "next/config";
+import getConfig from 'next/config';
 
 import styled from 'styled-components';
 
 const runtimeConfig = getConfig()?.publicRuntimeConfig ?? {};
 
 const StyledFooter = styled(Footer)`
-  bottom: 0;
   width: 100%;
+`;
+
+const StyledLayout = styled('div')`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
 `;
 
 const StyledMain = styled('main')`
   margin: 0 auto 50px;
   max-width: ${(props) => props.theme.width.pageMaxWidth};
+  flex: 1;
   padding: 2em 1.5em;
 
   @media (min-width: 768px) {
@@ -37,12 +44,10 @@ type Props = {
 const Layout: React.FC<Props> = ({ children, session, title }) => {
   const enableTimeMachine = runtimeConfig.ENABLE_MOCK_TIME;
   const environment = runtimeConfig.OPENSHIFT_APP_NAMESPACE;
-  const isTestEnv = environment 
-    ? !environment.endsWith('-prod')
-    : false;
+  const isTestEnv = environment ? !environment.endsWith('-prod') : false;
   const isLoggedIn = session?.sub;
   return (
-    <>
+    <StyledLayout>
       <Head>
         <meta
           name="viewport"
@@ -73,15 +78,13 @@ const Layout: React.FC<Props> = ({ children, session, title }) => {
       </Head>
       <Navigation isLoggedIn={isLoggedIn} />
       <StyledMain>{children}</StyledMain>
-      {isTestEnv && enableTimeMachine &&
-        <TimeTravel />
-      }
+      {isTestEnv && enableTimeMachine && <TimeTravel />}
       <StyledFooter>
         <StyledDiv>
           <FooterLinks />
         </StyledDiv>
       </StyledFooter>
-    </>
+    </StyledLayout>
   );
 };
 
