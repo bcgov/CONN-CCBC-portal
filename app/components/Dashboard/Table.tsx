dc517bb1b25c7848334ee4ed068d1fa5d326580b
Vladimir <vladimir@button.is>
2022-09-12 10:12:53 -0700

    fix: rolled back using mocks.mocked_timestamp cookie in FE
    Signed-off-by: Vladimir <vladimir@button.is>

--- app/components/Dashboard/Table.tsx
+++ app/components/Dashboard/Table.tsx
@@ -107,17 +107,19 @@ const Table = ({ applications }: Props) => {
 
             const lastEditedIndex = formPages.indexOf(lastEditedPage) + 1;
 
-            const mockDate = cookie.get('mocks.mocked_timestamp');    
-            const isIntakeOpen = checkIntakeValidity(intakeByIntakeId?.openTimestamp, intakeByIntakeId?.closeTimestamp, mockDate);
+            const intakeClosingDate = intakeByIntakeId?.closeTimestamp;
+            const isIntakeClosed = intakeClosingDate
+              ? Date.parse(intakeClosingDate) < Date.now()
+              : false;
 
             const isWithdrawn = application.status === 'withdrawn';
             const isSubmitted = application.status === 'submitted';
-            const isEditable = !isWithdrawn && !(isSubmitted && !isIntakeOpen);
+            const isEditable = !isWithdrawn && !(isSubmitted && isIntakeClosed);
 
             const getApplicationUrl = () => {
               if (isWithdrawn) {
                 return `/form/${application.rowId}/${reviewPage}`;
-              } else if (isSubmitted && !isIntakeOpen) {
+              } else if (isSubmitted && isIntakeClosed) {
                 return `/form/${application.rowId}/${reviewPage}`;
               } else if (isSubmitted) {
                 return `/form/${rowId}/1`;
@@ -140,7 +142,7 @@ const Table = ({ applications }: Props) => {
                     <Link href={getApplicationUrl()}>
                       {isEditable ? 'Edit' : 'View'}
                     </Link>
-                    {isSubmitted && isIntakeOpen && (
+                    {isSubmitted && !isIntakeClosed && (
                       <div
                         onClick={() => setWithdrawId(rowId)}
                         data-testid="withdraw-btn-test"
