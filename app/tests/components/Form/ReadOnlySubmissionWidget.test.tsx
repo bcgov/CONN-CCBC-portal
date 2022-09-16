c12fd70d25784395f53ef7e45e9509dde9ee96d4
marcellmueller <mlmueller@protonmail.com>
2022-09-16 12:05:22 -0700

    chore: separate submission widget tests

--- app/tests/components/Form/ReadOnlySubmissionWidget.test.tsx
+++ app/tests/components/Form/ReadOnlySubmissionWidget.test.tsx
@@ -5,12 +5,8 @@ import type { JSONSchema7 } from 'json-schema';
 const schema = {
   title: 'Read only submission widget test',
   type: 'object',
-  required: ['submissionCompletedFor', 'submissionDate'],
+  required: ['submissionDate'],
   properties: {
-    submissionCompletedFor: {
-      title: 'Completed for (Legal organization name)',
-      type: 'string',
-    },
     submissionDate: {
       title: 'On this date (YYYY-MM-DD)',
       type: 'string',
@@ -19,34 +15,10 @@ const schema = {
 };
 
 const uiSchema = {
-  submissionCompletedFor: {
-    'ui:widget': 'ReadOnlySubmissionWidget',
-    'ui:options': {
-      'field-name': 'submissionCompletedFor',
-    },
-  },
   submissionDate: {
     'ui:widget': 'ReadOnlySubmissionWidget',
   },
 };
-
-jest.mock('next/router', () => ({
-  useRouter() {
-    return {
-      route: '/',
-      pathname: '',
-      query: { id: 1 },
-      asPath: '',
-      push: jest.fn(),
-      events: {
-        on: jest.fn(),
-        off: jest.fn(),
-      },
-      beforePopState: jest.fn(() => null),
-      prefetch: jest.fn(() => null),
-    };
-  },
-}));
 
 const renderStaticLayout = (
   schema: JSONSchema7,
@@ -64,62 +36,17 @@ const renderStaticLayout = (
 };
 
 describe('The ReadOnlySubmissionWidget', () => {
-  beforeEach(() => {
-    // Mock query params since we use them to generate url in component
-
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const useRouter = jest.spyOn(require('next/router'), 'useRouter');
-    useRouter.mockImplementation(() => ({
-      route: '/',
-      pathname: '',
-      query: { id: 1 },
-      asPath: '',
-      push: jest.fn(),
-      events: {
-        on: jest.fn(),
-        off: jest.fn(),
-      },
-      beforePopState: jest.fn(() => null),
-      prefetch: jest.fn(() => null),
-    }));
-  });
-
-  it('should render the titles', () => {
+  it('should render the title', () => {
     renderStaticLayout(schema as JSONSchema7, uiSchema as JSONSchema7, {});
-
-    expect(
-      screen.getByText('Completed for (Legal organization name)')
-    ).toBeInTheDocument();
 
     expect(screen.getByText('On this date (YYYY-MM-DD)')).toBeInTheDocument();
   });
 
-  it('should render the error message when submissionCompletedFor is empty', () => {
-    renderStaticLayout(schema as JSONSchema7, uiSchema as JSONSchema7, {});
-
-    expect(
-      screen.getByRole('link', { name: 'Organization Profile' })
-    ).toBeInTheDocument();
-  });
-
-  it('should not render the error message when submissionCompletedFor is filled', () => {
+  it('should render the value', () => {
     renderStaticLayout(schema as JSONSchema7, uiSchema as JSONSchema7, {
-      submissionCompletedFor: 'submissionCompletedFor test',
       submissionDate: '2022-09-22',
     });
 
-    expect(screen.queryByRole('link', { name: 'Organization Profile' })).toBe(
-      null
-    );
-  });
-
-  it('should render the values when they exist', () => {
-    renderStaticLayout(schema as JSONSchema7, uiSchema as JSONSchema7, {
-      submissionCompletedFor: 'submissionCompletedFor test',
-      submissionDate: '2022-09-22',
-    });
-
-    expect(screen.getByText('submissionCompletedFor test')).toBeInTheDocument();
     expect(screen.getByText('2022-09-22')).toBeInTheDocument();
   });
 });
