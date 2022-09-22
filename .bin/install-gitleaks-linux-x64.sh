e80eb8bdfd3c532ffd47e82a08f42fd6baf4cfae
Anthony Bushara <anthony@button.is>
2022-09-22 16:36:31 -0400

    fix(ci): gitleaks uses latest version

--- .bin/install-gitleaks-linux-x64.sh
+++ .bin/install-gitleaks-linux-x64.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -xeuo pipefail;
 
-version="8.12.0";
+version="8.13.0";
 releases_api="https://api.github.com/repos/zricethezav/gitleaks/releases/latest";
 releases_json="$(curl -s ${releases_api})";
 
