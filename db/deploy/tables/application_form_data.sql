b3453a3f9e2eb135885d24d6f5706ebf5985e072
Anthony Bushara <anthony@button.is>
2022-09-28 14:42:34 -0400

    fix: update RLS for form_data

--- db/deploy/tables/application_form_data.sql
+++ db/deploy/tables/application_form_data.sql
@@ -23,16 +23,16 @@ $grant$;
 
 -- RLS: can only see and insert its own records in form_data
 perform ccbc_private.upsert_policy('ccbc_auth_user_select_form_data', 'form_data', 'select', 'ccbc_auth_user',
- 'id in (select form_data_id from ccbc_public.application_form_data where application_id in (select id from ccbc_public.appliction where owner=(select sub from ccbc_public.session())))');
-perform ccbc_private.upsert_policy('ccbc_auth_user_update_form_data', 'form_data', 'update', 'ccbc_auth_user',
-'id in (select form_data_id from ccbc_public.application_form_data where application_id in (select id from ccbc_public.appliction where owner=(select sub from ccbc_public.session())))');
+ 'id in (select form_data_id from ccbc_public.application_form_data where form_data_id=id)');
+
+perform ccbc_private.upsert_policy('ccbc_auth_user_select_form_data', 'form_data', 'insert', 'ccbc_auth_user',
+ 'id in (select form_data_id from ccbc_public.application_form_data where form_data_id=id)');
+ perform ccbc_private.upsert_policy('ccbc_auth_user_update_form_data', 'form_data', 'update', 'ccbc_auth_user',
+'id in (select form_data_id from ccbc_public.application_form_data where form_data_id=id)');
 perform ccbc_private.upsert_policy('ccbc_auth_user_insert_application_form_data', 'application_form_data', 'insert', 'ccbc_auth_user',
 'application_id in (select id from ccbc_public.appliction where owner=(select sub from ccbc_public.session()))');
 
 perform ccbc_private.upsert_policy('ccbc_auth_user_select_application_form_data', 'application_form_data', 'select', 'ccbc_auth_user',
-'application_id in (select id from ccbc_public.appliction where owner=(select sub from ccbc_public.session()))');
-
-perform ccbc_private.upsert_policy('ccbc_auth_user_update_application_form_data', 'application_form_data', 'update', 'ccbc_auth_user',
 'application_id in (select id from ccbc_public.appliction where owner=(select sub from ccbc_public.session()))');
 
 -- comment on table ccbc_public.
