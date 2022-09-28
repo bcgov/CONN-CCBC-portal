5af1aec0fa432ac2baa80c2c96eb6cfa4d7ec338
Anthony Bushara <anthony@button.is>
2022-09-28 16:20:41 -0400

    fix: another update on RLS on form_data

--- db/deploy/tables/application_form_data.sql
+++ db/deploy/tables/application_form_data.sql
@@ -6,7 +6,6 @@ create table ccbc_public.application_form_data(
   id integer primary key generated always as identity,
   form_data_id integer references ccbc_public.form_data(id),
   application_id integer references ccbc_public.application(id)
-
 );
 
 -- Enable row-level security
@@ -18,23 +17,33 @@ $grant$
 begin
 perform ccbc_private.grant_permissions('select', 'application_form_data', 'ccbc_auth_user');
 perform ccbc_private.grant_permissions('insert', 'application_form_data', 'ccbc_auth_user');
+
 end
 $grant$;
 
 -- RLS: can only see and insert its own records in form_data
-perform ccbc_private.upsert_policy('ccbc_auth_user_select_form_data', 'form_data', 'select', 'ccbc_auth_user',
- 'id in (select form_data_id from ccbc_public.application_form_data where form_data_id=id)');
-
-perform ccbc_private.upsert_policy('ccbc_auth_user_select_form_data', 'form_data', 'insert', 'ccbc_auth_user',
- 'id in (select form_data_id from ccbc_public.application_form_data where form_data_id=id)');
- perform ccbc_private.upsert_policy('ccbc_auth_user_update_form_data', 'form_data', 'update', 'ccbc_auth_user',
+do
+$policy$
+begin
+perform ccbc_private.upsert_policy('ccbc_auth_user_select_form_data', 'form_data', 'select', 'ccbc_auth_user', 'id in (select form_data_id from ccbc_public.application_form_data where form_data_id = id )');
+perform ccbc_private.upsert_policy('ccbc_auth_user_update_form_data', 'form_data', 'update', 'ccbc_auth_user',
 'id in (select form_data_id from ccbc_public.application_form_data where form_data_id=id)');
+
 perform ccbc_private.upsert_policy('ccbc_auth_user_insert_application_form_data', 'application_form_data', 'insert', 'ccbc_auth_user',
-'application_id in (select id from ccbc_public.appliction where owner=(select sub from ccbc_public.session()))');
+'application_id in (select id from ccbc_public.application where owner=(select sub from ccbc_public.session()))');
 
 perform ccbc_private.upsert_policy('ccbc_auth_user_select_application_form_data', 'application_form_data', 'select', 'ccbc_auth_user',
-'application_id in (select id from ccbc_public.appliction where owner=(select sub from ccbc_public.session()))');
+'application_id in (select id from ccbc_public.application where owner=(select sub from ccbc_public.session()))');
+end
+$policy$;
 
--- comment on table ccbc_public.
+
+comment on table ccbc_public.application_form_data is 'Table to pair an application to form data';
+
+comment on column ccbc_public.application_form_data.id is 'Primary key of application_form_data table';
+
+comment on column ccbc_public.application_form_data.form_data_id is 'The foreign key of a form';
+
+comment on column ccbc_public.application_form_data.application_id is 'The foreign key of an application';
 
 commit;
