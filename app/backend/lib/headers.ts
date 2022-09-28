0d836015af8b9a6857b444181ca22ae2dd2e86f2
Matthieu Foucault <matthieu@button.is>
2022-09-28 10:50:12 -0700

    revert: disable CSP header

--- app/backend/lib/headers.ts
+++ app/backend/lib/headers.ts
@@ -1,5 +1,5 @@
-import helmet from "helmet";
-import config from "../../config";
+import helmet from 'helmet';
+import config from '../../config';
 
 const headersMiddleware = () => {
   const helmetMiddleware = helmet({
@@ -8,17 +8,14 @@ const headersMiddleware = () => {
   return (req, res, next) => {
     // Tell search + crawlers not to index non-production environments:
     if (
-      !config.get("OPENSHIFT_APP_NAMESPACE") ||
-      !config.get("OPENSHIFT_APP_NAMESPACE").endsWith("-prod")
+      !config.get('OPENSHIFT_APP_NAMESPACE') ||
+      !config.get('OPENSHIFT_APP_NAMESPACE').endsWith('-prod')
     ) {
-      res.append("X-Robots-Tag", "noindex, noimageindex, nofollow, noarchive");
+      res.append('X-Robots-Tag', 'noindex, noimageindex, nofollow, noarchive');
     }
 
-    // https://www.zaproxy.org/docs/alerts/10038/
-    res.append("Content-Security-Policy","default-src 'self'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; frame-ancestors 'self'; form-action 'self'");            
-
     // https://www.zaproxy.org/docs/alerts/10063-1/
-    res.append("Permissions-Policy","display-capture 'none'");
+    res.append('Permissions-Policy', "display-capture 'none'");
 
     helmetMiddleware(req, res, next);
   };
