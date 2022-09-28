ed2ec3d77f2fc90e06c4fe5a35160b09cd285f40
marcellmueller <marcel@button.is>
2022-09-28 15:54:55 -0700

    test: update intake alert test

--- app/tests/components/IntakeAlert.test.tsx
+++ app/tests/components/IntakeAlert.test.tsx
@@ -2,14 +2,14 @@ import IntakeAlert from '../../components/IntakeAlert';
 import { render, screen } from '@testing-library/react';
 
 const renderStaticLayout = () => {
-  return render(<IntakeAlert />);
+  return render(<IntakeAlert openTimestamp="2022-09-19T09:00:00-07:00" />);
 };
 
 describe('The alert component', () => {
   it('should render the correct date', () => {
     renderStaticLayout();
-    const alert = screen.getByText('September 29');
+    const alert = screen.getByText('September 19, 2022');
 
-    expect(alert).toBeTruthy();
+    expect(alert).toBeInTheDocument();
   });
 });
