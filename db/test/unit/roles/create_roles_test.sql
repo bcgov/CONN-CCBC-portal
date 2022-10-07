90752f93588252b6eaa12d8b6ed5ccbd861f7ccc
marcellmueller <marcel@button.is>
2022-10-07 14:45:52 -0700

    feat: add function receive_applications to sqitch plan

--- db/test/unit/roles/create_roles_test.sql
+++ db/test/unit/roles/create_roles_test.sql
@@ -14,6 +14,11 @@ select isnt_superuser(
     'ccbc_guest should not be a super user'
 );
 
+select has_role( 'ccbc_guest', 'role ccbc_job_executor exists' );
+select isnt_superuser(
+    'ccbc_job_executor',
+    'ccbc_job_executor should not be a super user'
+);
 
 select finish();
 rollback;
