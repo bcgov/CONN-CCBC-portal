32423995bcf6011f914a7249ab6280f84e09c749
marcellmueller <marcel@button.is>
2022-10-11 10:27:16 -0700

    feat: add role ccbc_job_executor with permissions

--- db/revert/create_roles.sql
+++ db/revert/create_roles.sql
@@ -1,14 +1,40 @@
--- Revert ccbc:create_roles from pg
+-- Deploy ccbc:create_roles to pg
 
 begin;
 
 do
 $do$
 begin
-raise NOTICE 'Created roles may be used in other projects and need to be manually deleted if needed';
-end;
-$do$;
 
-select true;
+  if not exists (
+    select true
+    from pg_catalog.pg_roles
+    where rolname = 'ccbc_guest') then
+
+    create role ccbc_guest;
+  end if;
+
+  if not exists (
+    select true
+    from pg_catalog.pg_roles
+    where rolname = 'ccbc_auth_user') then
+
+    create role ccbc_auth_user;
+  end if;
+
+  if not exists (
+    select true
+    from pg_catalog.pg_roles
+    where rolname = 'ccbc_app') then
+
+    create user ccbc_app;
+  end if;
+
+
+  grant ccbc_guest, ccbc_auth_user to ccbc_app;
+  execute format('grant create, connect on database %I to ccbc_app', current_database());
+
+end
+$do$;
 
 commit;
