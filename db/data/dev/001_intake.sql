33c0b57b107dfb00439248921009b9f43c7a94f9
Matthieu Foucault <matthieu@button.is>
2022-10-12 16:12:42 -0700

    test: add second intake in dev and test data

--- db/data/dev/001_intake.sql
+++ db/data/dev/001_intake.sql
@@ -4,7 +4,8 @@ insert into
   ccbc_public.intake(id, open_timestamp, close_timestamp, ccbc_intake_number)
 overriding system value
 values
-  (1, '2022-08-19 09:00:00 America/Vancouver','2022-11-06 09:00:00 America/Vancouver', 1)
+  (1, '2022-08-19 09:00:00 America/Vancouver','2022-11-06 09:00:00 America/Vancouver', 1),
+  (2, '2022-01-15 00:00:00 America/Vancouver','2023-03-15 00:00:00 America/Vancouver', 2)
 on conflict (id) do update set
 open_timestamp = excluded.open_timestamp,
 close_timestamp = excluded.close_timestamp,
