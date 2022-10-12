04a3f79076f103b8171d41ff375552896177ecf5
Anthony Bushara <anthony@button.is>
2022-10-12 16:01:00 -0400

    feat: add application status to determine if is editable

--- db/deploy/computed_columns/form_data_is_editable.sql
+++ db/deploy/computed_columns/form_data_is_editable.sql
@@ -6,25 +6,28 @@ create or replace function ccbc_public.form_data_is_editable(form_data ccbc_publ
 $$
 declare
 open_intake_id int;
-attached_application_id int;
+attached_application ccbc_public.application;
+attached_application_status varchar(1000);
 form_data_status varchar(1000):= form_data.form_data_status_type_id;
 begin
 
   if form_data_status = 'pending' then
-    return 'true';
+    return true;
   end if;
 
   select id from ccbc_public.open_intake() into open_intake_id;
 
-  select application.intake_id from ccbc_public.application_form_data as afd, ccbc_public.application as application where afd.form_data_id = form_data.id
-  and afd.application_id = application.id limit 1 into attached_application_id;
+  select app.* into attached_application from ccbc_public.application_form_data as afd, ccbc_public.application as app where afd.form_data_id = form_data.id
+  and afd.application_id = app.id ;
 
-  if form_data_status = 'committed' and open_intake_id = attached_application_id then
-    return 'true';
+  select ccbc_public.application_status(attached_application) into attached_application_status;
+
+  if form_data_status = 'committed' and open_intake_id = attached_application.id and attached_application_status = 'submitted' then
+    return true;
   end if;
 
-  return 'false';
-end;
+  return false;
+end
 $$ language plpgsql stable;
 
 grant execute on function ccbc_public.form_data_is_editable to ccbc_auth_user;
