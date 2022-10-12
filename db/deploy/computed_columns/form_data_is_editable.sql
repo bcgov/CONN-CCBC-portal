474500b703e6eb222782c85e5333c9383d247d45
Anthony Bushara <anthony@button.is>
2022-10-12 10:08:26 -0400

    feat: update form_data_id_editable to use plpgsql for better readability

--- db/deploy/computed_columns/form_data_is_editable.sql
+++ db/deploy/computed_columns/form_data_is_editable.sql
@@ -4,12 +4,28 @@ begin;
 
 create or replace function ccbc_public.form_data_is_editable(form_data ccbc_public.form_data) returns boolean as
 $$
--- Need to change with the id being linked with the form's intake
-  select ((intake.id is not null and form_data.form_data_status_type_id = 'committed') or form_data.form_data_status_type_id = 'pending')
-  from ccbc_public.intake as intake, ccbc_public.application_form_data as afd, ccbc_public.application as application, ccbc_public.open_intake() as open_intake
-  where ((intake.id = application.intake_id and open_intake.id is not null and open_intake.id = intake.id) or application.intake_id is null or open_intake.id is null)
-  and afd.form_data_id = form_data.id and afd.application_id = application.id limit 1;
-$$ language sql stable;
+declare
+open_intake_id int;
+attached_application_id int;
+form_data_status varchar(1000):= form_data.form_data_status_type_id;
+begin
+
+  if form_data_status = 'pending' then
+    return 'true';
+  end if;
+
+  select id from ccbc_public.open_intake() into open_intake_id;
+
+  select application.intake_id from ccbc_public.application_form_data as afd, ccbc_public.application as application where afd.form_data_id = form_data.id
+  and afd.application_id = application.id limit 1 into attached_application_id;
+
+  if form_data_status = 'committed' and open_intake_id = attached_application_id then
+    return 'true';
+  end if;
+
+  return 'false';
+end;
+$$ language plpgsql stable;
 
 grant execute on function ccbc_public.form_data_is_editable to ccbc_auth_user;
 
