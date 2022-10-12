a30db0e8b8d80727d1c4ddc83c5a92d0ebcf23e2
Anthony Bushara <anthony@button.is>
2022-10-12 09:45:57 -0400

    feat: update form_data_is_editable for new form_data_is_editable statuses

--- db/deploy/computed_columns/form_data_is_editable.sql
+++ db/deploy/computed_columns/form_data_is_editable.sql
@@ -4,8 +4,11 @@ begin;
 
 create or replace function ccbc_public.form_data_is_editable(form_data ccbc_public.form_data) returns boolean as
 $$
-  select ((id is not null and form_data.form_data_status_type_id = 'submitted') or form_data.form_data_status_type_id = 'draft'
-  )from ccbc_public.open_intake();
+-- Need to change with the id being linked with the form's intake
+  select ((intake.id is not null and form_data.form_data_status_type_id = 'committed') or form_data.form_data_status_type_id = 'pending')
+  from ccbc_public.intake as intake, ccbc_public.application_form_data as afd, ccbc_public.application as application, ccbc_public.open_intake() as open_intake
+  where ((intake.id = application.intake_id and open_intake.id is not null and open_intake.id = intake.id) or application.intake_id is null or open_intake.id is null)
+  and afd.form_data_id = form_data.id and afd.application_id = application.id limit 1;
 $$ language sql stable;
 
 grant execute on function ccbc_public.form_data_is_editable to ccbc_auth_user;
