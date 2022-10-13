0297251b33979bdc24ccd26e0cb25afd4cfc7828
Matthieu Foucault <matthieu@button.is>
2022-10-13 16:32:10 -0700

    test: add tests for sso-middleware

---
+++ app/tests/backend/lib/sso-middleware.test.ts
@@ -0,0 +1,40 @@
+/**
+ * @jest-environment node
+ */
+import ssoMiddleware from 'backend/lib/sso-middleware';
+import request from 'supertest';
+import express from 'express';
+import session from 'express-session';
+import { IDP_HINTS, IDP_HINT_PARAM } from 'data/ssoConstants';
+
+describe('The sso middleware', () => {
+  let app;
+
+  beforeEach(async () => {
+    app = express();
+    app.use(session({ secret: 'keyboard cat' }));
+    app.use(await ssoMiddleware());
+  });
+
+  it('should forward valid kc_idp_hint parameters on login', async () => {
+    Object.values(IDP_HINTS).forEach(async (idpHint) => {
+      const response = await request(app).post(
+        `/login?${IDP_HINT_PARAM}=${idpHint}`
+      );
+      expect(response.status).toBe(302);
+      expect(response.headers.location).toInclude(
+        `${IDP_HINT_PARAM}=${idpHint}`
+      );
+    });
+  });
+
+  it('should ignore invalid kc_idp_hint parameters', async () => {
+    const response = await request(app).post(
+      `/login?${IDP_HINT_PARAM}=notAValidIDP`
+    );
+    expect(response.status).toBe(302);
+    expect(response.headers.location).not.toInclude(
+      `${IDP_HINT_PARAM}=notAValidIDP`
+    );
+  });
+});
--- app/tests/jest-setup.ts
+++ app/tests/jest-setup.ts
@@ -2,11 +2,13 @@ import crypto from 'crypto';
 import { Settings } from 'luxon';
 
 // Some rjsf features require window.crypto, which isn't provided by jsdom
-Object.defineProperty(global.self, 'crypto', {
-  value: {
-    getRandomValues: (arr) => crypto.randomBytes(arr.length),
-  },
-});
+if (global.self) {
+  Object.defineProperty(global.self, 'crypto', {
+    value: {
+      getRandomValues: (arr) => crypto.randomBytes(arr.length),
+    },
+  });
+}
 
 Settings.defaultLocale = 'en-CA';
 Settings.defaultZone = 'America/Vancouver';
