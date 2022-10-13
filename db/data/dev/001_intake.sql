7e0f52030b87c8f2e79393cfa8a75e7e981aa4fc
Matthieu Foucault <matthieu@button.is>
2022-10-13 11:07:56 -0700

    chore: fix opening year of second intake in test

--- db/data/dev/001_intake.sql
+++ db/data/dev/001_intake.sql
@@ -5,7 +5,7 @@ insert into
 overriding system value
 values
   (1, '2022-08-19 09:00:00 America/Vancouver','2022-11-06 09:00:00 America/Vancouver', 1),
-  (2, '2022-01-15 00:00:00 America/Vancouver','2023-03-15 00:00:00 America/Vancouver', 2)
+  (2, '2023-01-15 00:00:00 America/Vancouver','2023-03-15 00:00:00 America/Vancouver', 2)
 on conflict (id) do update set
 open_timestamp = excluded.open_timestamp,
 close_timestamp = excluded.close_timestamp,
