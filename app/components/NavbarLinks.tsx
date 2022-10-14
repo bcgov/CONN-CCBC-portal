e7807ba4270eb413ec53069215a74fbd4be5fad3
Matthieu Foucault <matthieu@button.is>
2022-10-14 11:50:24 -0700

    style: tweak navbar padding

--- app/components/NavbarLinks.tsx
+++ app/components/NavbarLinks.tsx
@@ -9,7 +9,7 @@ const StyledLi = styled.li`
 `;
 
 const StyledUl = styled.ul`
-  padding-left: ${(props) => props.theme.spacing.large};
+  padding-left: ${(props) => props.theme.spacing.medium};
 `;
 
 const SubHeaderNavbarLinks: React.FC = () => (
