15737abf0e64759e01f7ab6c34fa621796426faf
marcellmueller <marcel@button.is>
2022-10-17 13:26:48 -0700

    feat: grant execute function to ccbc_job_executor on application_status function

--- db/deploy/computed_columns/application_status.sql
+++ db/deploy/computed_columns/application_status.sql
@@ -3,14 +3,15 @@
 
 begin;
 
-create or replace function ccbc_public.application_status(application ccbc_public.application) returns varchar as 
+create or replace function ccbc_public.application_status(application ccbc_public.application) returns varchar as
 $$
-select status from ccbc_public.application_status 
+select status from ccbc_public.application_status
     where application_id= application.id
     order by id desc limit 1;
 $$ language sql stable;
 
 grant execute on function ccbc_public.application_status to ccbc_auth_user;
+grant execute on function ccbc_public.application_status to ccbc_job_executor;
 
 comment on function ccbc_public.application_status is 'Computed column to return status of an application';
 
