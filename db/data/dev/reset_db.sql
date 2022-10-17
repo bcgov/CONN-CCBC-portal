e0b27ab52a200e6d4a9fa2d153cb99d6f1b0d99d
Vladimir <vladimir@button.is>
2022-10-17 16:55:35 -0700

    fix: correct order of delete
    Signed-off-by: Vladimir <vladimir@button.is>

--- db/data/dev/reset_db.sql
+++ db/data/dev/reset_db.sql
@@ -1,8 +1,8 @@
 begin;
 
+delete from ccbc_public.application_form_data;
 delete from ccbc_public.form_data;
 delete from ccbc_public.attachment;
-delete from ccbc_public.application_form_data;
 delete from ccbc_public.application_status;
 delete from ccbc_public.application;
 
