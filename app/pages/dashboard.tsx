fc5ca02783140425bbee027f4da94e5bdacc07c1
Vladimir <vladimir@button.is>
2022-10-20 15:02:12 -0700

    fix: rollback - need to keep hardcoded messages too
    Signed-off-by: Vladimir <vladimir@button.is>

--- app/pages/dashboard.tsx
+++ app/pages/dashboard.tsx
@@ -3,6 +3,8 @@ import { useRouter } from 'next/router';
 import { withRelay, RelayProps } from 'relay-nextjs';
 import { usePreloadedQuery } from 'react-relay/hooks';
 import { graphql } from 'react-relay'; 
+import { DateTime } from 'luxon';
+import Link from '@button-inc/bcgov-theme/Link';
 import { useFeature } from '@growthbook/growthbook-react'; 
 import defaultRelayOptions from '../lib/relay/withRelayOptions';
 import StyledGovButton from '../components/StyledGovButton';
@@ -90,16 +92,42 @@ const Dashboard = ({
       <div>
         <section>
           <h1>Dashboard</h1>
-          {!openIntake && (
-            <DynamicAlert dateTimestamp={nextIntake?.openTimestamp} text={closedIntakeBanner.text}
-            variant={closedIntakeBanner.variant} includeLink 
-            displayOpenDate = {closedIntakeBanner.displayOpenDate}/>        
-          )}
+          
           {openIntake && (
             <DynamicAlert dateTimestamp={closeTimestamp} text={openIntakeBanner.text}
             variant={openIntakeBanner.variant} includeLink={false} displayOpenDate={false} />
           )}
 
+          {openIntake ? (
+            <p>
+              Review of applications will begin on{' '}
+              {DateTime.fromISO(closeTimestamp, {
+                locale: 'en-CA',
+                zone: 'America/Vancouver',
+              }).toFormat('MMMM dd, yyyy, ttt')}
+              . You can edit draft and submitted applications until this date.
+            </p>
+          ) : (
+            <div>
+              <p>Applications are currently not being accepted.</p>
+              <p>
+                Please check the{' '}
+                <Link
+                  href="https://www2.gov.bc.ca/gov/content/governments/connectivity-in-bc/20601/20601-63737"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  program webpage
+                </Link>{' '}
+                for updates.
+              </p>
+            </div>
+          )}
+          {!openIntake && (
+            <DynamicAlert dateTimestamp={nextIntake?.openTimestamp} text={closedIntakeBanner.text}
+            variant={closedIntakeBanner.variant} includeLink 
+            displayOpenDate = {closedIntakeBanner.displayOpenDate}/>        
+          )}
           <StyledGovButton
             onClick={handleCreateApplication}
             disabled={!openIntake}
