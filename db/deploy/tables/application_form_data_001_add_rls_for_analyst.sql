da782e4724080b3daef51f861cb555508f7f250d
Anthony Bushara <anthony@button.is>
2022-10-25 11:03:14 -0400

    feat: form data has permissions

---
+++ db/deploy/tables/application_form_data_001_add_rls_for_analyst.sql
@@ -0,0 +1,20 @@
+-- Deploy ccbc:tables/application_form_data_001_add_rls_for_analyst to pg
+
+begin;
+
+do
+$$
+begin
+perform ccbc_private.grant_permissions('select', 'application_form_data', 'ccbc_analyst');
+perform ccbc_private.grant_permissions('insert', 'application_form_data', 'ccbc_analyst');
+
+-- the application table rls prevents an analyst from seeing non-received applications
+perform ccbc_private.upsert_policy('ccbc_analyst_select_received_form_data',
+'application_form_data','select', 'ccbc_analyst', 'application_id in (select id from ccbc_public.application)');
+
+perform ccbc_private.upsert_policy('ccbc_analyst_insert_new_received_form_data',
+'application_form_data','insert', 'ccbc_analyst', 'application_id in (select id from ccbc_public.application)');
+end
+$$;
+
+commit;
