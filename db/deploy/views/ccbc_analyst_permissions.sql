1b7c32a12fc476378bc17f408d905a7a8774e7e5
Vladimir <vladimir@button.is>
2022-10-25 12:16:48 -0700

    fix: updated permissions

--- db/deploy/views/ccbc_analyst_permissions.sql
+++ db/deploy/views/ccbc_analyst_permissions.sql
@@ -2,6 +2,18 @@
 
 begin;
 
+do
+$grant$
+begin
+
+perform ccbc_private.grant_permissions('select', 'application', 'ccbc_analyst');
+perform ccbc_private.grant_permissions('select', 'application_status', 'ccbc_analyst');
+perform ccbc_private.grant_permissions('select', 'application_form_data', 'ccbc_analyst');
+perform ccbc_private.grant_permissions('select', 'form_data', 'ccbc_analyst');
+
 grant execute on function ccbc_public.application_status to ccbc_analyst;
+
+end
+$grant$;
 
 commit;
