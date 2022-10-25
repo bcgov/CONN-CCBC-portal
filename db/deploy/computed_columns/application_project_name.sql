6399e6c811870a3874a7ba04dc6980eb78366cb5
Anthony Bushara <anthony@button.is>
2022-10-25 15:18:05 -0400

    chore: add comment on application project name computed column

--- db/deploy/computed_columns/application_project_name.sql
+++ db/deploy/computed_columns/application_project_name.sql
@@ -9,4 +9,6 @@ $$ language sql stable;
 grant execute on function ccbc_public.application_project_name to ccbc_auth_user;
 grant execute on function ccbc_public.application_project_name to ccbc_analyst;
 
+comment on function ccbc_public.application_project_name is 'Computed column to display the project name';
+
 commit;
