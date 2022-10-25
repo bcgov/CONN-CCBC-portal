692af30cf206d51f7e9eb73a213fd862da9ce7b6
Vladimir <vladimir@button.is>
2022-10-25 14:46:51 -0700

    fix: policy

--- db/deploy/views/ccbc_analyst_permissions.sql
+++ db/deploy/views/ccbc_analyst_permissions.sql
@@ -11,6 +11,11 @@ perform ccbc_private.grant_permissions('select', 'application_status', 'ccbc_ana
 perform ccbc_private.grant_permissions('select', 'application_form_data', 'ccbc_analyst');
 perform ccbc_private.grant_permissions('select', 'form_data', 'ccbc_analyst');
 
+perform ccbc_private.upsert_policy('ccbc_analyst_select_application', 'application', 'select', 'ccbc_analyst', 'true');
+perform ccbc_private.upsert_policy('ccbc_analyst_select_application', 'application_status', 'select', 'ccbc_analyst', 'true');
+perform ccbc_private.upsert_policy('ccbc_analyst_select_application', 'application_form_data', 'select', 'ccbc_analyst', 'true');
+perform ccbc_private.upsert_policy('ccbc_analyst_select_application', 'form_data', 'select', 'ccbc_analyst', 'true');
+
 grant execute on function ccbc_public.application_status to ccbc_analyst;
 
 end
