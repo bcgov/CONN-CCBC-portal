1f0850bb7518d9435e36461242d891e0cab74b6a
Matthieu Foucault <matthieu@button.is>
2022-10-28 11:36:57 -0700

    test: test application uses correct owner sub

--- db/data/e2e/001_application.sql
+++ db/data/e2e/001_application.sql
@@ -1,21 +1,21 @@
 begin;
 
-insert into ccbc_public.application 
+insert into ccbc_public.application
 (id, ccbc_number,owner,intake_id,created_by, created_at,updated_by, updated_at)
 overriding system value
-values 
-(1,'', '00000000-0000-0000-0000-000000000000',1,1,'2022-10-17 10:16:45.319172-07',1,'2022-10-17 10:16:45.319172-07');
+values
+(1,'', 'mockUser@ccbc_auth_user',1,1,'2022-10-17 10:16:45.319172-07',1,'2022-10-17 10:16:45.319172-07');
 
 insert into ccbc_public.application_status
 (id, application_id, status,created_by, created_at)
 overriding system value
-values 
+values
 (1,1,'draft',1,'2022-10-17 10:16:45.319172-07');
 
 insert into ccbc_public.form_data
 (id, json_data, last_edited_page, form_data_status_type_id, created_by, created_at, updated_by, updated_at)
 overriding system value
-values 
+values
 (1,'{"submission": {"submissionDate": "2022-10-17"}, "projectInformation": {"projectTitle": "Test application"}}',
     'projectArea', 'pending',1,'2022-10-17 16:28:11.006719-07',1,'2022-10-17 16:28:26.105206-07');
 
