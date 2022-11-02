bf7b4aba6e3c7af5cc11a4c7c30c9bdaf0ec00f1
marcellmueller <marcel@button.is>
2022-11-02 12:32:06 -0700

    test: add placeholder tests for assessments and history page

---
+++ app/tests/pages/analyst/application/[applicationId]/assessments.test.tsx
@@ -0,0 +1,40 @@
+import { screen } from '@testing-library/react';
+import Assessments from 'pages/analyst/application/[applicationId]/assessments';
+import PageTestingHelper from 'tests/utils/pageTestingHelper';
+import compiledassessmentsQuery, {
+  assessmentsQuery,
+} from '__generated__/assessmentsQuery.graphql';
+
+const mockQueryPayload = {
+  Query() {
+    return {
+      session: {
+        sub: '4e0ac88c-bf05-49ac-948f-7fd53c7a9fd6',
+      },
+    };
+  },
+};
+
+const pageTestingHelper = new PageTestingHelper<assessmentsQuery>({
+  pageComponent: Assessments,
+  compiledQuery: compiledassessmentsQuery,
+  defaultQueryResolver: mockQueryPayload,
+  defaultQueryVariables: {},
+});
+
+describe('The index page', () => {
+  beforeEach(() => {
+    pageTestingHelper.reinit();
+  });
+
+  it('displays the title', async () => {
+    pageTestingHelper.loadQuery();
+    pageTestingHelper.renderPage();
+
+    expect(screen.getByText(`Assessments placeholder`)).toBeVisible();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
