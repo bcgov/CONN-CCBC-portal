23d70b530ff29cd1560a61c8913e5fbcd18bac58
marcellmueller <marcel@button.is>
2022-11-03 16:25:47 -0700

    feat: increase happo max screenshot height

--- app/.happo.js
+++ app/.happo.js
@@ -2,6 +2,7 @@ const { RemoteBrowserTarget } = require('happo.io');
 const config = require('./config/index.js');
 
 const VIEWPORT = '1366x768';
+const MAXHEIGHT = 20000;
 
 module.exports = {
   apiKey: config.get('HAPPO_API_KEY'),
@@ -10,15 +11,20 @@ module.exports = {
   targets: {
     chrome: new RemoteBrowserTarget('chrome', {
       viewport: VIEWPORT,
+      maxHeight: MAXHEIGHT,
     }),
     edge: new RemoteBrowserTarget('edge', {
       viewport: VIEWPORT,
+      maxHeight: MAXHEIGHT,
     }),
     firefox: new RemoteBrowserTarget('firefox', {
       viewport: VIEWPORT,
+      maxHeight: MAXHEIGHT,
     }),
     safari: new RemoteBrowserTarget('safari', {
       viewport: VIEWPORT,
+      maxHeight: MAXHEIGHT,
+      scrollStitch: true,
     }),
   },
 };
