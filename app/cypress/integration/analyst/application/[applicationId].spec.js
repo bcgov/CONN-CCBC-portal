22dbee876f3a8f503eebc954c0a770ee5ae67e5c
marcellmueller <marcel@button.is>
2022-11-04 09:03:39 -0700

    test: refactor applicationId e2e test

--- app/cypress/integration/analyst/application/[applicationId].spec.js
+++ app/cypress/integration/analyst/application/[applicationId].spec.js
@@ -1,5 +1,5 @@
 describe('The analyst application view', () => {
-  it('loads', () => {
+  beforeEach(() => {
     cy.mockLogin('ccbc_analyst');
     const mockedDateString = '2022-10-10';
     const mockedDate = new Date(mockedDateString);
@@ -8,6 +8,9 @@ describe('The analyst application view', () => {
     cy.sqlFixture('e2e/001_intake');
     cy.sqlFixture('e2e/001_application');
     cy.sqlFixture('e2e/001_application_received');
+  });
+
+  it('loads', () => {
     cy.visit('/analyst/application/1');
     cy.contains('h1', 'Application');
     cy.get('body').happoScreenshot({ component: 'Analyst application view' });
