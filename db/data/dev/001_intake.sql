acff47f18900b73735024fa04d8ee4bc39e33a80
Matthieu Foucault <matthieu@button.is>
2022-11-04 13:23:06 -0700

    fix: use the correct role before inserting in intake table

--- db/data/dev/001_intake.sql
+++ db/data/dev/001_intake.sql
@@ -1,5 +1,14 @@
 begin;
 
+do
+$$
+begin
+
+execute format('set role to %I',(select tableowner from pg_tables where tablename = 'intake' and schemaname = 'ccbc_public'));
+
+end
+$$;
+
 insert into
   ccbc_public.intake(id, open_timestamp, close_timestamp, ccbc_intake_number)
 overriding system value
