9af436e35b3804737378ddb8183506ea30298181
Anthony Bushara <anthony@button.is>
2022-11-09 09:59:15 -0500

    chore: update mutations and computed columns

--- db/revert/computed_columns/application_form_data@1.12.0.sql
+++ db/revert/computed_columns/application_form_data@1.12.0.sql
@@ -4,18 +4,12 @@ begin;
 
 create or replace function ccbc_public.application_form_data(application ccbc_public.application) returns ccbc_public.form_data as
 $$
-  select row(fd.*)::ccbc_public.form_data from ccbc_public.form_data as fd, ccbc_public.form as f
-   where
-   fd.form_schema_id = f.id and
-   f.form_type = 'intake' and
-   fd.id =
+  select row(form_data.*)::ccbc_public.form_data from ccbc_public.form_data where id =
    (select form_data_id from ccbc_public.application_form_data where
     application_id = application.id order by form_data_id desc limit 1 );
 $$ language sql stable;
 
 grant execute on function ccbc_public.application_form_data to ccbc_auth_user;
-grant execute on function ccbc_public.application_form_data to ccbc_analyst;
-grant execute on function ccbc_public.application_form_data to ccbc_admin;
 
 comment on function ccbc_public.application_form_data is 'Computed column to display form_data';
 
--- db/revert/mutations/create_application@1.12.0.sql
+++ db/revert/mutations/create_application@1.12.0.sql
@@ -10,8 +10,6 @@ declare
   _sub varchar;
   result ccbc_public.application;
   new_application_id int;
-  new_form_data_id int;
-  _form_schema_id int;
 begin
 
   select open_timestamp from ccbc_public.open_intake() into _open_timestamp;
@@ -21,21 +19,8 @@ begin
 
   select sub into _sub from ccbc_public.session();
 
--- Should intakes be linked to the form_schema_id? I'm guessing here that the latest schema is the one we want to use
-  select id into _form_schema_id from ccbc_public.form where form_type = 'intake' order by id desc limit 1;
-
-  insert into ccbc_public.application (owner) values (_sub)
+  insert into ccbc_public.application (form_data, owner) values ('{}'::jsonb, _sub)
    returning id into new_application_id;
-
-  -- using nextval instead of returning id on insert to prevent triggering select RLS,
-  -- which requires the application_form_data record
-  new_form_data_id := nextval(pg_get_serial_sequence('ccbc_public.form_data','id'));
-
-  insert into ccbc_public.form_data (id, json_data, form_schema_id) overriding system value
-   values (new_form_data_id , '{}'::jsonb, _form_schema_id);
-
-  insert into ccbc_public.application_form_data (application_id, form_data_id)
-   values (new_application_id, new_form_data_id);
 
   insert into ccbc_public.application_status (application_id, status)
   values (new_application_id, 'draft');
