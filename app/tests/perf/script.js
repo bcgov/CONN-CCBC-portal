603dca6cb5e85ba3aae4c32b945b8e922701cf0b
Matthieu Foucault <matthieu@button.is>
2022-11-10 08:20:37 -0800

    chore: cleanup script

--- app/tests/perf/script.js
+++ app/tests/perf/script.js
@@ -2,6 +2,7 @@ import http from 'k6/http';
 import exec from 'k6/execution';
 import { check } from 'k6';
 
+// Smoke testing options, use those when updating the script
 // export const options = {
 //   scenarios: {
 //     queries: {
@@ -46,24 +47,8 @@ export const options = {
   thresholds: {
     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
   },
-  rps: 50,
+  rps: 50, // don't increase this without consulting platform services first
 };
-
-// export const options = {
-//   stages: [
-//     { duration: '5m', target: 60 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
-//     { duration: '10m', target: 60 }, // stay at 60 users for 10 minutes
-//     { duration: '3m', target: 100 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
-//     { duration: '2m', target: 100 }, // stay at 100 users for short amount of time (peak hour)
-//     { duration: '3m', target: 60 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
-//     { duration: '10m', target: 60 }, // continue at 60 for additional 10 minutes
-//     { duration: '5m', target: 0 }, // ramp-down to 0 users
-//   ],
-//   thresholds: {
-//     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
-//   },
-//   rps: 50,
-// };
 
 const numApplications = __ENV.NUM_APPLICATIONS;
 
