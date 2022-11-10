3fd696d9569f79c48636597a555cd6883ffc2b77
marcellmueller <marcel@button.is>
2022-11-10 11:23:55 -0800

    feat: add application header component

--- app/components/Analyst/index.ts
+++ app/components/Analyst/index.ts
@@ -1,3 +1,4 @@
 export { default as AnalystLayout } from './AnalystLayout';
+export { default as ApplicationHeader } from './ApplicationHeader';
 export { default as NavigationSidebar } from './NavigationSidebar';
 export { default as NavItem } from './NavItem';
