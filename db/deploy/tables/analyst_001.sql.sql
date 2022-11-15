cbb9bd9bb299a35caf63c44e863c83c53a17e3f7
marcellmueller <marcel@button.is>
2022-11-15 14:40:20 -0800

    feat: add analyst table active column

---
+++ db/deploy/tables/analyst_001.sql.sql
@@ -0,0 +1,8 @@
+-- Deploy ccbc:tables/analyst_001.sql to pg
+
+begin;
+
+alter table ccbc_public.analyst
+add column active boolean default false;
+
+commit;
---
+++ db/revert/tables/analyst_001.sql.sql
@@ -0,0 +1,7 @@
+-- Revert ccbc:tables/analyst_001.sql from pg
+
+behin;
+
+-- XXX Add DDLs here.
+
+COMMIT;
