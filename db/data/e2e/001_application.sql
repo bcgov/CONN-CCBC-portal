decd7f4a98c71598c0f90fe22804dfeb102a7bfb
Anthony Bushara <anthony@button.is>
2022-11-15 11:34:10 -0500

    chore(test): update insert to include json_schema

--- db/data/e2e/001_application.sql
+++ db/data/e2e/001_application.sql
@@ -15,7 +15,7 @@ values
 (1,1,'draft',1,'2022-10-17 10:16:45.319172-07');
 
 insert into ccbc_public.form_data
-(id, json_data, last_edited_page, form_data_status_type_id, created_by, created_at, updated_by, updated_at)
+(id, json_data, last_edited_page, form_data_status_type_id, created_by, created_at, updated_by, updated_at, form_schema_id)
 overriding system value
 values
 (1,
@@ -54,7 +54,7 @@ $${
     ]}
 }$$,
 'projectInformation',
-'pending',1,'2022-10-17 16:28:11.006719-07',1,'2022-10-17 16:28:26.105206-07');
+'pending',1,'2022-10-17 16:28:11.006719-07',1,'2022-10-17 16:28:26.105206-07', 1);
 
 insert into ccbc_public.application_form_data
 (form_data_id, application_id)
