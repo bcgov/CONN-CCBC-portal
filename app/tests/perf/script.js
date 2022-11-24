f0410ff9ec42baf79ecca62f825e04eb82619301
marcellmueller <marcel@button.is>
2022-11-24 15:27:11 -0800

    feat: add FormData function and start k6 upload test

--- app/tests/perf/script.js
+++ app/tests/perf/script.js
@@ -1,54 +1,68 @@
+/* eslint-disable */
 import http from 'k6/http';
 import exec from 'k6/execution';
 import { check } from 'k6';
+import { FormData } from './form-data.js';
+
+const binFile = open('../../../k6Results/file2.bin', 'b');
 
 // Smoke testing options, use those when updating the script
-// export const options = {
-//   scenarios: {
-//     queries: {
-//       executor: 'constant-vus',
-//       vus: 1, // 1 user looping for 1 minute
-//       duration: '10s',
-//     },
-//     mutations: {
-//       startTime: '10s',
-//       executor: 'shared-iterations',
-//       vus: 1, // 1 user looping for 10 iterations
-//       iterations: 10,
-//     },
-//   },
-//   thresholds: {
-//     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
-//   },
-//   rps: 50,
-// };
-
 export const options = {
   scenarios: {
-    queries: {
-      executor: 'ramping-vus',
-      stages: [
-        { duration: '5m', target: 60 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
-        { duration: '10m', target: 60 }, // stay at 60 users for 10 minutes
-        { duration: '3m', target: 100 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
-        { duration: '2m', target: 100 }, // stay at 100 users for short amount of time (peak hour)
-        { duration: '3m', target: 60 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
-        { duration: '10m', target: 60 }, // continue at 60 for additional 10 minutes
-        { duration: '2m', target: 0 }, // ramp-down to 0 users
-      ],
-    },
-    mutations: {
-      startTime: '35m',
+    //   queries: {
+    //     executor: 'constant-vus',
+    //     vus: 1, // 1 user looping for 1 minute
+    //     duration: '10s',
+    //   },
+    //   mutations: {
+    //     startTime: '10s',
+    //     executor: 'shared-iterations',
+    //     vus: 1, // 1 user looping for 10 iterations
+    //     iterations: 10,
+    //   },
+    uploads: {
       executor: 'shared-iterations',
-      vus: 50, // 50 user looping for 1000 iterations
-      iterations: 1000,
+      vus: 1, // 1 user looping for 10 iterations
+      iterations: 10,
     },
   },
   thresholds: {
     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
   },
-  rps: 50, // don't increase this without consulting platform services first
+  rps: 50,
 };
+
+// export const options = {
+//   scenarios: {
+//     queries: {
+//       executor: 'ramping-vus',
+//       stages: [
+//         { duration: '5m', target: 60 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
+//         { duration: '10m', target: 60 }, // stay at 60 users for 10 minutes
+//         { duration: '3m', target: 100 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
+//         { duration: '2m', target: 100 }, // stay at 100 users for short amount of time (peak hour)
+//         { duration: '3m', target: 60 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
+//         { duration: '10m', target: 60 }, // continue at 60 for additional 10 minutes
+//         { duration: '2m', target: 0 }, // ramp-down to 0 users
+//       ],
+//     },
+//     mutations: {
+//       startTime: '35m',
+//       executor: 'shared-iterations',
+//       vus: 50, // 50 user looping for 1000 iterations
+//       iterations: 1000,
+//     },
+//     uploads: {
+//       executor: 'shared-iterations',
+//       vus: 1, // 50 user looping for 1000 iterations
+//       iterations: 100,
+//     },
+//   },
+//   thresholds: {
+//     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+//   },
+//   rps: 50, // don't increase this without consulting platform services first
+// };
 
 const numApplications = __ENV.NUM_APPLICATIONS;
 
@@ -183,7 +197,7 @@ export default function () {
             application {
               updatedAt
               status
-              ccbcNumber
+            ccbcNumber
               intakeId
             }
           }
@@ -211,6 +225,59 @@ export default function () {
           return !errors;
         },
       }
+    );
+  }
+
+  if (exec.scenario.name === 'uploads') {
+    const file = http.file(binFile, 'file.bin');
+
+    const createAttachmentPayload = JSON.stringify({
+      id: 'createAttachmentMutation',
+      query: `
+          mutation createAttachmentMutation($input: CreateAttachmentInput!) {
+            createAttachment(input: $input) {
+              attachment {
+                rowId
+                file
+              }
+            }
+          }`,
+      variables: {
+        input: {
+          attachment: {
+            file: null,
+            fileName: 'file.bin',
+            fileSize: '100MB',
+            fileType: 'test',
+            applicationId: 1,
+          },
+        },
+      },
+    });
+
+    const fd = new FormData();
+
+    fd.append('operations', createAttachmentPayload);
+
+    fd.append('map', '{ "0": ["variables.input.attachment.file"] }');
+    fd.append(0, file);
+
+    const postParams = {
+      cookies: {
+        'mocks.auth_role': 'ccbc_auth_user',
+        'mocks.mocked_timestamp': 1660924800,
+      },
+      headers: {
+        'Content-Type': `multipart/form-data; boundary=${fd.boundary}`,
+      },
+    };
+
+    http.post(
+      __ENV.APP_HOST + '/graphql',
+      fd.body(),
+      Object.assign({}, postParams, {
+        tags: { name: 'createAttachmentMutation' },
+      })
     );
   }
 }
