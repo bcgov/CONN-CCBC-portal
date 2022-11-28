a4a998a16423c4e3c5badc5fcedcf07bd8b3f2b3
Vladimir <vladimir@button.is>
2022-11-28 13:01:02 -0800

    feat: added computed column

--- db/deploy/tables/rfi_data.sql
+++ db/deploy/tables/rfi_data.sql
@@ -4,8 +4,8 @@ begin;
 
 create table ccbc_public.rfi_data(
   id integer primary key generated always as identity,
+  rfi_number varchar(1000),
   json_data jsonb not null default '{}'::jsonb,
-  last_edited_page varchar(100),
   rfi_data_status_type_id varchar(1000) references ccbc_public.rfi_data_status_type(name) default 'draft'
 );
 select ccbc_private.upsert_timestamp_columns('ccbc_public', 'rfi_data');
@@ -28,13 +28,13 @@ perform ccbc_private.upsert_policy('ccbc_analyst can always insert', 'rfi_data',
 end
 $grant$;
 
-comment on table ccbc_public.rfi_data is 'Table to hold applicant form data';
+comment on table ccbc_public.rfi_data is 'Table to hold RFI form data';
 
 comment on column ccbc_public.rfi_data.id is 'The unique id of the form data';
 
-comment on column ccbc_public.rfi_data.json_data is 'The data entered into the form by the respondent';
+comment on column ccbc_public.rfi_data.rfi_number is 'Reference number assigned to the RFI';
 
-comment on column ccbc_public.rfi_data.last_edited_page is 'Column saving the key of the last edited form page';
+comment on column ccbc_public.rfi_data.json_data is 'The data entered into the form by the respondent';
 
 comment on column ccbc_public.rfi_data.rfi_data_status_type_id is 'Column referencing the form data status type, defaults to draft';
 
