4fe630d77625d171a53a47ff4a003d5901eb97e4
Anthony Bushara <anthony@button.is>
2022-12-01 11:00:35 -0500

    feat: db tests

---
+++ db/test/unit/computed_columns/application_assessment_form_test.sql
@@ -0,0 +1,74 @@
+begin;
+select plan(3);
+
+truncate table
+  ccbc_public.application,
+  ccbc_public.application_status,
+  ccbc_public.attachment,
+  ccbc_public.form_data,
+  ccbc_public.application_form_data,
+  ccbc_public.intake
+restart identity;
+
+select has_function('ccbc_public', 'application_assessment_form',
+'Function application_assessment_form should exist');
+
+insert into ccbc_public.intake(open_timestamp, close_timestamp, ccbc_intake_number)
+values('2022-03-01 09:00:00-07', '2022-05-01 09:00:00-07', 1);
+
+select mocks.set_mocked_time_in_transaction('2022-04-01 09:00:00-07'::timestamptz);
+set jwt.claims.sub to 'testCcbcAuthUser';
+set role ccbc_auth_user;
+
+select ccbc_public.create_application();
+
+select ccbc_public.create_application();
+
+insert into ccbc_public.application_status
+ (application_id, status) values (1,'received'), (2, 'received');
+
+-- Set role to job_executor and put in test slug for assessment forms
+set role ccbc_job_executor;
+-- insert here to use for tests
+insert into ccbc_public.form (id, slug, form_type, json_schema) overriding system value
+ values (200, 'assessment', 'assessment', '{}'::jsonb) on conflict (id) do update set
+ json_schema=excluded.json_schema, slug=excluded.slug, form_type = excluded.form_type;
+
+-- set role to analyst and create assessment form
+set role ccbc_analyst;
+select ccbc_public.create_assessment_form('assessment'::varchar , '{}'::jsonb, 1);
+
+-- form_data id is '3' here because we have more than one application
+select results_eq(
+  $$
+    select id, json_data, form_schema_id from ccbc_public.application_assessment_form(
+      (select row(application.*)::ccbc_public.application
+       from ccbc_public.application where id=1), 'assessment'
+    );
+  $$,
+  $$
+    values(3, '{}'::jsonb, 200);
+  $$,
+  'Should return the newly created assessment form'
+);
+
+select ccbc_public.create_assessment_form('assessment'::varchar, '{"asdf":3}'::jsonb, 1);
+
+select results_eq(
+  $$
+    select id, json_data, form_schema_id from ccbc_public.application_assessment_form(
+      (select row(application.*)::ccbc_public.application
+       from ccbc_public.application where id=1), 'assessment'
+    );
+  $$,
+  $$
+    values(4, '{"asdf":3}'::jsonb, 200);
+  $$,
+  'Should return the newly created assessment form'
+);
+
+
+
+
+select finish();
+rollback;
