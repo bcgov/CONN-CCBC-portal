1cc2d6168a8c5907e814bcc5b4712309950fc8f5
Anthony Bushara <anthony@button.is>
2022-12-09 16:21:12 -0500

    test: add test for mutation

--- app/tests/pages/analyst/application/[applicationId]/rfi/[rfiId]/index.test.tsx
+++ app/tests/pages/analyst/application/[applicationId]/rfi/[rfiId]/index.test.tsx
@@ -2,9 +2,7 @@ import { screen } from '@testing-library/react';
 import RfiId from 'pages/analyst/application/[applicationId]/rfi/[rfiId]';
 import userEvent from '@testing-library/user-event';
 import PageTestingHelper from 'tests/utils/pageTestingHelper';
-import compiledhistoryQuery, {
-  historyQuery,
-} from '__generated__/historyQuery.graphql';
+import compiledRfiQuery, { RfiIdQuery } from '__generated__/RfiIdQuery.graphql';
 
 const mockQueryPayload = {
   Query() {
@@ -19,6 +17,9 @@ const mockQueryPayload = {
             jsonSchema: {},
           },
         },
+      },
+      rfiDataByRowId: {
+        jsonData: {},
       },
       session: {
         sub: '4e0ac88c-bf05-49ac-948f-7fd53c7a9fd6',
@@ -41,9 +42,46 @@ const mockQueryPayload = {
   },
 };
 
-const pageTestingHelper = new PageTestingHelper<historyQuery>({
+const mockPreFilledFormPayload = {
+  rfiDataByRowId: {
+    jsonData: {
+      detailedBudgetRfi: true,
+      eligibilityAndImpactsCalculatorRfi: true,
+    },
+  },
+  applicationByRowId: {
+    ccbcNumber: 'CCBC-10001',
+    organizationName: 'test org',
+    projectName: 'test project',
+    formData: {
+      jsonData: {},
+      formByFormSchemaId: {
+        jsonSchema: {},
+      },
+    },
+  },
+  session: {
+    sub: '4e0ac88c-bf05-49ac-948f-7fd53c7a9fd6',
+  },
+  allAnalysts: {
+    nodes: [
+      {
+        rowId: 1,
+        givenName: 'Test',
+        familyName: '1',
+      },
+      {
+        rowId: 2,
+        givenName: 'Test',
+        familyName: '2',
+      },
+    ],
+  },
+};
+
+const pageTestingHelper = new PageTestingHelper<RfiIdQuery>({
   pageComponent: RfiId,
-  compiledQuery: compiledhistoryQuery,
+  compiledQuery: compiledRfiQuery,
   defaultQueryResolver: mockQueryPayload,
   defaultQueryVariables: {
     rowId: 1,
@@ -206,6 +244,30 @@ describe('The index page', () => {
     pageTestingHelper.expectMutationToBeCalled('createRfiMutation', {
       input: {
         applicationRowId: 1,
+        jsonData: {
+          rfiAdditionalFiles: {},
+        },
+      },
+    });
+  });
+
+  it('calls the update mutation on save if exiting rfi present', async () => {
+    pageTestingHelper.loadQuery(mockPreFilledFormPayload);
+    pageTestingHelper.renderPage();
+    const user = userEvent.setup();
+    pageTestingHelper.setMockRouterValues({
+      query: { applicationId: '1', rfiId: '1' },
+    });
+
+    const button = screen.getByRole('button', {
+      name: 'Save',
+    });
+
+    await user.click(button);
+
+    pageTestingHelper.expectMutationToBeCalled('updateRfiMutation', {
+      input: {
+        rfiRowId: 1,
         jsonData: {
           rfiAdditionalFiles: {},
         },
