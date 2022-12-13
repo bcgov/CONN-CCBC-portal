f5ee8a68734b78d8c905ea2f26b351c7957ab1b2
Vladimir <vladimir@button.is>
2022-12-13 11:18:39 -0800

    fix: updated permissions

--- db/deploy/computed_columns/application_status@1.20.0.sql
+++ db/deploy/computed_columns/application_status@1.20.0.sql
@@ -14,7 +14,6 @@ grant execute on function ccbc_public.application_status to ccbc_auth_user;
 grant execute on function ccbc_public.application_status to ccbc_job_executor;
 grant execute on function ccbc_public.application_status to ccbc_analyst;
 grant execute on function ccbc_public.application_status to ccbc_admin;
-grant execute on function ccbc_public.application_status to public; -- needed for Metabase
 
 comment on function ccbc_public.application_status is 'Computed column to return status of an application';
 
