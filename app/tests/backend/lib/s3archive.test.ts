0335fef40cbbabe72bc42027c5a2af2fcfc5fa0d
Vladimir <vladimir@button.is>
2022-12-12 20:03:22 -0800

    chore: added test to verify archive

--- app/tests/backend/lib/s3archive.test.ts
+++ app/tests/backend/lib/s3archive.test.ts
@@ -1,17 +1,72 @@
 /**
  * @jest-environment node
  */
-import s3archive from 'backend/lib/s3archive';
 import { mocked } from 'jest-mock';
+import AdmZip from 'adm-zip'; 
 import request from 'supertest';
 import express from 'express';
 import session from 'express-session';
 import crypto from 'crypto';
+import { PassThrough } from 'stream'; 
+import s3archive from '../../../backend/lib/s3archive'; 
 import { performQuery } from '../../../backend/lib/graphql';
 import getAuthRole from '../../../utils/getAuthRole';
 
 jest.mock('../../../backend/lib/graphql');
 jest.mock('../../../utils/getAuthRole');
+
+jest.setTimeout(10000000);
+
+const mockStream = new PassThrough();
+
+const mockObjectTagging =  {
+  promise:jest.fn(() => {
+    return new Promise((resolve, reject) => {
+      return resolve({
+        TagSet:[{"Key":"av_status", "Value":"dirty"}],
+      });
+    });
+  }),
+  catch: jest.fn(),
+};
+
+jest.mock('../../../backend/lib/s3client',()=>{
+  return {
+    upload: jest.fn().mockReturnThis(), 
+    listObjects: jest.fn().mockReturnThis(),
+    getObject: ()=>{
+      return { 
+        createReadStream: () => {
+          mockStream.emit('data', 'hello world');
+          mockStream.end();
+          return mockStream;
+        },
+        promise:jest.fn(() => {
+          return new Promise((resolve, reject) => {
+            return resolve({
+              TagSet:[{"Key":"av_status", "Value":"dirty"}],
+            });
+          });
+        }),
+        catch: jest.fn(),
+      };
+    },
+    getObjectTagging: () => {
+      return mockObjectTagging;
+    }
+  }
+});
+
+const binaryParser = (res, callback) => {
+  res.setEncoding('binary');
+  res.data = '';
+  res.on('data', function (chunk) {
+      res.data += chunk;
+  });
+  res.on('end', function () {
+      callback(null, Buffer.from(res.data, 'binary'));
+  });
+}
 
 describe('The s3 archive', () => {
   let app;
@@ -67,6 +122,64 @@ describe('The s3 archive', () => {
     );
 
     expect(response.headers['content-type']).toBe('application/zip');
+  });
+
+  
+  it('should receive the archive with correct file', async () => {
+    mocked(getAuthRole).mockImplementation(() => {
+      return {
+        pgRole: 'ccbc_admin',
+        landingRoute: '/',
+      };
+    });
+
+    mocked(performQuery).mockImplementation(async () => {
+      return {
+        data: {
+          allApplications: {
+            nodes: [
+              {
+                formData: {
+                  jsonData: {
+                      templateUploads: {
+                          detailedBudget :[
+                              {
+                              uuid: 'd56a8477-b4d8-43c7-bd75-75d376ddeca4',
+                              name: 'File.pdf',
+                              size: 35,
+                              type: 'text/plain',
+                              },
+                          ]
+                      }
+                  },
+                },
+                ccbcNumber: 'CCBC-100001',
+              },
+            ],
+          },
+        },
+      };
+    });
+
+    const response = await request(app).get('/api/analyst/archive')
+      .set('Accept', 'application/zip')
+      .buffer()
+      .parse(binaryParser);
+    
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toBe('application/zip');
+
+    expect(Buffer.isBuffer(response.body)).toBeTrue;
+
+    var zip = new AdmZip(response.body);
+    
+    var zipEntries = zip.getEntries(); 
+    expect(zipEntries.length).toBe(1);
+
+    zipEntries.forEach(function (zipEntry) { 
+        expect(zipEntry.entryName.indexOf('Template 2')).not.toBe(-1); 
+    });
+    
   });
 
   jest.resetAllMocks();
