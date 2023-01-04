ab5f60b56777e9692c27cf168d2da4a61b8d6c6d
Anthony Bushara <anthony@button.is>
2023-01-04 17:53:51 -0500

    test: fix test with refactor

--- db/deploy/computed_columns/application_has_rfi_open.sql
+++ db/deploy/computed_columns/application_has_rfi_open.sql
@@ -10,6 +10,8 @@ $$
 $$ language sql stable;
 
 grant execute on function ccbc_public.application_has_rfi_open to ccbc_auth_user;
+grant execute on function ccbc_public.application_has_rfi_open to ccbc_admin;
+grant execute on function ccbc_public.application_has_rfi_open to ccbc_analyst;
 
 comment on function ccbc_public.application_has_rfi_open is 'Computed column to return whether the rfi is open';
 
---
+++ db/test/unit/computed_columns/application_has_rfi_open_test.sql
@@ -0,0 +1,83 @@
+begin;
+
+select plan(7);
+
+truncate table
+  ccbc_public.application,
+  ccbc_public.application_status,
+  ccbc_public.attachment,
+  ccbc_public.form_data,
+  ccbc_public.application_form_data,
+  ccbc_public.intake,
+  ccbc_public.application_analyst_lead
+restart identity cascade;
+
+select has_function(
+  'ccbc_public', 'application_has_rfi_open',
+  'Function application_has_rfi_open should exist'
+);
+
+insert into ccbc_public.intake(open_timestamp, close_timestamp, ccbc_intake_number)
+values('2022-03-01 09:00:00-07', '2022-05-01 09:00:00-07', 1);
+
+select mocks.set_mocked_time_in_transaction('2022-04-01 09:00:00-07'::timestamptz);
+
+set jwt.claims.sub to 'testCcbcAuthUser';
+set role ccbc_auth_user;
+
+select ccbc_public.create_application();
+
+insert into ccbc_public.application_status (application_id, status) VALUES (1,'received');
+
+set role ccbc_analyst;
+
+select results_eq (
+  $$
+  select ccbc_public.application_has_rfi_open(
+    (select row(application.*)::ccbc_public.application from ccbc_public.application)
+  )
+  $$,
+  $$
+    values('f'::boolean)
+  $$,
+  'No existing open rfi '
+);
+
+select ccbc_public.create_rfi(1, '{}'::jsonb);
+
+select results_eq (
+  $$
+  select ccbc_public.application_has_rfi_open(
+    (select row(application.*)::ccbc_public.application from ccbc_public.application)
+  )
+  $$,
+  $$
+    values('t'::boolean)
+  $$,
+  'The current rfi is open as it has no end date'
+);
+
+
+select function_privs_are(
+  'ccbc_public', 'application_has_rfi_open', ARRAY['ccbc_public.application'], 'ccbc_analyst', ARRAY['EXECUTE']::text[],
+  'ccbc_analyst can execute ccbc_public.application_has_rfi_open(ccbc_public.application)'
+);
+
+select function_privs_are(
+  'ccbc_public', 'application_has_rfi_open', ARRAY['ccbc_public.application'], 'ccbc_admin', ARRAY['EXECUTE']::text[],
+  'ccbc_analyst can execute ccbc_public.application_has_rfi_open(ccbc_public.application)'
+);
+
+select function_privs_are(
+  'ccbc_public', 'application_has_rfi_open', ARRAY['ccbc_public.application'], 'ccbc_guest', ARRAY[]::text[],
+  'ccbc_guest cannot execute ccbc_public.application_has_rfi_open(ccbc_public.application)'
+);
+
+select function_privs_are(
+  'ccbc_public', 'application_has_rfi_open', ARRAY['ccbc_public.application'], 'ccbc_auth_user', ARRAY['EXECUTE']::text[],
+  'ccbc_auth_user can execute ccbc_public.application_has_rfi_open(ccbc_public.application)'
+);
+
+select finish();
+
+rollback;
