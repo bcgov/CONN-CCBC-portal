890ed937cedaf0eecfb27cae51851ae9f10d5961
Vladimir <vladimir@button.is>
2023-01-24 14:23:10 -0800

    fix: v2 aws s3 client config

--- app/backend/lib/s3client.ts
+++ app/backend/lib/s3client.ts
@@ -6,6 +6,27 @@ import config from '../../config/index';
 import awsConfig from './awsCommon'
 
 const AWS_S3_BUCKET = config.get('AWS_S3_BUCKET');
+const AWS_S3_REGION = config.get('AWS_S3_REGION');
+const AWS_S3_KEY = config.get('AWS_S3_KEY');
+const AWS_S3_SECRET_KEY = config.get('AWS_S3_SECRET_KEY');
+const AWS_ROLE_ARN = config.get('AWS_ROLE_ARN');
+
+aws.config.update({
+  correctClockSkew: true,
+  credentials: new aws.ChainableTemporaryCredentials({
+    params: {
+      RoleArn: AWS_ROLE_ARN,
+      RoleSessionName: `s3-role-session-${Date.now()}`,
+      DurationSeconds: 15 * 60,
+    },
+    masterCredentials: new aws.Credentials({
+      accessKeyId: AWS_S3_KEY,
+      secretAccessKey: AWS_S3_SECRET_KEY,
+    }),
+  }),
+
+  region: AWS_S3_REGION,
+});
 
 const s3Client = new aws.S3();
 
