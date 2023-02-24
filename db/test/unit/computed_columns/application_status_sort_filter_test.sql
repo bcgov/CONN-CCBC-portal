4673f20fddddcf2e35396a2484a65534182cf7fa
marcellmueller <marcel@button.is>
2023-02-24 07:10:51 -0800

    fix: broken test due to history rebase

--- db/test/unit/computed_columns/application_status_sort_filter_test.sql
+++ db/test/unit/computed_columns/application_status_sort_filter_test.sql
@@ -36,8 +36,15 @@ select function_privs_are(
 insert into ccbc_public.intake(open_timestamp, close_timestamp, ccbc_intake_number)
 values('2022-03-01 09:00:00-07', '2022-05-01 09:00:00-07', 1);
 
+
+
 select mocks.set_mocked_time_in_transaction('2022-04-01 09:00:00-07'::timestamptz);
 set jwt.claims.sub to 'testCcbcAuthUser';
+
+insert into ccbc_public.ccbc_user
+  (given_name, family_name, email_address, session_sub) values
+  ('foo1', 'bar', 'foo1@bar.com', 'testCcbcAuthUser');
+
 set role ccbc_auth_user;
 
 select ccbc_public.create_application();
@@ -46,6 +53,11 @@ insert into ccbc_public.application_status
  (application_id, status) values (1,'received');
 
 set jwt.claims.sub to 'testCcbcAnalyst';
+
+insert into ccbc_public.ccbc_user
+  (given_name, family_name, email_address, session_sub) values
+  ('foo1', 'bar', 'foo1@bar.com', 'testCcbcAnalyst');
+
 set role ccbc_analyst;
 
 insert into ccbc_public.application_status
