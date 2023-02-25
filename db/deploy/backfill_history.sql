972a7ef641b2d2f0516c250cc2f33e0f2d32e182
Vladimir <vladimir@button.is>
2023-02-24 18:39:26 -0800

    fix: schema
    Signed-off-by: Vladimir <vladimir@button.is>

--- db/deploy/backfill_history.sql
+++ db/deploy/backfill_history.sql
@@ -6,13 +6,14 @@ $do$
 begin
     -- run only once
     if exists (select true from pg_proc where proname='import_once') then
-        select ccbc_public.import_applications();
-        select ccbc_public.import_attachments() ;
-        select ccbc_public.import_application_statuses();
-        select ccbc_public.import_assessment_data();
-        select ccbc_public.import_rfi_data();
+        perform ccbc_public.import_applications();
+        perform ccbc_public.import_attachments() ;
+        perform ccbc_public.import_application_statuses();
+        perform ccbc_public.import_assessment_data();
+        perform ccbc_public.import_rfi_data();
         drop function ccbc_public.import_once;
     end if;
+    
 end
 $do$;
 
--- db/deploy/import/import_once.sql
+++ db/deploy/import/import_once.sql
@@ -4,6 +4,7 @@ BEGIN;
 
 create or replace function ccbc_public.import_once() returns table(result_id int)
 as $function$
+begin
     return query select 0 as result_id; 
 end;
 $function$ language plpgsql volatile;
