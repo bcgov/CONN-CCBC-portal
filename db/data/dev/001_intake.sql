4943a61beff48bad35f3fb3446bde6a41958f381
Anthony Bushara <anthony@button.is>
2023-02-28 09:31:39 -0500

    chore(test): fix e2e screenshots

--- db/data/dev/001_intake.sql
+++ db/data/dev/001_intake.sql
@@ -16,7 +16,7 @@ truncate table
   ccbc_public.ccbc_user
 restart identity cascade;
 
-insert into ccbc_public.gapless_counter(id, counter) overriding sytstem value
+insert into ccbc_public.gapless_counter(id, counter) overriding system value
  values (1, 0), (2, 0);
 
 insert into
@@ -24,7 +24,7 @@ insert into
 overriding system value
 values
   (1, '2022-08-19 09:00:00 America/Vancouver','2022-11-06 09:00:00 America/Vancouver', 1, 1),
-  (2, '2023-01-15 00:00:00 America/Vancouver','2023-03-15 00:00:00 America/Vancouver', 2, 1)
+  (2, '2023-01-15 00:00:00 America/Vancouver','2023-03-15 00:00:00 America/Vancouver', 2, 2)
 on conflict (id) do update set
 open_timestamp = excluded.open_timestamp,
 close_timestamp = excluded.close_timestamp,
