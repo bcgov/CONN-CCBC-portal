8a5d2fb523515711daf338818c95fb0b1e7b90b8
marcellmueller <marcel@button.is>
2023-03-14 11:47:07 -0700

    refactor: move bc and ised headers to separate files

---
+++ app/components/Analyst/Tracking/ConditionalApproval/index.ts
@@ -0,0 +1,2 @@
+export { default as BCHeader } from './BCHeader';
+export { default as ISEDHeader } from './ISEDHeader';
--- app/formSchema/uiSchema/analyst/conditionalApprovalUiSchema.tsx
+++ app/formSchema/uiSchema/analyst/conditionalApprovalUiSchema.tsx
@@ -1,17 +1,7 @@
-import styled from 'styled-components';
-import Image from 'next/image';
-
-const StyledFlex = styled.div`
-  display: flex;
-  align-items: center;
-  color: #9b9b9b;
-  font-size: 16px;
-  margin: 0;
-
-  & svg {
-    margin-right: 8px;
-  }
-`;
+import {
+  BCHeader,
+  ISEDHeader,
+} from 'components/Analyst/Tracking/ConditionalApproval';
 
 const assessmentsUiSchema = {
   'ui:order': [
@@ -33,17 +23,7 @@ const assessmentsUiSchema = {
       flexDirection: 'column',
       maxWidth: '220px',
     },
-    'ui:before': (
-      <StyledFlex>
-        <Image
-          width={24}
-          height={24}
-          src="/icons/bcid-favicon-32x32.png"
-          alt="bc-gov-logo"
-        />
-        <span style={{ marginLeft: '8px' }}>BC</span>
-      </StyledFlex>
-    ),
+    'ui:before': <BCHeader />,
     ministerDecision: {
       'ui:widget': 'SelectWidget',
       'ui:placeholder': 'No decision',
@@ -63,22 +43,7 @@ const assessmentsUiSchema = {
       maxWidth: '220px',
       divider: true,
     },
-    'ui:before': (
-      <StyledFlex>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="-2015 -2000 4030 4030"
-        >
-          <path
-            fill="#f00"
-            d="m-90 2030 45-863a95 95 0 0 0-111-98l-859 151 116-320a65 65 0 0 0-20-73l-941-762 212-99a65 65 0 0 0 34-79l-186-572 542 115a65 65 0 0 0 73-38l105-247 423 454a65 65 0 0 0 111-57l-204-1052 327 189a65 65 0 0 0 91-27l332-652 332 652a65 65 0 0 0 91 27l327-189-204 1052a65 65 0 0 0 111 57l423-454 105 247a65 65 0 0 0 73 38l542-115-186 572a65 65 0 0 0 34 79l212 99-941 762a65 65 0 0 0-20 73l116 320-859-151a95 95 0 0 0-111 98l45 863z"
-          />
-        </svg>
-        <span>ISED</span>
-      </StyledFlex>
-    ),
+    'ui:before': <ISEDHeader />,
     isedDecision: {
       'ui:widget': 'SelectWidget',
       'ui:placeholder': 'No decision',
