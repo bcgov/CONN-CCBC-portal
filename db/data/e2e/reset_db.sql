30ddc948d317d14cdd0a98d44d431da4c4af993e
Anthony Bushara <anthony@button.is>
2023-03-20 12:39:10 -0400

    chore: add removing analysts in reset_db

--- db/data/e2e/reset_db.sql
+++ db/data/e2e/reset_db.sql
@@ -6,5 +6,6 @@ delete from ccbc_public.attachment;
 delete from ccbc_public.application_status;
 delete from ccbc_public.application;
 delete from ccbc_public.intake;
+delete from ccbc_public.analyst;
 
 commit;
