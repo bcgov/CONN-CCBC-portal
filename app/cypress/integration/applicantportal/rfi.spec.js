3a9b74304214e2a2fddedf1c2cf721746aae595a
Anthony Bushara <anthony@button.is>
2023-03-22 14:42:23 -0400

    chore: remove non-working RFI e2e tests

--- app/cypress/integration/applicantportal/rfi.spec.js
+++ app/cypress/integration/applicantportal/rfi.spec.js
@@ -1,5 +1,3 @@
-/// <reference types="Cypress" />
-
 describe('Rfi on Applicant side', () => {
   beforeEach(() => {
     cy.mockLogin('ccbc_auth_user');
@@ -11,7 +9,6 @@ describe('Rfi on Applicant side', () => {
     cy.sqlFixture('e2e/001_application');
     cy.sqlFixture('e2e/001_application_received');
     cy.sqlFixture('e2e/001_rfi');
-    cy.intercept('POST', '/graphql').as('graphql');
   });
 
   it('Applicant side RFI', () => {
@@ -19,24 +16,5 @@ describe('Rfi on Applicant side', () => {
     // check if the text rendered for the due date is there
     cy.contains('p', '2023-03-23').should('be.visible');
     cy.get('body').happoScreenshot({ component: 'RFI Upload Page Blank' });
-    cy.get('[data-testid=file-test]')
-      .first()
-      .selectFile('cypress/fixtures/doc.txt', { force: true });
-    // Wait for upload to finish
-    cy.contains('button', 'doc.txt').should('be.visible');
-    cy.contains('button', /^Save$/).click();
-    cy.url().should('include', '/applicantportal/form/1/rfi/2');
-    // Ensure the page has rendered after refreshing
-    cy.contains('p', '2023-03-23').should('be.visible');
-    cy.get('body').happoScreenshot({
-      component: 'RFI Upload Page Field Filled',
-    });
-    cy.mockLogin('ccbc_analyst');
-    cy.visit('analyst/application/1/rfi');
-    // Wait for page to render fully and expect to find RFI file
-    cy.contains('button', 'doc.txt').should('be.visible');
-    cy.get('body').happoScreenshot({
-      component: 'RFI Analyst Side After Applicant Upload',
-    });
   });
 });
