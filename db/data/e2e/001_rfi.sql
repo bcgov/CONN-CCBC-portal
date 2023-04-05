abd017e879f1768ffc50d254b16bb4a9b2ae5f9a
Anthony Bushara <anthony@button.is>
2023-04-05 11:57:54 -0400

    fix(test): use mocks for creating rfi

--- db/data/e2e/001_rfi.sql
+++ db/data/e2e/001_rfi.sql
@@ -5,6 +5,8 @@ $$
 declare _application_id int;
 begin
 
+perform mocks.set_mocked_time_in_transaction('2022-10-09 09:00:00-07'::timestamptz);
+
 select id into _application_id from ccbc_public.application limit 1;
 
 perform ccbc_public.create_rfi(_application_id, '{
