c706483d74c97b271cd120c478586a87dcd784e6
Anthony Bushara <anthony@button.is>
2023-04-06 16:59:34 -0400

    chore: add constraint for visibility

--- db/deploy/tables/application_status_type_004_analyst_visible.sql
+++ db/deploy/tables/application_status_type_004_analyst_visible.sql
@@ -4,6 +4,9 @@ begin;
 
 alter table ccbc_public.application_status_type add column visible_by_analyst boolean default true;
 
+alter table ccbc_public.application_status_type add constraint no_invisible_status check (visible_by_analyst or visible_by_applicant);
+
 update ccbc_public.application_status_type set visible_by_analyst = false where name = 'draft';
+update ccbc_public.application_status_type set visible_by_analyst = false where name = 'submitted';
 
 commit;
