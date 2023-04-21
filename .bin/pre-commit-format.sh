f5d4c441e22fb0c975c5fb23f14fa44068c61516
marcellmueller <marcel@button.is>
2023-04-21 09:25:43 -0700

    fix: add prettierignore path to pre-commit command

--- .bin/pre-commit-format.sh
+++ .bin/pre-commit-format.sh
@@ -7,4 +7,4 @@ files=("$@")
 files=("${files[@]/#/../}") # add ../ to each element
 
 # --ignore-unknown prevents prettier from complaining about file types it doesn't know about
-yarn run prettier --ignore-unknown --write "${files[@]}"
+yarn run prettier --ignore-unknown --ignore-path ../.prettierignore --write "${files[@]}"
