edff1bbd5ce776acdf0f7883b012e40463745209
marcellmueller <marcel@button.is>
2023-04-27 14:56:05 -0700

    feat: update announcements schema

--- app/formSchema/analyst/announcements.ts
+++ app/formSchema/analyst/announcements.ts
@@ -4,42 +4,49 @@ const announcements: JSONSchema7 = {
   description: '',
   type: 'object',
   properties: {
-    primary: {
-      type: 'array',
-      items: {
-        type: 'object',
-        properties: {
-          announcementUrl: {
-            type: 'string',
-            title: 'Announcement URL',
-          },
-          announcementDate: {
-            type: 'string',
-            title: 'Announcement date',
-          },
-          otherProjectsInAnnouncement: {
-            type: 'string',
-            title: 'Other projects in announcement',
+    announcements: {
+      type: 'object',
+      properties: {
+        primary: {
+          title: 'Primary news release',
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              announcementUrl: {
+                type: 'string',
+                title: 'Announcement URL',
+              },
+              announcementDate: {
+                type: 'string',
+                title: 'Announcement date',
+              },
+              otherProjectsInAnnouncement: {
+                type: 'string',
+                title: 'Other projects in announcement',
+              },
+            },
           },
         },
-      },
-    },
-    secondary: {
-      type: 'array',
-      items: {
-        type: 'object',
-        properties: {
-          announcementUrl: {
-            type: 'string',
-            title: 'Announcement URL',
-          },
-          announcementDate: {
-            type: 'string',
-            title: 'Announcement date',
-          },
-          otherProjectsInAnnouncement: {
-            type: 'string',
-            title: 'Other projects in announcement',
+        secondary: {
+          title: 'Secondary news releases',
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              announcementUrl: {
+                type: 'string',
+                title: 'Announcement URL',
+              },
+              announcementDate: {
+                type: 'string',
+                title: 'Announcement date',
+              },
+              otherProjectsInAnnouncement: {
+                type: 'string',
+                title: 'Other projects in announcement',
+              },
+            },
           },
         },
       },
