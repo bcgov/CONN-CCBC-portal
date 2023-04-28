31b2e5314923696ce317a14becaefa5ad8cf0cd8
Anthony Bushara <anthony@button.is>
2023-04-28 17:19:31 -0400

    feat: use CTE and make gis_data_counts function stable

--- db/deploy/computed_columns/gis_data_counts.sql
+++ db/deploy/computed_columns/gis_data_counts.sql
@@ -3,64 +3,46 @@
 begin;
 
 create or replace function ccbc_public.gis_data_counts(batchId int)
-returns setof ccbc_public.gis_data_count as 
+returns setof ccbc_public.gis_data_count  as
 $$
-    declare
-        application_row_id int;
-        ccbc_id text;
-        json_row jsonb;
-        json_file jsonb; 
-    begin
-    --  Create a temporary table
-    CREATE TEMPORARY TABLE if not exists temp_application_gis
-    (
-        batch_id integer,
-        application_id integer,
-        json_data jsonb not null default '{}'::jsonb
-    ) 
-    ON COMMIT DROP;
+declare
+    json_file jsonb;
+begin
+-- load JSON data
+select json_data into json_file  from ccbc_public.gis_data where id = batchId;
 
-    delete from temp_application_gis;
-
-     -- load JSON data
-    select json_data into json_file  from ccbc_public.gis_data where id = batchId;
-
-    -- Loop over each object in the JSON array
-    for json_row in select * from jsonb_array_elements(json_file)
-    loop
-        ccbc_id := json_row->>'ccbc_number'; 
-
-        -- Look up the application_id based on the ccbc_number value
-        select id into application_row_id from ccbc_public.application where ccbc_number = ccbc_id; 
-        -- If no row was found, add to list 
-        if found then
-            insert into temp_application_gis (batch_id, application_id, json_data) 
-            select batchId, application_row_id, json_row
-            WHERE
-              NOT EXISTS (
-                SELECT id FROM ccbc_public.application_gis_data t 
-                WHERE t.application_id = application_row_id and t.json_data = json_row
-              );
-
-        end if;
-    end loop;
-
-    return query select count(*)::int as total, 'new' as data_type,string_agg(ccbc_number, ', ') AS ccbc_numbers 
-        from temp_application_gis agd 
-        inner join ccbc_public.application app on agd.application_id=app.id 
-    where agd.batch_id=batchId and agd.application_id not in
+return query
+    with json_rows as (
+        select json_row
+        from jsonb_array_elements(json_file) as json_row
+    ),
+    application_gis_data as (
+        select
+            (json_row->>'ccbc_number') as ccbc_number,
+            a.id as application_id,
+            json_row as json_data
+        from json_rows, ccbc_public.application a
+        where a.ccbc_number = (json_row->>'ccbc_number')
+        and not exists (
+            select 1
+            from ccbc_public.application_gis_data agd
+            where agd.application_id = a.id and agd.json_data = json_row
+        )
+    )
+    select count(*)::int as total, 'new' as data_type, string_agg(ccbc_number, ', ') AS ccbc_numbers
+    from application_gis_data agd
+    where agd.application_id not in
         (select application_id from ccbc_public.application_gis_data group by application_id)
     union all
-    select count(*)::int as total, 'updated' as data_type,string_agg(ccbc_number, ', ') AS ccbc_numbers   
-        from temp_application_gis agd
-        inner join ccbc_public.application app on agd.application_id=app.id 
-    where agd.batch_id=batchId and agd.application_id in
+    select count(*)::int as total, 'updated' as data_type, string_agg(ccbc_number, ', ') AS ccbc_numbers
+    from application_gis_data agd
+    where agd.application_id in
         (select application_id from ccbc_public.application_gis_data group by application_id)
     union all
-        select  jsonb_array_length(json_data) as total, 'total' as data_type,null as ccbc_numbers 
-        from ccbc_public.gis_data where id=batchId;
-    end;
-$$ language plpgsql volatile;
+    select jsonb_array_length(json_data) as total, 'total' as data_type, null as ccbc_numbers
+    from ccbc_public.gis_data where id=batchId;
+end;
+$$ language plpgsql stable;
 
 grant execute on function ccbc_public.gis_data_counts to ccbc_analyst;
 grant execute on function ccbc_public.gis_data_counts to ccbc_admin;
