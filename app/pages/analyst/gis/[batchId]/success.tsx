9ff295b3767681e19a5aac1cfeac5fb1cce349a2
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2023-05-09 13:58:20 -0700

    feat: gis analysis import success summary

--- app/pages/analyst/gis/[batchId]/success.tsx
+++ app/pages/analyst/gis/[batchId]/success.tsx
@@ -6,8 +6,12 @@ import { ButtonLink, Layout } from 'components';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import ToggleDropdown from 'components/ToggleDropdown';
 
-const StyledContainer = styled.div``;
+const StyledContainer = styled.div`
+  margin-bottom: 5px;
+  margin-top: 5px;
+`;
 
 const getGisUploadSuccessQuery = graphql`
   query successGisUploadQuery($batchId: Int!) {
@@ -32,15 +36,48 @@ const BatchIdPage: React.FC<
     getGisUploadSuccessQuery,
     preloadedQuery
   );
-
   return (
     <Layout session={session} title="Connecting Communities BC">
       <StyledContainer>
-        <p>
-          <FontAwesomeIcon icon={faCircleCheck} color="#2E8540" /> GIS
-          information successfully added to {gisDataCounts.nodes[2].total}{' '}
-          projects
-        </p>
+        <h1>GIS Analysis Import</h1>
+        <StyledContainer>
+          <FontAwesomeIcon icon={faCircleCheck} color="#2E8540" /> GIS analysis
+          added to {gisDataCounts.nodes[0].total} projects for the first time
+        </StyledContainer>
+        <ToggleDropdown
+          items={gisDataCounts.nodes[0]?.ccbcNumbers?.split(',') || []}
+          hideText="Hide Projects"
+          showText="View Projects"
+        />
+        <StyledContainer>
+          <FontAwesomeIcon icon={faCircleCheck} color="#2E8540" /> GIS analysis
+          updated for {gisDataCounts.nodes[1].total} projects
+        </StyledContainer>
+        <ToggleDropdown
+          items={gisDataCounts.nodes[1]?.ccbcNumbers?.split(',') || []}
+          hideText="Hide Projects"
+          showText="View Projects"
+        />
+        <StyledContainer>
+          GIS analysis unchanged for {gisDataCounts.nodes[3].total} projects and
+          was not updated
+        </StyledContainer>
+        <ToggleDropdown
+          items={gisDataCounts.nodes[3]?.ccbcNumbers?.split(',') || []}
+          hideText="Hide Projects"
+          showText="View Projects"
+        />
+        <StyledContainer>
+          GIS analysis unmatched for {gisDataCounts.nodes[2].total} projects
+        </StyledContainer>
+        <ToggleDropdown
+          items={gisDataCounts.nodes[2]?.ccbcNumbers?.split(',') || []}
+          hideText="Hide Projects"
+          showText="View Projects"
+        />
+        <StyledContainer>
+          Total processed {gisDataCounts.nodes[4].total}
+        </StyledContainer>
         <ButtonLink href="/analyst/dashboard">Return to dashboard</ButtonLink>
       </StyledContainer>
     </Layout>
