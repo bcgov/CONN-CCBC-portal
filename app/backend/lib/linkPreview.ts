b9e5615cbcd2785a44adb6cb2587bf8277d35626
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2023-05-23 15:56:57 -0700

    fix: add return, add rate limiter

--- app/backend/lib/linkPreview.ts
+++ app/backend/lib/linkPreview.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express';
+import RateLimit from 'express-rate-limit';
 import getLinkPreview from '../../utils/getLinkPreview';
 import getAuthRole from '../../utils/getAuthRole';
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 100,
+});
 
 const allowedHostnames = [
   'news.gov.bc.ca',
@@ -12,36 +18,34 @@ const allowedHostnames = [
 
 const linkPreview = Router();
 
-linkPreview.post('/api/announcement/linkPreview', async (req, res) => {
+linkPreview.post('/api/announcement/linkPreview', limiter, async (req, res) => {
   const authRole = getAuthRole(req);
   const isRoleAuthorized = authRole?.pgRole === 'ccbc_admin';
   if (!isRoleAuthorized) {
-    res.status(404).end();
-  } else {
-    const { url } = req.body;
-    try {
-      const urlObj = new URL(url);
-      if (!allowedHostnames.includes(urlObj.hostname)) {
-        res.json({
-          title: null,
-          description: 'No preview available',
-          image: '/images/noPreview.png',
-        });
-      } else {
-        const preview = await getLinkPreview(
-          `https://${urlObj.hostname}${urlObj.pathname}`
-        ).catch((e) => {
-          throw new Error(e);
-        });
-        res.json(preview);
-      }
-    } catch (e) {
-      res.json({
+    return res.status(404).end();
+  }
+  const { url } = req.body;
+  try {
+    const urlObj = new URL(url);
+    if (!allowedHostnames.includes(urlObj.hostname)) {
+      return res.json({
         title: null,
         description: 'No preview available',
         image: '/images/noPreview.png',
       });
     }
+    const preview = await getLinkPreview(
+      `https://${urlObj.hostname}${urlObj.pathname}`
+    ).catch((e) => {
+      throw new Error(e);
+    });
+    return res.json(preview);
+  } catch (e) {
+    return res.json({
+      title: null,
+      description: 'No preview available',
+      image: '/images/noPreview.png',
+    });
   }
 });
 
