d08a2e61d89b40ee3bb9795695f778319d124350
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2023-05-23 14:22:54 -0700

    test: add linkPreview backend tests

---
+++ app/tests/backend/lib/linkPreview.test.ts
@@ -0,0 +1,207 @@
+/**
+ * @jest-environment node
+ */
+import { mocked } from 'jest-mock';
+import request from 'supertest';
+import express from 'express';
+import session from 'express-session';
+import crypto from 'crypto';
+import bodyParser from 'body-parser';
+import linkPreview from '../../../backend/lib/linkPreview';
+import getAuthRole from '../../../utils/getAuthRole';
+
+jest.mock('../../../backend/lib/graphql');
+jest.mock('../../../utils/getAuthRole');
+
+function FormDataMock() {
+  this.append = jest.fn();
+}
+
+global.FormData = jest.fn(() => {
+  return FormDataMock();
+}) as jest.Mock;
+
+jest.setTimeout(10000000);
+
+describe('The Link Preview', () => {
+  let app;
+
+  beforeEach(async () => {
+    app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(session({ secret: crypto.randomUUID(), cookie: { secure: true } }));
+    app.use('/', linkPreview);
+  });
+
+  it('should receive the correct response for unauthorized user', async () => {
+    mocked(getAuthRole).mockImplementation(() => {
+      return {
+        pgRole: 'ccbc_guest',
+        landingRoute: '/',
+      };
+    });
+
+    const response = await request(app).post('/api/announcement/linkPreview');
+    expect(response.status).toBe(404);
+  });
+
+  it('should return preview for allowed hostname with gov image', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        text: () =>
+          Promise.resolve(`
+        <html>
+          <head>
+            <title>Test Title</title>
+            <meta name="twitter:description" content="Test Description">
+            <meta name="twitter:image" content="https://news.gov.bc.ca/testImage">
+            <meta name="description" content="Test Description">
+            <meta property="og:image" content="https://live.staticflickr.com/nonGovImage.jpg">
+          </head>
+          <body>
+          </body>
+        </html>`),
+      })
+    ) as jest.Mock;
+
+    mocked(getAuthRole).mockImplementation(() => {
+      return {
+        pgRole: 'ccbc_admin',
+        landingRoute: '/',
+      };
+    });
+
+    const response = await request(app)
+      .post('/api/announcement/linkPreview')
+      .set('Content-Type', 'application/json')
+      .set('Connection', 'keep-alive')
+      .send({
+        url: 'https://news.gov.bc.ca/test-news',
+      })
+      .expect(200);
+    expect(response.body.title).toBe('Test Title');
+    expect(response.body.description).toBe('Test Description');
+    expect(response.body.image).toBe('https://news.gov.bc.ca/testImage');
+    expect(response.status).toBe(200);
+  });
+  it('should return preview for allowed hostname with non-gov image', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        text: () =>
+          Promise.resolve(`
+        <html>
+          <head>
+            <title>Test Title</title>
+            <meta name="twitter:description" content="Test Description">
+            <meta name="description" content="Test Description">
+            <meta property="og:image" content="https://live.staticflickr.com/nonGovImage.jpg">
+          </head>
+          <body>
+          </body>
+        </html>`),
+      })
+    ) as jest.Mock;
+
+    mocked(getAuthRole).mockImplementation(() => {
+      return {
+        pgRole: 'ccbc_admin',
+        landingRoute: '/',
+      };
+    });
+
+    const response = await request(app)
+      .post('/api/announcement/linkPreview')
+      .set('Content-Type', 'application/json')
+      .set('Connection', 'keep-alive')
+      .send({
+        url: 'https://news.gov.bc.ca/test-news',
+      })
+      .expect(200);
+    expect(response.body.title).toBe('Test Title');
+    expect(response.body.description).toBe('Test Description');
+    expect(response.body.image).toBe(
+      'https://live.staticflickr.com/nonGovImage.jpg'
+    );
+    expect(response.status).toBe(200);
+  });
+  it('should return preview for allowed hostname with no image', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        text: () =>
+          Promise.resolve(`
+        <html>
+          <head>
+            <title>Test Title</title>
+            <meta name="twitter:description" content="Test Description">
+            <meta name="description" content="Test Description">
+          </head>
+          <body>
+          </body>
+        </html>`),
+      })
+    ) as jest.Mock;
+
+    mocked(getAuthRole).mockImplementation(() => {
+      return {
+        pgRole: 'ccbc_admin',
+        landingRoute: '/',
+      };
+    });
+
+    const response = await request(app)
+      .post('/api/announcement/linkPreview')
+      .set('Content-Type', 'application/json')
+      .set('Connection', 'keep-alive')
+      .send({
+        url: 'https://news.gov.bc.ca/test-news',
+      })
+      .expect(200);
+    expect(response.body.title).toBe('Test Title');
+    expect(response.body.description).toBe('Test Description');
+    expect(response.body.image).toBe('/images/noPreview.png');
+    expect(response.status).toBe(200);
+  });
+  it('should return no preview for non allowed hostname', async () => {
+    mocked(getAuthRole).mockImplementation(() => {
+      return {
+        pgRole: 'ccbc_admin',
+        landingRoute: '/',
+      };
+    });
+
+    const response = await request(app)
+      .post('/api/announcement/linkPreview')
+      .set('Content-Type', 'application/json')
+      .set('Connection', 'keep-alive')
+      .send({
+        url: 'https://a-news-outlet.ca/test-news',
+      })
+      .expect(200);
+    expect(response.body.title).toBe(null);
+    expect(response.body.description).toBe('No preview available');
+    expect(response.body.image).toBe('/images/noPreview.png');
+    expect(response.status).toBe(200);
+  });
+  it('should return no preview for invalid URL', async () => {
+    mocked(getAuthRole).mockImplementation(() => {
+      return {
+        pgRole: 'ccbc_admin',
+        landingRoute: '/',
+      };
+    });
+
+    const response = await request(app)
+      .post('/api/announcement/linkPreview')
+      .set('Content-Type', 'application/json')
+      .set('Connection', 'keep-alive')
+      .send({
+        url: 'i-am-not-a-url',
+      })
+      .expect(200);
+    expect(response.body.title).toBe(null);
+    expect(response.body.description).toBe('No preview available');
+    expect(response.body.image).toBe('/images/noPreview.png');
+    expect(response.status).toBe(200);
+  });
+});
