c3e5af1ad348ba565fc5c914143aacaca8e9ca81
Vladimir <vladimir@button.is>
2023-05-25 18:25:21 -0700

    feat: added tab2 and tests
    Signed-off-by: Vladimir <vladimir@button.is>

--- app/backend/lib/sow_import/tab_2.ts
+++ app/backend/lib/sow_import/tab_2.ts
@@ -2,31 +2,71 @@ import * as XLSX from 'xlsx';
 import { performQuery } from '../graphql';
 
 const createSomeMutation = `
-  mutation tab2Mutation($input: CreateSowTab2Input!) {
-    createSowTab2(
-      input: 
-      {
-        sowTab2: $input
-      }
-    )
+  mutation tab2Mutation($input: SowTab2Input!) {
+    createSowTab2(input: {sowTab2: $input}) {
+      clientMutationId
+    }
   }
 `;
 
-const readData = async(sow_id, wb, sheet_name) => {
+const readData = async(wb, sheet_name) => {
   const sheet = XLSX.utils.sheet_to_json(wb.Sheets[sheet_name], { header: "A" });
-  for (let row = 1; row < sheet.length; row ++) {
-    // place logic here
-  }
   const result = [];
-
+  let tableDetected = false;
+  for (let row = 1; row < sheet.length; row ++) {
+    let validLine = false;
+      const suspect = sheet[row]['A'];
+      let value;
+      if (suspect === undefined) continue;
+      if (typeof(suspect) !== 'string') { 
+        value = suspect.toString();
+      }
+      else {
+        value = suspect;
+      }
+      // if (typeof(value) !== 'string') continue;
+      if (value.indexOf('Entry Number')>-1) {
+          tableDetected=true;
+          continue;
+      }  
+      if (tableDetected) { 
+          validLine = sheet[row]['O'] && typeof(sheet[row]['O']) === 'string'; 
+          value = sheet[row]['O'];
+          // now reading line
+          if (validLine && (value === 'Complete')) {
+            const lineData = 
+            {  
+              entryNumber: sheet[row]['A'], 
+              projectSiteName: sheet[row]['B'], 
+              projectSiteIdentifier: sheet[row]['C'], 
+              projectSiteType: sheet[row]['D'], 
+              latitude: sheet[row]['E'], 
+              longitude: sheet[row]['F'], 
+              newOrExisting: sheet[row]['G'], 
+              isSitePop: sheet[row]['H'], 
+              isSiteGateway: sheet[row]['I'], 
+              landAccessType: sheet[row]['J'], 
+              description: sheet[row]['K'], 
+              milestone1: sheet[row]['L'], 
+              milestone2: sheet[row]['M'], 
+              milestone3: sheet[row]['N']
+            }  
+            result.push(lineData);
+          }
+      }
+  }
   return result;
 }
 
 const LoadTab2Data = async(sow_id, wb, sheet_name, req) => {
-  const data = await readData(sow_id, wb, sheet_name);
-   
+  const data = await readData(wb, sheet_name);
+  
+  if (data.length === 0) {
+    return { error: 'no data found for Tab 2'};
+  }
   // time to persist in DB
-  const result = await performQuery(createSomeMutation, {input: data}, req)
+  const input = {input: {sowId: sow_id, jsonData: data}};
+  const result = await performQuery(createSomeMutation, input, req)
   .catch((e) => {
     return { error: e };
   });
---
+++ app/tests/backend/lib/sow_tab_2.test.ts
@@ -0,0 +1,208 @@
+/**
+ * @jest-environment node
+ */
+import { mocked } from 'jest-mock';
+import request from 'supertest';
+import * as XLSX from 'xlsx'; 
+import { performQuery } from '../../../backend/lib/graphql';
+import LoadTab2Data from '../../../backend/lib/sow_import/tab_2'
+
+jest.mock('../../../backend/lib/graphql');
+
+describe('sow_tab_2', () => {
+  beforeEach(() => {
+    mocked(performQuery).mockImplementation(async () => {
+      return {}
+    });
+    jest.spyOn(XLSX, 'read').mockReturnValue({
+      Sheets: { Sheet1:{ } },
+      SheetNames: ["2"]
+    });
+  })
+  it('should parse worksheet and return error if no data found', async ()  => { 
+    jest.spyOn(XLSX.utils, 'sheet_to_json').mockReturnValue([
+      { a: 1, b: 2, c: 3 }
+    ]);
+    const wb = XLSX.read(null); 
+    // This call will use XLSX mocks
+    const data = await LoadTab2Data(1,wb,'2', request);
+    console.log(data);  
+    expect(data).toEqual({error:'no data found for Tab 2'});
+  });
+  
+  it('should parse worksheet and submit expected mutation', async ()  => { 
+    const fakeTab2 = [
+      { AG: 122 },
+      { A: 'Step 2. Project Sites' },
+      { AD: 'Total', AF: 122 },
+      {
+        A: 'Entry Number',
+        B: 'Project Site Name',
+        C: 'Project Site Identifier',
+        D: 'Project Site Type',
+        E: 'Project Site Location ',
+        G: 'New or Existing Site',
+        H: 'Is the Site a POP?',
+        I: 'Is the Site an Internet Gateway  External to the New Network?',
+        J: 'Land Access Type',
+        K: 'Short Description and Comments',
+        L: 'Milestone Deliverable Completion Dates',
+        O: 'Information Complete',
+        P: 'Message Center'
+      },
+      {
+        E: 'Latitude',
+        F: 'Longitude',
+        L: 'Milestone 1',
+        M: 'Milestone 2',
+        N: 'Milestone 3',
+        S: 'Invalid Data. ',
+        T: 'Duplicate Site ID. ',
+        U: 'Missing Info. ',
+        V: 'Invalid MS Dates. ',
+        W: 'At least one cell has to be filled-in. ',
+        X: 'Missing App Num. ',
+        Y: '?',
+        Z: 'Missing Equipment for Site. '
+      },
+      {
+        A: 1,
+        B: 'Site A1',
+        C: 'A1',
+        D: 'Water Tower',
+        E: 40,
+        F: -145,
+        G: 'New',
+        H: 'No',
+        I: 'No',
+        J: 'Owned',
+        K: 'Edge, Interconnected, Linked',
+        L: 45199,
+        M: 45382,
+        N: 45657,
+        O: 'Complete',
+        P: '',
+        S: 0,
+        T: true,
+        U: true,
+        V: true,
+        W: true,
+        Z: true
+      },
+      {
+        A: 2,
+        B: 'Site A2',
+        C: 'A2',
+        D: 'Water Tower',
+        E: 40,
+        F: -145,
+        G: 'Existing',
+        H: 'No',
+        I: 'No',
+        J: 'Owned',
+        K: 'Edge, Interconnected, Linked',
+        L: 45199,
+        M: 45382,
+        N: 45657,
+        O: 'Complete',
+        P: '',
+        S: 0,
+        T: true,
+        U: true,
+        V: true,
+        W: true,
+        Z: true
+      },
+      {
+        A: 3,
+        B: 'Site A3',
+        C: 'A3',
+        D: 'Water Tower',
+        E: 40,
+        F: -145,
+        G: 'New',
+        H: 'No',
+        I: 'No',
+        J: 'Owned',
+        K: 'Edge, Interconnected, Linked',
+        L: 45199,
+        M: 45382,
+        N: 45657,
+        O: 'Complete',
+        P: '',
+        S: 0,
+        T: true,
+        U: true,
+        V: true,
+        W: true,
+        Z: true
+      }
+    ];
+
+    const expectedInput ={
+      input: {
+        jsonData: [
+          {
+            entryNumber: 1,
+            projectSiteName: 'Site A1',
+            projectSiteIdentifier: 'A1',
+            projectSiteType: 'Water Tower',
+            latitude: 40,
+            longitude: -145,
+            newOrExisting: 'New',
+            isSitePop: 'No',
+            isSiteGateway: 'No',
+            landAccessType: 'Owned',
+            description: 'Edge, Interconnected, Linked',
+            milestone1: 45199,
+            milestone2: 45382,
+            milestone3: 45657
+          },
+          {
+            entryNumber: 2,
+            projectSiteName: 'Site A2',
+            projectSiteIdentifier: 'A2',
+            projectSiteType: 'Water Tower',
+            latitude: 40,
+            longitude: -145,
+            newOrExisting: 'Existing',
+            isSitePop: 'No',
+            isSiteGateway: 'No',
+            landAccessType: 'Owned',
+            description: 'Edge, Interconnected, Linked',
+            milestone1: 45199,
+            milestone2: 45382,
+            milestone3: 45657
+          },
+          {
+            entryNumber: 3,
+            projectSiteName: 'Site A3',
+            projectSiteIdentifier: 'A3',
+            projectSiteType: 'Water Tower',
+            latitude: 40,
+            longitude: -145,
+            newOrExisting: 'New',
+            isSitePop: 'No',
+            isSiteGateway: 'No',
+            landAccessType: 'Owned',
+            description: 'Edge, Interconnected, Linked',
+            milestone1: 45199,
+            milestone2: 45382,
+            milestone3: 45657
+          }
+        ],
+        sowId: 1
+      }
+    };
+
+    jest.spyOn(XLSX.utils, 'sheet_to_json').mockReturnValue(fakeTab2);
+    const wb = XLSX.read(null); 
+
+    await LoadTab2Data(1,wb,'2', request);
+    expect(performQuery).toBeCalledWith( expect.anything(), expectedInput, expect.anything());
+  });
+  
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
