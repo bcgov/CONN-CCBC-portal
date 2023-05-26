29bd4ca4bb564377c4497ae0c1b355b03c1c4b6f
marcellmueller <marcel@button.is>
2023-05-26 09:18:26 -0700

    feat: add project information form schema and ui schema

---
+++ app/formSchema/analyst/projectInformation.ts
@@ -0,0 +1,42 @@
+import { JSONSchema7 } from 'json-schema';
+
+const projectInformation: JSONSchema7 = {
+  title: 'Project information',
+  description: '',
+  type: 'object',
+  properties: {
+    hasFundingAgreementBeenSigned: {
+      title: 'Has the funding agreement been signed?',
+      type: 'boolean',
+    },
+    main: {
+      title: '',
+      type: 'object',
+      required: [
+        'dateFundingAgreementSigned',
+        'fundingAgreementUpload',
+        'statementOfWorkUpload',
+      ],
+      properties: {
+        dateFundingAgreementSigned: {
+          type: 'string',
+        },
+        fundingAgreementUpload: {
+          title:
+            'Upload the Funding Agreement signed by both the Recipient & Province',
+          type: 'string',
+        },
+        statementOfWorkUpload: {
+          title: 'Upload the completed statement of work tables',
+          type: 'string',
+        },
+        finalizedMapUpload: {
+          title: 'Upload the finalized map, if available',
+          type: 'string',
+        },
+      },
+    },
+  },
+};
+
+export default projectInformation;
---
+++ app/formSchema/uiSchema/analyst/projectInformationUiSchema.ts
@@ -0,0 +1,25 @@
+const projectInformationUiSchema = {
+  hasFundingAgreementBeenSigned: {
+    'ui:title': 'Has the funding agreement been signed by the province?',
+    'ui:widget': 'RadioWidget',
+  },
+  main: {
+    dateFundingAgreementSigned: {
+      'ui:title': 'Date funding agreement signed by the province',
+      'ui:widget': 'DatePickerWidget',
+    },
+    fundingAgreementUpload: {
+      'ui:widget': 'FileWidget',
+    },
+    statementOfWorkUpload: {
+      'ui:title':
+        'After pressing Import, key information will be extracted from the Statement of Work Tables to the database such as Dates, Communities & households, and Project costing & funding',
+      'ui:widget': 'FileWidget',
+    },
+    finalizedMapUpload: {
+      'ui:widget': 'FileWidget',
+    },
+  },
+};
+
+export default projectInformationUiSchema;
