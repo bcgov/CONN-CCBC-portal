0e1feb30c0e8929f5beaacdcc60b38462ba85792
Anthony Bushara <anthony@button.is>
2023-05-29 16:29:10 -0400

    chore: update tests for tab 1

--- app/tests/backend/lib/sow_tab_1.test.ts
+++ app/tests/backend/lib/sow_tab_1.test.ts
@@ -1,8 +1,13 @@
+/**
+ * @jest-environment node
+ */
 import {
   hasDataInRow,
   readRowData,
-  COLUMN_KEYS,
+  TAB_ONE_CONSTANTS,
 } from '../../../backend/lib/sow_import/tab_1';
+
+const { COLUMN_KEYS } = TAB_ONE_CONSTANTS;
 
 describe('sow_tab_1 parsing tests', () => {
   it('the hasDataInRow function returns correctly', () => {
@@ -24,8 +29,19 @@ describe('sow_tab_1 parsing tests', () => {
   it('the readRowData function returns only the relevant row data', () => {
     const rowData = {};
     // build successful row data
+    const BOOLEAN_KEYS = [
+      TAB_ONE_CONSTANTS.IS_DIRECT_TO_HOME_SATELITE,
+      TAB_ONE_CONSTANTS.IS_IMPACTED_BY_MOBILE_WIRELESS_SERVICE,
+      TAB_ONE_CONSTANTS.IS_INDIGENOUS_COMMUNITY_COLUMN,
+      TAB_ONE_CONSTANTS.IS_WIRED,
+      TAB_ONE_CONSTANTS.IS_WIRELESS,
+    ];
     COLUMN_KEYS.forEach((key, index) => {
-      rowData[key] = `Value_${index}`;
+      if (BOOLEAN_KEYS.includes(key)) {
+        rowData[key] = 'Yes';
+      } else {
+        rowData[key] = `Value_${index}`;
+      }
     });
 
     const dataRead = readRowData(rowData);
@@ -35,9 +51,11 @@ describe('sow_tab_1 parsing tests', () => {
     expect(dataRead.communityName).toBe(rowData[COLUMN_KEYS[2]]);
     expect(dataRead.latitude).toBe(rowData[COLUMN_KEYS[3]]);
     expect(dataRead.longitude).toBe(rowData[COLUMN_KEYS[4]]);
-    expect(dataRead.isIndigenousCommunity).toBe(rowData[COLUMN_KEYS[5]]);
+    expect(dataRead.isIndigenousCommunity).toBeTrue();
     expect(dataRead.householdsImpactedIndigenous).toBe(rowData[COLUMN_KEYS[6]]);
     expect(dataRead.numberOfHouseholds).toBe(rowData[COLUMN_KEYS[7]]);
-    expect(dataRead.isWired).toBe(rowData[COLUMN_KEYS[7]]);
+    expect(dataRead.isWired).toBeTrue();
+    expect(dataRead.isWireless).toBeTrue();
+    expect(dataRead.isImpactedByMobileWirelessService).toBeTrue();
   });
 });
