c240bf47aff964950d6f297ef72486e4b19d1eee
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2023-06-13 10:12:34 -0700

    feat: project information history details

---
+++ app/formSchema/uiSchema/history/projectInformation.ts
@@ -0,0 +1,14 @@
+const projectInformationSchema = {
+  projectInformation: {
+    properties: {
+      hasFundingAgreementBeenSigned: {
+        title: 'Funding agreement signed',
+      },
+      dateFundingAgreementSigned: {
+        title: 'Date signed',
+      },
+    },
+  },
+};
+
+export default projectInformationSchema;
