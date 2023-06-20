5553bd88f4dbeac2e8d81a6b3965d3044e07777b
marcellmueller <marcel@button.is>
2023-06-20 16:11:29 -0700

    feat: add createChangeRequest graphql mutation

---
+++ app/schema/mutations/project/createChangeRequest.ts
@@ -0,0 +1,29 @@
+import { graphql } from 'react-relay';
+import { createChangeRequestMutation } from '__generated__/createConditionalApprovalMutation.graphql';
+import useMutationWithErrorMessage from '../useMutationWithErrorMessage';
+
+const mutation = graphql`
+  mutation createChangeRequestMutation(
+    $connections: [ID!]!
+    $input: CreateChangeRequestInput!
+  ) {
+    createChangeRequest(input: $input) {
+      changeRequestDataEdge @appendEdge(connections: $connections) {
+        cursor
+        node {
+          id
+          jsonData
+          rowId
+        }
+      }
+    }
+  }
+`;
+
+const useCreateChangeRequestMutation = () =>
+  useMutationWithErrorMessage<createChangeRequestMutation>(
+    mutation,
+    () => 'An error occured while attempting to create the conditional approval'
+  );
+
+export { mutation, useCreateChangeRequestMutation };
