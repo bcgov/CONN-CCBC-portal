d97eb18c8ce5637b3401e657e0ce1ce85e3b41c8
marcellmueller <marcel@button.is>
2023-06-26 13:35:34 -0700

    refactor: merge create_application_sow updates into create_application_sow_data

--- db/deploy/mutations/create_application_sow_data.sql
+++ db/deploy/mutations/create_application_sow_data.sql
@@ -29,8 +29,26 @@ begin
   insert into ccbc_public.application_sow_data (application_id, json_data, amendment_number, is_amendment)
     values (_application_id, _json_data, sow_amendment_number, sow_is_amendment) returning id into new_application_sow_data_id;
 
+  -- archive the old sow data
   if exists (select * from ccbc_public.application_sow_data where id = old_application_sow_data_id)
     then update ccbc_public.application_sow_data set archived_at = now() where id = old_application_sow_data_id;
+  end if;
+
+ -- archive all the sow tabs related to the old sow id
+  if exists (select * from ccbc_public.sow_tab_1 where sow_id = old_application_sow_data_id)
+    then update ccbc_public.sow_tab_1 set archived_at = now() where sow_id = old_application_sow_data_id;
+  end if;
+
+  if exists (select * from ccbc_public.sow_tab_2 where sow_id = old_application_sow_data_id)
+    then update ccbc_public.sow_tab_2 set archived_at = now() where sow_id = old_application_sow_data_id;
+  end if;
+
+  if exists (select * from ccbc_public.sow_tab_7 where sow_id = old_application_sow_data_id)
+    then update ccbc_public.sow_tab_7 set archived_at = now() where sow_id = old_application_sow_data_id;
+  end if;
+
+  if exists (select * from ccbc_public.sow_tab_8 where sow_id = old_application_sow_data_id)
+    then update ccbc_public.sow_tab_8 set archived_at = now() where sow_id = old_application_sow_data_id;
   end if;
 
   return (select row(ccbc_public.application_sow_data.*) from ccbc_public.application_sow_data where id = new_application_sow_data_id);
