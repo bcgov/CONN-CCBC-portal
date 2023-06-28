77d5495a1d908c03e1076ca559eedae7fea52093
marcellmueller <marcel@button.is>
2023-06-28 15:37:35 -0700

    fix: wrap metabase link test component in global theme

--- app/tests/components/MetabaseLink.test.tsx
+++ app/tests/components/MetabaseLink.test.tsx
@@ -1,14 +1,25 @@
-import { render, screen } from '@testing-library/react'; 
+import { render, screen } from '@testing-library/react';
 import MetabaseLink from 'components/Analyst/Project/ProjectInformation/MetabaseLink';
+import GlobalTheme from 'styles/GlobalTheme';
 
 const renderStaticLayout = (href?, text?, width?) => {
-  return render(<MetabaseLink href={href} text={text} width={width}/>);
+  return render(
+    <GlobalTheme>
+      <MetabaseLink href={href} text={text} width={width} />
+    </GlobalTheme>
+  );
 };
 
 describe('The SubHeaderNavbarLinks component', () => {
   it('should render Metabase link for GIS screen', () => {
-    renderStaticLayout('linkto:metabase_link_to_gis_dashboard','Click here to see GIS dashboard', 600);
-    const link = screen.getByRole('link', { name: 'Click here to see GIS dashboard' });
+    renderStaticLayout(
+      'linkto:metabase_link_to_gis_dashboard',
+      'Click here to see GIS dashboard',
+      600
+    );
+    const link = screen.getByRole('link', {
+      name: 'Click here to see GIS dashboard',
+    });
     expect(link).toHaveAttribute(
       'href',
       'linkto:metabase_link_to_gis_dashboard'
@@ -16,14 +27,19 @@ describe('The SubHeaderNavbarLinks component', () => {
   });
 
   it('should render Metabase link for SoW screen', () => {
-    renderStaticLayout('linkto:metabase_link_to_sow_dashboard','Click here to see SoW dashboard');
-    const link = screen.getByRole('link', { name: 'Click here to see SoW dashboard' });
+    renderStaticLayout(
+      'linkto:metabase_link_to_sow_dashboard',
+      'Click here to see SoW dashboard'
+    );
+    const link = screen.getByRole('link', {
+      name: 'Click here to see SoW dashboard',
+    });
     expect(link).toHaveAttribute(
       'href',
       'linkto:metabase_link_to_sow_dashboard'
     );
   });
-  
+
   it('should render empty Metabase link', () => {
     renderStaticLayout();
     expect(screen.getByTestId('metabase-link')).toBeInTheDocument();
