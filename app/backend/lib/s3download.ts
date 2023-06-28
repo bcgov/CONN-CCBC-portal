c7fa6c721fdee4b0a3ffbdf241c189646f9157a3
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2023-06-28 10:15:19 -0700

    feat: quarantined files handler for analysts

--- app/backend/lib/s3download.ts
+++ app/backend/lib/s3download.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import config from '../../config';
 import getAuthRole from '../../utils/getAuthRole';
-import { getFileTagging, getFileFromS3} from './s3client';
+import { getFileTagging, getFileFromS3 } from './s3client';
 
 const AWS_S3_BUCKET = config.get('AWS_S3_BUCKET');
 
@@ -15,7 +15,8 @@ const detectInfected = async (uuid: string) => {
   return getTags;
 };
 
-s3download.get('/api/s3/download/:uuid/:fileName', async(req, res) => {
+// eslint-disable-next-line consistent-return
+s3download.get('/api/s3/download/:uuid/:fileName', async (req, res) => {
   const { uuid, fileName } = req.params;
 
   const authRole = getAuthRole(req);
@@ -27,20 +28,26 @@ s3download.get('/api/s3/download/:uuid/:fileName', async(req, res) => {
   if (!isRoleAuthorized || !uuid || !fileName) {
     return res.status(404).end();
   }
-  // first check AV tag
-  const healthCheck = await detectInfected(uuid);
-  const suspect = healthCheck.TagSet.find((x) => x.Key === 'av-status');
-  if (suspect?.Value === 'dirty') {
-    return res.json({avstatus:'dirty'});
-  } 
-  
+  if (
+    authRole?.pgRole === 'ccbc_admin' ||
+    authRole?.pgRole === 'ccbc_analyst'
+  ) {
+    // first check AV tag
+    // only for admin and analyst
+    const healthCheck = await detectInfected(uuid);
+    const suspect = healthCheck.TagSet.find((x) => x.Key === 'av-status');
+    if (suspect?.Value === 'dirty') {
+      return res.json({ avstatus: 'dirty' });
+    }
+  }
+
   getFileFromS3(uuid, fileName, res)
-   .then(()=>{
-    res.end();
-   })
-   .catch(()=>{
-    res.status(500).end();
-   })
+    .then(() => {
+      res.end();
+    })
+    .catch(() => {
+      res.status(500).end();
+    });
 });
 
 export default s3download;
--- app/components/DownloadLink.tsx
+++ app/components/DownloadLink.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import * as Sentry from '@sentry/nextjs';
+import { useState } from 'react';
 
 const StyledLink = styled.a`
   color: ${(props) => props.theme.color.links};
@@ -11,22 +12,33 @@ const StyledLink = styled.a`
   }
 `;
 
-const handleDownload = async (uuid, fileName) => {
+const handleDownload = async (uuid, fileName, setQuarantinedLink) => {
   const url = `/api/s3/download/${uuid}/${fileName}`;
   await fetch(url)
     .then((response) => response.json())
     .then((response) => {
-      window.open(response, '_blank');
+      if (typeof response === 'object' && response.avstatus === 'dirty') {
+        setQuarantinedLink(true);
+        // eslint-disable-next-line no-alert
+        window.alert(
+          'An error occurred when downloading the file. Contact the CCBC Portal administrator'
+        );
+      } else {
+        window.open(response, '_blank');
+      }
     });
 };
 
 const DownloadLink = ({ uuid, fileName }) => {
-  return (
+  const [quarantinedLink, setQuarantinedLink] = useState(false);
+  return quarantinedLink ? (
+    `${fileName}`
+  ) : (
     <StyledLink
       data-testid="history-attachment-link"
       onClick={(e) => {
         e.preventDefault();
-        handleDownload(uuid, fileName).catch((error) => {
+        handleDownload(uuid, fileName, setQuarantinedLink).catch((error) => {
           Sentry.captureException(error);
         });
       }}
