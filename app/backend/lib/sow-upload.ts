b1c150a0caf2ea2f82fbc0bbf5855c9436d3ec96
Vladimir <vladimir@button.is>
2023-07-07 14:28:40 -0700

    feat: failsafe to prevent filepath error

--- app/backend/lib/sow-upload.ts
+++ app/backend/lib/sow-upload.ts
@@ -41,7 +41,9 @@ const processSow: ExpressMiddleware = async (req, res) => {
 
   const filename = Object.keys(files)[0];
   const uploaded = files[filename];
-
+  if (!!uploaded === false) {
+    return res.status(200).end();
+  }
   const buf = fs.readFileSync(uploaded.filepath);
   const wb = XLSX.read(buf);
 
