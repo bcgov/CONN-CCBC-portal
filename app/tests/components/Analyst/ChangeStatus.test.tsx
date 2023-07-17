622f824cb35cd2cb8b548603db9cb8b4ad7d55c9
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2023-07-17 09:27:11 -0700

    test: update external status tests

--- app/tests/components/Analyst/ChangeStatus.test.tsx
+++ app/tests/components/Analyst/ChangeStatus.test.tsx
@@ -271,7 +271,8 @@ describe('The application header component', () => {
       fireEvent.change(select, { target: { value: 'approved' } });
     });
 
-    expect(screen.getByTestId('change-status')).toHaveValue('approved');
+    // this will stay on-hold as it will immediately change back before modal
+    expect(screen.getByTestId('change-status')).toHaveValue('on_hold');
 
     const okButton = screen.getByText('Ok');
     await act(async () => {
@@ -291,7 +292,7 @@ describe('The application header component', () => {
       fireEvent.change(select, { target: { value: 'conditionally_approved' } });
     });
 
-    const closeButton = screen.getByText('Close');
+    const closeButton = screen.getByText('Cancel change');
     await act(async () => {
       fireEvent.click(closeButton);
     });
@@ -316,6 +317,11 @@ describe('The application header component', () => {
     expect(screen.getByTestId('change-status')).toHaveValue(
       'conditionally_approved'
     );
+
+    const saveButton = screen.getByText('Save change');
+    await act(async () => {
+      fireEvent.click(saveButton);
+    });
 
     externalComponentTestingHelper.expectMutationToBeCalled(
       'createApplicationStatusMutation',
