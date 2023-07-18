e3de2f806921d8e6a1e07d0ffd743bf9a794e68b
Anthony Bushara <anthony@button.is>
2023-07-18 14:13:05 -0400

    refactor: use ProjectTheme as the base instead of copy-paste

--- app/components/Analyst/Project/ChangeRequestTheme.ts
+++ app/components/Analyst/Project/ChangeRequestTheme.ts
@@ -1,33 +1,10 @@
-import { ThemeProps, utils } from '@rjsf/core';
-import ArrayFieldTemplate from 'lib/theme/fields/ArrayFieldTemplate';
-import * as widgets from 'lib/theme/widgets';
-import ReadOnlyWidget from 'components/Analyst/Project/ConditionalApproval/widgets/ReadOnlyWidget';
-import SowImportFileWidget from 'components/Analyst/Project/ProjectInformation/widgets/SowImportFileWidget';
-import { StatusSelectWidget } from './ConditionalApproval/widgets';
-import ProjectObjectFieldTemplate from './fields/ProjectObjectFieldTemplate';
-import { ProjectSectionField } from './fields';
-import CcbcIdWidget from './widgets/CcbcIdWidget';
+import { ThemeProps } from '@rjsf/core';
 import ChangeRequestFieldTemplate from './fields/ChangeRequestFieldTemplate';
-
-const { fields } = utils.getDefaultRegistry();
+import ProjectTheme from './ProjectTheme';
 
 const ChangeRequestTheme: ThemeProps = {
-  fields: {
-    ...fields,
-    SectionField: ProjectSectionField,
-  },
-  widgets: {
-    ...widgets,
-    ReadOnlyWidget,
-    SowImportFileWidget,
-    StatusSelectWidget,
-    CcbcIdWidget,
-  },
-  ObjectFieldTemplate: ProjectObjectFieldTemplate,
-  // ObjectFieldTemplate: ObjectFieldTemplate,
+  ...ProjectTheme,
   FieldTemplate: ChangeRequestFieldTemplate,
-  // FieldTemplate: FieldTemplate,
-  ArrayFieldTemplate,
 };
 
 export default ChangeRequestTheme;
