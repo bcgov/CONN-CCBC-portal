5b71166e5459f8e4a9e837e85327cf08590dd961
marcellmueller <marcel@button.is>
2023-07-20 11:12:15 -0700

    feat: improve intake animation

--- app/components/Admin/AddIntake.tsx
+++ app/components/Admin/AddIntake.tsx
@@ -12,16 +12,19 @@ interface EditProps {
   isFormEditMode: boolean;
 }
 
-const StyledContainer = styled.div`
+const StyledContainer = styled.div<EditProps>`
   border: 1px solid #606060;
   border-radius: 4px;
-  padding: 24px;
+  padding: 0 24px;
+  visibility: ${({ isFormEditMode }) =>
+    isFormEditMode ? 'visible' : 'hidden'};
+  transition: all 0.5s;
 `;
 
 const StyledBtnContainer = styled.div<EditProps>`
-  margin: ${(props) => (props.isFormEditMode ? '0px' : '16px 0')};
+  margin: ${({ isFormEditMode }) => (isFormEditMode ? '0px' : '16px 0')};
   overflow: hidden;
-  max-height: ${(props) => (props.isFormEditMode ? '0px' : '50px')};
+  max-height: ${({ isFormEditMode }) => (isFormEditMode ? '0px' : '50px')};
   transition: all 0.5s;
 
   & button {
@@ -31,10 +34,11 @@ const StyledBtnContainer = styled.div<EditProps>`
 
 const StyledForm = styled.section<EditProps>`
   overflow: hidden;
-  max-height: ${(props) => (props.isFormEditMode ? '500px' : '0px')};
+  max-height: ${({ isFormEditMode }) => (isFormEditMode ? '500px' : '0px')};
   transition: max-height 0.5s;
   padding-left: 4px;
   max-width: 480px;
+  margin-top: ${({ isFormEditMode }) => (isFormEditMode ? '24px' : '0px')};
 
   .datetime-widget {
     width: 240px;
@@ -57,7 +61,7 @@ const AddIntake = () => {
   } as any);
 
   return (
-    <StyledContainer>
+    <section>
       <StyledBtnContainer isFormEditMode={isFormEditMode}>
         <Button
           isFormEditMode={isFormEditMode}
@@ -67,27 +71,32 @@ const AddIntake = () => {
           Add intake
         </Button>
       </StyledBtnContainer>
-      <StyledForm isFormEditMode={isFormEditMode}>
-        <FormBase
-          formData={formData}
-          schema={intakeSchema}
-          uiSchema={intakeUiSchema}
-          theme={{
-            ...DefaultTheme,
-            widgets: {
-              ...DefaultTheme.widgets,
-              ReadOnlyWidget,
-            },
-            FieldTemplate: BasicFieldTemplate,
-          }}
-        >
-          <StyledSaveBtn>Save</StyledSaveBtn>
-          <Button onClick={() => setIsFormEditMode(false)} variant="secondary">
-            Cancel
-          </Button>
-        </FormBase>
-      </StyledForm>
-    </StyledContainer>
+      <StyledContainer isFormEditMode={isFormEditMode}>
+        <StyledForm isFormEditMode={isFormEditMode}>
+          <FormBase
+            formData={formData}
+            schema={intakeSchema}
+            uiSchema={intakeUiSchema}
+            theme={{
+              ...DefaultTheme,
+              widgets: {
+                ...DefaultTheme.widgets,
+                ReadOnlyWidget,
+              },
+              FieldTemplate: BasicFieldTemplate,
+            }}
+          >
+            <StyledSaveBtn>Save</StyledSaveBtn>
+            <Button
+              onClick={() => setIsFormEditMode(false)}
+              variant="secondary"
+            >
+              Cancel
+            </Button>
+          </FormBase>
+        </StyledForm>
+      </StyledContainer>
+    </section>
   );
 };
 
