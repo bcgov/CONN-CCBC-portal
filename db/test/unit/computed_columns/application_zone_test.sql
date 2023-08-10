45baddf6c1b134686f3f7f3b8e7f1e15e8532a2b
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2023-08-10 09:42:43 -0700

    test: add application zone test

---
+++ db/test/unit/computed_columns/application_zone_test.sql
@@ -0,0 +1,83 @@
+begin;
+
+select plan(7);
+
+truncate table
+  ccbc_public.application,
+  ccbc_public.application_status,
+  ccbc_public.attachment,
+  ccbc_public.form_data,
+  ccbc_public.application_form_data,
+  ccbc_public.intake,
+  ccbc_public.application_analyst_lead
+restart identity cascade;
+
+select has_function(
+  'ccbc_public', 'application_zone',
+  'Function application_zone should exist'
+);
+
+insert into ccbc_public.intake(open_timestamp, close_timestamp, ccbc_intake_number)
+values('2022-03-01 09:00:00-07', '2022-05-01 09:00:00-07', 1);
+
+select mocks.set_mocked_time_in_transaction('2022-04-01 09:00:00-07'::timestamptz);
+
+set jwt.claims.sub to 'testCcbcAuthUser';
+insert into ccbc_public.ccbc_user
+  (given_name, family_name, email_address, session_sub) values
+  ('foo1', 'bar', 'foo1@bar.com', 'testCcbcAuthUser');
+set role ccbc_auth_user;
+
+select ccbc_public.create_application();
+
+select ccbc_public.create_application();
+update ccbc_public.form_data set json_data = '{ "projectArea": {"geographicArea": [1,2,3] }}'::jsonb;
+
+insert into ccbc_public.application_status (application_id, status) VALUES (1,'received');
+
+select throws_like (
+  $$
+  select ccbc_public.application_zone(
+    (select row(application.*)::ccbc_public.application from ccbc_public.application)
+  )
+  $$,
+  'permission denied for function application_zone',
+  'ccbc_auth_user does not have access to application_zone'
+);
+
+set role ccbc_analyst;
+
+select is (
+  (
+    select ccbc_public.application_zone(
+      (select row(application.*)::ccbc_public.application from ccbc_public.application)
+    )
+  ),
+  1,
+  'application_zone retrieves the lowest zone from the form_data'
+);
+
+
+select function_privs_are(
+  'ccbc_public', 'application_zone', ARRAY['ccbc_public.application'], 'ccbc_analyst', ARRAY['EXECUTE'],
+  'ccbc_analyst can execute ccbc_public.application_zone(ccbc_public.application)'
+);
+
+select function_privs_are(
+  'ccbc_public', 'application_zone', ARRAY['ccbc_public.application'], 'ccbc_admin', ARRAY['EXECUTE'],
+  'ccbc_analyst can execute ccbc_public.application_zone(ccbc_public.application)'
+);
+
+select function_privs_are(
+  'ccbc_public', 'application_zone', ARRAY['ccbc_public.application'], 'ccbc_guest', ARRAY[]::text[],
+  'ccbc_guest cannot execute ccbc_public.application_zone(ccbc_public.application)'
+);
+
+select function_privs_are(
+  'ccbc_public', 'application_zone', ARRAY['ccbc_public.application'], 'ccbc_auth_user', ARRAY[]::text[],
+  'ccbc_auth_user cannot execute ccbc_public.application_zone(ccbc_public.application)'
+);
+
+select finish();
+
+rollback;
