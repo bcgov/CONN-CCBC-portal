e927c343d87117935fd921a6a36eb365f82f4f7e
marcellmueller <marcel@button.is>
2023-08-21 16:04:10 -0700

    feat: add claims form

---
+++ app/components/Analyst/Project/Claims/ClaimsForm.tsx
@@ -0,0 +1,267 @@
+import { useCallback, useRef, useState } from 'react';
+import styled from 'styled-components';
+import Button from '@button-inc/bcgov-theme/Button';
+import { ConnectionHandler, graphql, useFragment } from 'react-relay';
+import claimsSchema from 'formSchema/analyst/claims';
+import claimsUiSchema from 'formSchema/uiSchema/analyst/claimsUiSchema';
+import { useCreateClaimsMutation } from 'schema/mutations/project/createClaimsData';
+/* import { useArchiveApplicationClaimsMutation as useArchiveCpr } from 'schema/mutations/project/archiveApplicationClaims'; */
+import excelValidateGenerator from 'lib/helpers/excelValidate';
+import Toast from 'components/Toast';
+import Modal from 'components/Modal';
+import ProjectTheme from '../ProjectTheme';
+import ProjectForm from '../ProjectForm';
+import AddButton from '../AddButton';
+
+const StyledContainer = styled.div`
+  text-align: center;
+  max-width: 400px;
+
+  p {
+    margin-top: 16px;
+  }
+`;
+
+const StyledProjectForm = styled(ProjectForm)`
+  .datepicker-widget {
+    width: 180px;
+    margin-bottom: 0px;
+  }
+`;
+
+const StyledFlex = styled.div`
+  display: flex;
+  justify-content: center;
+
+  button:first-child {
+    margin-right: 16px;
+  }
+`;
+
+interface FormData {
+  claimsFile?: any;
+  fromDate?: string;
+  toDate?: string;
+}
+
+const ClaimsForm = ({ application }) => {
+  const queryFragment = useFragment(
+    graphql`
+      fragment ClaimsForm_application on Application {
+        id
+        rowId
+        applicationClaimsDataByApplicationId(
+          filter: { archivedAt: { isNull: true } }
+          first: 1000
+        ) @connection(key: "ClaimsForm_applicationClaimsDataByApplicationId") {
+          __id
+          edges {
+            node {
+              __id
+              id
+              rowId
+              jsonData
+            }
+          }
+        }
+        ccbcNumber
+      }
+    `,
+    application
+  );
+  const {
+    applicationClaimsDataByApplicationId: claimsData,
+    rowId: applicationRowId,
+  } = queryFragment;
+
+  const [formData, setFormData] = useState({} as FormData);
+  const [showModal, setShowModal] = useState(false);
+  // store the current community progress data node for edit mode so we have access to row id and relay connection
+  const [currentClaimsData, setCurrentClaimsData] = useState(null);
+  const [isFormEditMode, setIsFormEditMode] = useState(false);
+  const [createClaims] = useCreateClaimsMutation();
+  /*   const [archiveClaims] = useArchiveCpr(); */
+  const hiddenSubmitRef = useRef<HTMLButtonElement>(null);
+  // use this to live validate the form after the first submit attempt
+  const [isSubmitAttempted, setIsSubmitAttempted] = useState(false);
+  const [excelFile, setExcelFile] = useState(null);
+  const [showToast, setShowToast] = useState(false);
+  const [claimsValidationErrors, setClaimsValidationErrors] = useState([]);
+  const [isFormSubmitting, setIsFormSubmitting] = useState(false);
+
+  const claimsConnectionId = claimsData?.__id;
+  const claimsList = claimsData?.edges?.filter((data) => {
+    // filter null nodes from the list caused by relay connection update
+    return data.node !== null;
+  });
+
+  const apiPath = `/api/analyst/claims/${applicationRowId}/${currentClaimsData?.rowId}`;
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const validateClaims = useCallback(
+    excelValidateGenerator(apiPath, setExcelFile, setClaimsValidationErrors),
+    [setExcelFile]
+  );
+
+  const handleResetFormData = () => {
+    setIsFormEditMode(false);
+    setFormData({} as FormData);
+    setCurrentClaimsData(null);
+    setIsSubmitAttempted(false);
+    setExcelFile(null);
+    setShowToast(false);
+  };
+
+  const handleSubmit = async (e) => {
+    hiddenSubmitRef.current.click();
+    e.preventDefault();
+    setIsSubmitAttempted(true);
+    /*   if (!formData?.fromDate) return; */
+    setIsFormSubmitting(true);
+
+    validateClaims(excelFile, false).then((res) => {
+      // get the excel data row i from the response or the current community progress data
+      const responseExcelDataId =
+        res?.result?.data.createApplicationCommunityReportExcelData
+          ?.applicationCommunityReportExcelData?.rowId;
+
+      // get the excel data row id from the current community progress data if it exists
+      const currentExcelDataId = currentClaimsData?.excelDataId;
+
+      // replace the current excel data id if a new excel file was uploaded since the previous data will be archived
+      const excelDataId = responseExcelDataId || currentExcelDataId;
+      console.log(claimsList, excelDataId);
+
+      /// save form data
+      createClaims({
+        variables: {
+          connections: [claimsConnectionId],
+          input: {
+            _jsonData: formData,
+            _applicationId: applicationRowId,
+            _oldClaimsId: currentClaimsData?.rowId,
+            /*             _excelDataId: excelDataId, */
+          },
+        },
+        onCompleted: () => {
+          handleResetFormData();
+          setIsFormSubmitting(false);
+
+          if (res?.status === 200) {
+            setShowToast(true);
+          }
+        },
+        onError: () => {
+          setIsFormSubmitting(false);
+        },
+        updater: (store) => {
+          if (currentClaimsData?.id) {
+            const connection = store.get(claimsConnectionId);
+
+            store.delete(currentClaimsData.id);
+            ConnectionHandler.deleteNode(connection, currentClaimsData.id);
+          }
+        },
+      });
+    });
+  };
+
+  const handleDelete = async () => {
+    // archiveClaims({
+    //   variables: {
+    //     input: {
+    //       _claimsId: currentClaimsData?.rowId,
+    //     },
+    //   },
+    //   updater: (store) => {
+    //     const connection = store.get(claimsConnectionId);
+    //     const claimsConnectionId = currentClaimsData?.__id;
+    //
+    //     store.delete(claimsConnectionId);
+    //     ConnectionHandler.deleteNode(connection, claimsConnectionId);
+    //   },
+    //   onCompleted: () => {
+    //     setShowModal(false);
+    //     setCurrentClaimsData(null);
+    //   },
+    // });
+  };
+
+  return (
+    <>
+      {showToast && (
+        <Toast timeout={5000}>
+          Claims & progress report excel data successfully imported
+        </Toast>
+      )}
+      <Modal
+        open={showModal}
+        onClose={() => {
+          setCurrentClaimsData(null);
+          setShowModal(false);
+        }}
+        title="Delete"
+      >
+        <StyledContainer>
+          <p>
+            Are you sure you want to delete this claim and all accompanying
+            data?
+          </p>
+          <StyledFlex>
+            <Button onClick={handleDelete}>Yes, delete</Button>
+            <Button onClick={() => setShowModal(false)} variant="secondary">
+              No, keep
+            </Button>
+          </StyledFlex>
+        </StyledContainer>
+      </Modal>
+      <StyledProjectForm
+        additionalContext={{
+          applicationId: applicationRowId,
+          validateExcel: validateClaims,
+          excelValidationErrors: claimsValidationErrors,
+        }}
+        schema={claimsSchema}
+        uiSchema={claimsUiSchema}
+        formData={formData}
+        theme={ProjectTheme}
+        onSubmit={handleSubmit}
+        formAnimationHeight={600}
+        isFormAnimated
+        hiddenSubmitRef={hiddenSubmitRef}
+        isFormEditMode={isFormEditMode}
+        setIsFormEditMode={(boolean) => setIsFormEditMode(boolean)}
+        saveBtnText={
+          formData?.claimsFile && excelFile ? 'Save & Import' : 'Save'
+        }
+        title="Claims"
+        handleChange={(e) => {
+          setFormData({ ...e.formData });
+        }}
+        submitting={isFormSubmitting}
+        submittingText="Importing claim data. Please wait."
+        showEditBtn={false}
+        saveBtnDisabled={isFormSubmitting}
+        cancelBtnDisabled={isFormSubmitting}
+        resetFormData={handleResetFormData}
+        liveValidate={isSubmitAttempted}
+        setFormData={setFormData}
+        before={
+          <AddButton
+            isFormEditMode={isFormEditMode}
+            onClick={() => {
+              setIsSubmitAttempted(false);
+              setIsFormEditMode(true);
+            }}
+            title="Add claim"
+          />
+        }
+        saveDataTestId="save-claims data"
+      >
+        map view here
+      </StyledProjectForm>
+    </>
+  );
+};
+
+export default ClaimsForm;
