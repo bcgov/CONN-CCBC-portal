8dc09e15a808d2691bc23bc936ad4aed6b28c01a
marcellmueller <marcel@button.is>
2023-08-21 16:25:06 -0700

    feat: add claims form header

--- app/formSchema/uiSchema/analyst/claimsUiSchema.ts
+++ app/formSchema/uiSchema/analyst/claimsUiSchema.ts
@@ -8,8 +8,6 @@ const claimsUiSchema = {
     'ui:widget': 'DatePickerWidget',
   },
   claimsFile: {
-    'ui:title':
-      'Claims & progress reports are submitted by recipients for incurred or paid expenses during the previous quarter(s). While they are due 45 days after that quarter ends, recipients do not always submit claim & progress reports every quarter. \nAll processing of claims takes place outside of the CCBC portal. After a claim is processed and paid, please upload the finalized and completed claim Excel file here.',
     'ui:widget': 'ExcelImportFileWidget',
     'ui:options': {
       excelImport: {
