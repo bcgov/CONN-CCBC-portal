57550ed1de027d52f2a0ea11aacbf10d38ac6f43
marcellmueller <marcel@button.is>
2023-08-22 09:54:30 -0700

    feat: add mutation create_application_claims_data

--- db/test/unit/mutations/create_application_claims_excel_data_test.sql
+++ db/test/unit/mutations/create_application_claims_excel_data_test.sql
@@ -11,8 +11,8 @@ truncate table
   ccbc_public.intake
 restart identity cascade;
 
-select has_function('ccbc_public', 'create_application_community_report_excel_data',
-  'Function create_application_community_report_excel_data should exist');
+select has_function('ccbc_public', 'create_application_claims_excel_data',
+  'Function create_application_claims_excel_data should exist');
 
 insert into ccbc_public.intake(open_timestamp, close_timestamp, ccbc_intake_number)
 values('2022-03-01 09:00:00-07', '2022-05-01 09:00:00-07', 1);
@@ -30,11 +30,11 @@ select ccbc_public.create_application();
 -- set role to analyst
 set role ccbc_analyst;
 
-select ccbc_public.create_application_community_report_excel_data(1::int , '{}'::jsonb);
+select ccbc_public.create_application_claims_excel_data(1::int , '{}'::jsonb);
 
 select results_eq (
   $$
-    select count(*) from ccbc_public.application_community_report_excel_data
+    select count(*) from ccbc_public.application_claims_excel_data
     where application_id = 1
   $$,
   $$
@@ -42,15 +42,15 @@ select results_eq (
     1::bigint
   )
   $$,
-  'Should crete community report data'
+  'Should crete claims data'
 );
 
 -- create another, pass in the previous id and make sure the previous one is archived
-select ccbc_public.create_application_community_report_excel_data(1::int , '{}'::jsonb, 1);
+select ccbc_public.create_application_claims_excel_data(1::int , '{}'::jsonb, 1);
 
 select results_eq (
   $$
-    select count(*) from ccbc_public.application_community_report_excel_data
+    select count(*) from ccbc_public.application_claims_excel_data
     where application_id = 1 and archived_at is not null
   $$,
   $$
@@ -58,7 +58,7 @@ select results_eq (
     1::bigint
   )
   $$,
-  'Original community report data should be archived'
+  'Original claims data should be archived'
 );
 
 select finish();
