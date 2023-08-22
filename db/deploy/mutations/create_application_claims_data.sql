8b03c72622afa0f2462b0c5326831a4fbd230421
marcellmueller <marcel@button.is>
2023-08-22 09:54:30 -0700

    feat: add mutation create_application_claims_data

--- db/deploy/mutations/create_application_claims_data.sql
+++ db/deploy/mutations/create_application_claims_data.sql
@@ -5,19 +5,27 @@ begin;
 create or replace function ccbc_public.create_application_claims_data(
     _application_id integer,
     _json_data jsonb,
-    _old_claims_id integer default null
+    _old_claims_id integer default null,
+    _excel_data_id integer default null
 ) returns ccbc_public.application_claims_data as $$
 declare
   new_id integer;
+  old_excel_data_id integer;
 begin
 
-  insert into ccbc_public.application_claims_data (application_id, json_data)
-  values (_application_id, _json_data)
+  insert into ccbc_public.application_claims_data (application_id, json_data, excel_data_id)
+  values (_application_id, _json_data, _excel_data_id)
   returning id into new_id;
 
+  -- archive old claims data if it exists
   if exists (select * from ccbc_public.application_claims_data where id = _old_claims_id)
-    then update ccbc_public.application_claims_data set archived_at = now() where id = _old_claims_id;
+    then update ccbc_public.application_claims_data set archived_at = now() where id = _old_claims_id returning excel_data_id into old_excel_data_id;
   end if;
+
+  -- archive excel data if the form data has no claims file data
+   if(not (_json_data ? 'claimsFile') or jsonb_typeof(_json_data -> 'claimsFile') = 'null') then
+      update ccbc_public.application_claims_excel_data set archived_at = now() where id = old_excel_data_id;
+   end if;
 
   return (select row(ccbc_public.application_claims_data.*)
     from ccbc_public.application_claims_data
