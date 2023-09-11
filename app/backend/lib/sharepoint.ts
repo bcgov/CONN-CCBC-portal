b823660023ac1199d7f5ffcc7dca5f678226a075
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2023-09-11 16:01:30 -0700

    chore: use spauth

--- app/backend/lib/sharepoint.ts
+++ app/backend/lib/sharepoint.ts
@@ -7,6 +7,7 @@ import {
   ResponseType,
 } from '@microsoft/microsoft-graph-client';
 import * as XLSX from 'xlsx';
+import * as spauth from 'node-sp-auth';
 
 const sharepoint = Router();
 const siteId = process.env.SP_SITE_ID || '';
@@ -29,6 +30,50 @@ const options: Options = {
 };
 
 const client = Client.init(options);
+
+sharepoint.get('/api/sharepoint/auth', async (req, res) => {
+  const authHeaders = await spauth
+    .getAuth('https://bcgov.sharepoint.com/sites/CITZ-CONN_NETWORKBC_COM/', {
+      ondemand: true,
+    })
+    .then(async (data) => {
+      const { headers } = data;
+      headers['Accept'] = 'application/json;odata=verbose';
+      headers['Content-Type'] = 'application/json';
+      return headers;
+    });
+  const t = await fetch(
+    `https://bcgov.sharepoint.com/sites/CITZ-CONN_NETWORKBC_COM/_api/web/GetFolderByServerRelativeUrl('Shared%20Documents')/Files('Connecting BC Program_Approved Projects.xlsx')
+      `,
+    {
+      method: 'GET',
+      headers: authHeaders,
+    }
+  );
+  if (t.ok) {
+    const jsonResponse = await t.json();
+    console.log('JSON Response T:', jsonResponse);
+  } else {
+    console.error('Request failed T:', t.status, t.statusText);
+  }
+  const z = await fetch(
+    `https://bcgov.sharepoint.com/sites/CITZ-CONN_NETWORKBC_COM/_api/web/GetFolderByServerRelativeUrl('Shared%20Documents')/Files('Connecting BC Program_Approved Projects.xlsx')/$value
+      `,
+    {
+      method: 'GET',
+      headers: authHeaders,
+    }
+  );
+  if (z.ok) {
+    const jsonResponse = await z.arrayBuffer();
+    console.log('JSON Response Z:', jsonResponse);
+  } else {
+    console.error('Request failed Z:', t.status, t.statusText);
+  }
+  //  `https://bcgov.sharepoint.com/_api/v2.0/drives/${driveId}/items/${itemId}`,
+
+  res.send('Ok');
+});
 
 sharepoint.get('/api/sharepoint', async (req, res) => {
   const responseObject = { usingXLSX: null, usingGraph: null };
