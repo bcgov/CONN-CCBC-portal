c9c2f98f364c9bac44a67b251547b9587037b49c
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2023-09-12 13:55:32 -0700

    chore: add 8px top margin to helper labels

--- app/components/Form/Label.tsx
+++ app/components/Form/Label.tsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 const StyledLabel = styled('label')`
   color: rgba(49, 49, 50, 0.7);
+  display: inline-block;
+  margin-top: 8px;
   // color: ${(props) => props.theme.color.descriptionGrey};
 `;
 
