f8ba07292593f58bb530debc5c482f323b03cb6a
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2023-09-19 08:08:55 -0700

    chore: sonarcloud void return

--- app/backend/lib/sharepoint.ts
+++ app/backend/lib/sharepoint.ts
@@ -13,52 +13,53 @@ const SP_SA_PASSWORD = config.get('SP_SA_PASSWORD');
 const sharepoint = Router();
 
 // eslint-disable-next-line consistent-return
-sharepoint.get('/api/sharepoint/masterSpreadsheet', async (req, res) => {
+sharepoint.get('/api/sharepoint/masterSpreadsheet', (req, res) => {
   const authRole = getAuthRole(req);
   const isRoleAuthorized = authRole?.pgRole === 'ccbc_admin';
 
   if (!isRoleAuthorized) {
     return res.status(404).end();
   }
+  (async () => {
+    const authHeaders = await spauth
+      .getAuth(SP_SITE, {
+        username: SP_SA_USER,
+        password: SP_SA_PASSWORD,
+      })
+      .then(async (data) => {
+        const { headers } = data;
+        headers['Accept'] = 'application/json;odata=verbose';
+        headers['Content-Type'] = 'application/json';
+        return headers;
+      });
 
-  const authHeaders = await spauth
-    .getAuth(SP_SITE, {
-      username: SP_SA_USER,
-      password: SP_SA_PASSWORD,
-    })
-    .then(async (data) => {
-      const { headers } = data;
-      headers['Accept'] = 'application/json;odata=verbose';
-      headers['Content-Type'] = 'application/json';
-      return headers;
-    });
-
-  const metadata = await fetch(
-    `${SP_SITE}/_api/web/GetFolderByServerRelativeUrl('${SP_DOC_LIBRARY}')/Files('${SP_FILE_NAME}')
+    const metadata = await fetch(
+      `${SP_SITE}/_api/web/GetFolderByServerRelativeUrl('${SP_DOC_LIBRARY}')/Files('${SP_FILE_NAME}')
     `,
-    {
-      method: 'GET',
-      headers: authHeaders,
-    }
-  );
-  const file = await fetch(
-    `${SP_SITE}/_api/web/GetFolderByServerRelativeUrl('${SP_DOC_LIBRARY}')/Files('${SP_FILE_NAME}')/$value
+      {
+        method: 'GET',
+        headers: authHeaders,
+      }
+    );
+    const file = await fetch(
+      `${SP_SITE}/_api/web/GetFolderByServerRelativeUrl('${SP_DOC_LIBRARY}')/Files('${SP_FILE_NAME}')/$value
     `,
-    {
-      method: 'GET',
-      headers: authHeaders,
+      {
+        method: 'GET',
+        headers: authHeaders,
+      }
+    );
+    if (metadata.ok && file.ok) {
+      const bufferResponse = await file.arrayBuffer();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const wb = XLSX.read(bufferResponse);
+      // TODO: check if metadata.TimeLastModified is different from the last time we imported the data
+      // TODO: add logic to parse the workbook and insert data to DB
+    } else {
+      return res.sendStatus(500);
     }
-  );
-  if (metadata.ok && file.ok) {
-    const bufferResponse = await file.arrayBuffer();
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const wb = XLSX.read(bufferResponse);
-    // TODO: check if metadata.TimeLastModified is different from the last time we imported the data
-    // TODO: add logic to parse the workbook and insert data to DB
-  } else {
-    return res.sendStatus(500);
-  }
-  return res.sendStatus(200);
+    return res.sendStatus(200);
+  })();
 });
 
 export default sharepoint;
