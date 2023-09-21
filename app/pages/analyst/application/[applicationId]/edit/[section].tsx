2a8aa9f31402ea859d23ee5e83bebe7e25f8bd46
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2023-09-21 11:42:16 -0700

    chore: update order on mergeFormSectionData

--- app/pages/analyst/application/[applicationId]/edit/[section].tsx
+++ app/pages/analyst/application/[applicationId]/edit/[section].tsx
@@ -55,6 +55,8 @@ const EditApplication = ({
   const hiddenSubmitRef = useRef<HTMLButtonElement>(null);
   const router = useRouter();
   const sectionName = router.query.section as string;
+  console.log('sectionName', sectionName);
+  console.log(jsonSchema);
   const applicationId = router.query.applicationId as string;
   const sectionSchema = jsonSchema.properties[sectionName] as JSONSchema7;
 
@@ -76,10 +78,10 @@ const EditApplication = ({
     const calculatedSectionData = calculate(sectionFormData, sectionName);
 
     const newFormData = mergeFormSectionData(
-      jsonSchema,
       jsonData,
       sectionName,
-      calculatedSectionData
+      calculatedSectionData,
+      jsonSchema
     );
 
     createNewFormData({
