875d554c8014caacd4469baa9c75b697edf1ae2d
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2023-10-05 14:58:56 -0700

    chore: add service account role

--- db/deploy/create_roles@1.105.0.sql
+++ db/deploy/create_roles@1.105.0.sql
@@ -46,14 +46,6 @@ begin
     create role ccbc_admin;
   end if;
 
-    if not exists (
-    select true
-    from pg_catalog.pg_roles
-    where rolname = 'ccbc_service_account') then
-
-    create role ccbc_service_account;
-  end if;
-
   if not exists (
     select true
     from pg_catalog.pg_roles
@@ -71,7 +63,7 @@ begin
   end if;
 
 
-  grant ccbc_guest, ccbc_auth_user, ccbc_job_executor, ccbc_analyst, ccbc_admin, ccbc_archiver, ccbc_service_account to ccbc_app;
+  grant ccbc_guest, ccbc_auth_user, ccbc_job_executor, ccbc_analyst, ccbc_admin, ccbc_archiver to ccbc_app;
   execute format('grant create, connect on database %I to ccbc_app', current_database());
 
 end
