7d4dd61da4646569aa321c3bea4adc16841cea7d
marcellmueller <marcel@button.is>
2023-10-16 14:43:17 -0700

    feat: add assessment assignment table

--- app/components/AnalystDashboard/index.tsx
+++ app/components/AnalystDashboard/index.tsx
@@ -1,4 +1,5 @@
 export { default as AnalystTable } from './AnalystTable';
 export { default as AnalystRow } from './AnalystRow';
+export { default as AssessmentAssignmentTable } from './AssessmentAssignmentTable';
 export { default as DashboardTabs } from './DashboardTabs';
 export { default as TableTabs } from './TableTabs';
--- app/pages/analyst/assessments.tsx
+++ app/pages/analyst/assessments.tsx
@@ -2,7 +2,11 @@ import styled from 'styled-components';
 import { usePreloadedQuery } from 'react-relay/hooks';
 import { withRelay, RelayProps } from 'relay-nextjs';
 import { graphql } from 'react-relay';
-import { DashboardTabs, TableTabs } from 'components/AnalystDashboard';
+import {
+  AssessmentAssignmentTable,
+  DashboardTabs,
+  TableTabs,
+} from 'components/AnalystDashboard';
 import defaultRelayOptions from 'lib/relay/withRelayOptions';
 import { Layout } from 'components';
 import { assessmentsTableQuery } from '__generated__/assessmentsTableQuery.graphql';
@@ -28,7 +32,9 @@ const getAssessmentsTableQuery = graphql`
             }
           }
           organizationName
+          package
           status
+          ccbcNumber
           rowId
           projectName
           intakeId
@@ -46,23 +52,24 @@ const StyledContainer = styled.div`
   width: 100%;
 `;
 
-const AnalystDashboard = ({
+const Assessments = ({
   preloadedQuery,
 }: RelayProps<Record<string, unknown>, assessmentsTableQuery>) => {
   const query = usePreloadedQuery(getAssessmentsTableQuery, preloadedQuery);
-  const { session } = query;
+  const { allApplications, session } = query;
 
   return (
     <Layout session={session} title="Connecting Communities BC">
       <StyledContainer>
         <DashboardTabs session={session} />
         <TableTabs />
+        <AssessmentAssignmentTable allApplications={allApplications} />
       </StyledContainer>
     </Layout>
   );
 };
 
-export default withRelay(AnalystDashboard, getAssessmentsTableQuery, {
+export default withRelay(Assessments, getAssessmentsTableQuery, {
   ...defaultRelayOptions,
   variablesFromContext: (ctx) => {
     return defaultRelayOptions.variablesFromContext(ctx);
