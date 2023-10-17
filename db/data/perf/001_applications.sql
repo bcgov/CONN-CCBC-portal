e7da9499682adffb3339ba80c9c29552ec9a4092
Anthony Bushara <anthony@button.is>
2023-10-17 16:41:32 -0400

    chore: fix data fixtures

--- db/data/perf/001_applications.sql
+++ db/data/perf/001_applications.sql
@@ -15,7 +15,7 @@ set jwt.claims.sub to 'mockUser@ccbc_auth_user';
 insert into ccbc_public.ccbc_user
   (id, given_name, family_name, email_address, session_sub) overriding system value values
   (1, 'foo1', 'bar', 'foo1@bar.com', 'mockUser@ccbc_auth_user');
-  
+
 do
 $do_block$
 declare
@@ -25,7 +25,7 @@ begin
 for appNum in 1..100 loop
 
   select id into appId
-  from ccbc_public.create_application();
+  from ccbc_public.create_application('');
 
   update ccbc_public.form_data
   set json_data = format($$
