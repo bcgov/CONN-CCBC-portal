58235e6abd88ad9af8beadd97c83dc01c943ca53
marcellmueller <marcel@button.is>
2023-10-17 11:01:33 -0700

    feat: add analyst/assessments page test

---
+++ app/tests/pages/analyst/assessments.test.tsx
@@ -0,0 +1,136 @@
+import { mocked } from 'jest-mock';
+import { screen } from '@testing-library/react';
+import { isAuthenticated } from '@bcgov-cas/sso-express/dist/helpers';
+import Assessments from '../../../pages/analyst/assessments';
+import defaultRelayOptions from '../../../lib/relay/withRelayOptions';
+import PageTestingHelper from '../../utils/pageTestingHelper';
+import compiledAssessmentsTableQuery, {
+  assessmentsTableQuery,
+} from '../../../__generated__/assessmentsTableQuery.graphql';
+
+const mockQueryPayload = {
+  Query() {
+    return {
+      session: {
+        sub: '4e0ac88c-bf05-49ac-948f-7fd53c7a9fd6',
+        authRole: 'ccbc_analyst',
+      },
+      allApplications: {
+        edges: [
+          {
+            node: {
+              id: '1',
+              rowId: 1,
+              analystStatus: 'received',
+              projectName: 'Test Proj Name',
+              ccbcNumber: 'CCBC-010001',
+              organizationName: 'Test Org Name',
+              intakeNumber: 1,
+            },
+          },
+        ],
+      },
+      allAnalysts: {
+        nodes: [
+          {
+            rowId: 1,
+            givenName: 'Test',
+            familyName: '1',
+          },
+        ],
+      },
+    };
+  },
+};
+
+jest.mock('@bcgov-cas/sso-express/dist/helpers');
+window.scrollTo = jest.fn();
+
+const pageTestingHelper = new PageTestingHelper<assessmentsTableQuery>({
+  pageComponent: Assessments,
+  compiledQuery: compiledAssessmentsTableQuery,
+  defaultQueryResolver: mockQueryPayload,
+});
+
+describe('The index page', () => {
+  beforeEach(() => {
+    pageTestingHelper.reinit();
+    pageTestingHelper.setMockRouterValues({
+      pathname: '/analyst/assessments',
+    });
+  });
+
+  it('should redirect an unauthorized user', async () => {
+    const ctx = {
+      req: {
+        url: '/analyst/assessments',
+      },
+    } as any;
+
+    expect(await defaultRelayOptions.serverSideProps(ctx)).toEqual({
+      redirect: {
+        destination: '/analyst',
+      },
+    });
+  });
+
+  it('should redirect a user logged in with IDIR but not assigned a role', async () => {
+    mocked(isAuthenticated).mockReturnValue(true);
+
+    const ctx = {
+      req: {
+        url: '/analyst/assessments',
+        claims: {
+          client_roles: [],
+          identity_provider: 'idir',
+        },
+      },
+    } as any;
+
+    expect(await defaultRelayOptions.serverSideProps(ctx)).toEqual({
+      redirect: {
+        destination: '/analyst/request-access',
+      },
+    });
+  });
+
+  it('should not redirect a user logged in with IDIR and an assigned role', async () => {
+    mocked(isAuthenticated).mockReturnValue(true);
+
+    const ctx = {
+      req: {
+        url: '/analyst/assessments',
+        claims: {
+          client_roles: ['admin'],
+          identity_provider: 'idir',
+        },
+      },
+    } as any;
+
+    expect(await defaultRelayOptions.serverSideProps(ctx)).toEqual({});
+  });
+
+  it('should highlight the assessments tab', async () => {
+    pageTestingHelper.loadQuery();
+    pageTestingHelper.renderPage();
+
+    expect(screen.getByRole('link', { name: 'Assessments' })).toHaveStyle(
+      'font-weight: 700;'
+    );
+  });
+
+  it('should render the table', async () => {
+    pageTestingHelper.loadQuery();
+    pageTestingHelper.renderPage();
+
+    expect(screen.getByText('CCBC ID')).toBeInTheDocument();
+    expect(screen.getByText('Package')).toBeInTheDocument();
+    expect(screen.getByText('PM Assessment')).toBeInTheDocument();
+    expect(screen.getByText('Tech Assessment')).toBeInTheDocument();
+    expect(screen.getByText('Permitting Assessment')).toBeInTheDocument();
+    expect(screen.getByText('GIS Assessment')).toBeInTheDocument();
+    expect(screen.getByText('Target Date')).toBeInTheDocument();
+    expect(screen.getByText('Project Title')).toBeInTheDocument();
+    expect(screen.getByText('Organization Name')).toBeInTheDocument();
+  });
+});
