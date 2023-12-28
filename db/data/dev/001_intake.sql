a2057915740781326e4b692bb7a32dd1cc7d6b5c
Anthony Bushara <anthony@button.is>
2023-12-28 10:50:55 -0500

    chore: update dev data to create relevant intake

--- db/data/dev/001_intake.sql
+++ db/data/dev/001_intake.sql
@@ -17,7 +17,7 @@ truncate table
 restart identity cascade;
 
 insert into ccbc_public.gapless_counter(id, counter) overriding system value
- values (1, 0), (2, 0) on conflict (id) do update set
+ values (1, 0), (2, 0), (3, 0) on conflict (id) do update set
  counter= excluded.counter;
 
 insert into
@@ -25,7 +25,8 @@ insert into
 overriding system value
 values
   (1, '2022-08-19 09:00:00 America/Vancouver','2022-11-06 09:00:00 America/Vancouver', 1, 1),
-  (2, '2023-01-15 00:00:00 America/Vancouver','2023-03-15 00:00:00 America/Vancouver', 2, 2)
+  (2, '2023-01-15 00:00:00 America/Vancouver','2023-03-15 00:00:00 America/Vancouver', 2, 2),
+  (3, now(), now() + interval '10 days', 3, 3)
 on conflict (id) do update set
 open_timestamp = excluded.open_timestamp,
 close_timestamp = excluded.close_timestamp,
