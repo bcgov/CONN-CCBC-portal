c6616ff1203417f9dc5be90117b06aef65fd640f
R Ranathunga <rumesha.ranathunga@gov.bc.ca>
2024-02-08 08:32:14 -0800

    test: add gis e2e tests invalid scenarios

--- app/cypress/e2e/analyst/gis/upload.cy.js
+++ app/cypress/e2e/analyst/gis/upload.cy.js
@@ -29,5 +29,44 @@ describe('GIS Upload', () => {
         cy.contains('td', '30').should('exist');
         cy.contains('td', '1').should('exist');
       });
+
+    cy.contains('button', 'Import').click();
+    cy.contains('h1', 'GIS Analysis Import');
+    cy.contains(/GIS analysis added to 1 projects for the first time/);
+    cy.contains(/GIS analysis updated for 0 projects/);
+    cy.contains(/Total processed 1/);
+  });
+
+  it('upload invalid json with invalid schema', () => {
+    cy.visit('/analyst/dashboard');
+    cy.wait('@get-features');
+    cy.contains('a', 'GIS').click();
+    cy.url().should('include', '/analyst/gis');
+    cy.contains('h2', 'GIS Input');
+    cy.wait('@graphql');
+    cy.get('[data-testid=file-test]')
+      .first()
+      .selectFile('cypress/fixtures/gis_invalid.json', { force: true });
+    cy.contains('gis_invalid.json');
+    cy.contains('button', 'Continue').click();
+    cy.contains(/Error uploading JSON file/);
+    cy.contains(/GIS_TOTAL_HH must be number/);
+    cy.contains(/GIS_PERCENT_OVERBUILD must be number/);
+  });
+
+  it('upload invalid json - not an array', () => {
+    cy.visit('/analyst/dashboard');
+    cy.wait('@get-features');
+    cy.contains('a', 'GIS').click();
+    cy.url().should('include', '/analyst/gis');
+    cy.contains('h2', 'GIS Input');
+    cy.wait('@graphql');
+    cy.get('[data-testid=file-test]')
+      .first()
+      .selectFile('cypress/fixtures/gis_invalid_2.json', { force: true });
+    cy.contains('gis_invalid_2.json');
+    cy.contains('button', 'Continue').click();
+    cy.contains(/Error uploading JSON file/);
+    cy.contains(/must be array at line 1/);
   });
 });
