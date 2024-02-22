3374aeb7e2c6bee4a8664502f3735f3b6f98855e
R Ranathunga <rumesha.ranathunga@gov.bc.ca>
2024-02-22 06:05:00 -0800

    test: set intake timestamps static

--- app/cypress/e2e/admin/setup.cy.js
+++ app/cypress/e2e/admin/setup.cy.js
@@ -3,6 +3,8 @@ const testSetup = () => {
   const mockedDateString = '2023-10-10';
   const mockedDate = new Date(mockedDateString);
   cy.useMockedTime(mockedDate);
+  const now = new Date(Date.parse('2024-01-15')).getTime();
+  cy.clock(now, ['Date']);
   cy.sqlFixture('e2e/reset_db');
   cy.sqlFixture('e2e/001_intake');
   cy.sqlFixture('e2e/001_application');
