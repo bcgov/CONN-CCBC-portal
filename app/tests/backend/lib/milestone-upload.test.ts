f4d093e8baa39210cd3caadf5f4066889232e664
Anthony Bushara <anthony@button.is>
2024-02-22 11:17:12 -0500

    test: update test with new milestone data

--- app/tests/backend/lib/milestone-upload.test.ts
+++ app/tests/backend/lib/milestone-upload.test.ts
@@ -13,6 +13,113 @@ import getAuthRole from '../../../utils/getAuthRole';
 jest.mock('../../../backend/lib/graphql');
 jest.mock('../../../utils/getAuthRole');
 
+const expectedQueryInput = {
+  input: {
+    _applicationId: 1,
+    _jsonData: {
+      milestone1DateOfReception: '2023-01-01T00:00:00.000Z',
+      milestone1Progress: 0.2,
+      milestone1ProjectSites: [
+        {
+          isPOP: 'Y',
+          isRequired: true,
+          isSubmitted: true,
+          isedComments: 'This is an ISED Comment',
+          milestoneOneDueDuate: 46388,
+          projectSite: 'SiteName1',
+          recipientComments: 'Milestone 1 Recipient Comment',
+          siteId: 'SiteId1',
+          status: 'Approved',
+        },
+      ],
+      milestone2DateOfReception: '2023-01-01T00:00:00.000Z',
+      milestone2Progress: 0.3,
+      milestone2ProjectSites: [
+        {
+          detailedProgress: {
+            landAccessPermitEvidence: {
+              applicable: true,
+              progress: 0,
+            },
+            photographsOfProjectSites: {
+              applicable: true,
+              progress: 0,
+            },
+            pointOfPresenceConfirmation: {
+              applicable: true,
+              progress: 0,
+            },
+            radioAndSpectrumLicenses: {
+              applicable: false,
+              progress: 0,
+            },
+          },
+          isPOP: undefined,
+          isedComments: undefined,
+          landAccessPermitEvidenceIsRequired: undefined,
+          landAccessPermitEvidenceIsSubmitted: undefined,
+          landAccessPermitEvidenceStatus: 'Status:',
+          milestoneTwoDueDuate: undefined,
+          photographsOfProjectSitesIsRequired: undefined,
+          photographsOfProjectSitesIsSubmitted: undefined,
+          photographsOfProjectSitesStatus: 'Status:',
+          pointOfPresenceConfirmationIsRequired: undefined,
+          pointOfPresenceConfirmationIsSubmitted: undefined,
+          pointOfPresenceConfirmationStatus: 'Status:',
+          projectSite: undefined,
+          radioAndSpectrumLicensesIsRequired: undefined,
+          radioAndSpectrumLicensesIsSubmitted: undefined,
+          radioandspetrumLicensesStatus: 'Status:',
+          recipientComments: undefined,
+          siteId: undefined,
+        },
+        {
+          detailedProgress: {
+            landAccessPermitEvidence: {
+              applicable: true,
+              progress: 0,
+            },
+            photographsOfProjectSites: {
+              applicable: true,
+              progress: 0,
+            },
+            pointOfPresenceConfirmation: {
+              applicable: true,
+              progress: 0,
+            },
+            radioAndSpectrumLicenses: {
+              applicable: false,
+              progress: 0,
+            },
+          },
+          isPOP: 'Y',
+          isedComments: 'This is an ISED comment',
+          landAccessPermitEvidenceIsRequired: true,
+          landAccessPermitEvidenceIsSubmitted: true,
+          landAccessPermitEvidenceStatus: 'Incomplete',
+          milestoneTwoDueDuate: 46420,
+          photographsOfProjectSitesIsRequired: true,
+          photographsOfProjectSitesIsSubmitted: false,
+          photographsOfProjectSitesStatus: 'Incomplete',
+          pointOfPresenceConfirmationIsRequired: false,
+          pointOfPresenceConfirmationIsSubmitted: true,
+          pointOfPresenceConfirmationStatus: 'Approved',
+          projectSite: 'SiteName1',
+          radioAndSpectrumLicensesIsRequired: false,
+          radioAndSpectrumLicensesIsSubmitted: true,
+          radioandspetrumLicensesStatus: 'Approved',
+          recipientComments: 'This is a recipient comment',
+          siteId: 'SiteId1',
+        },
+      ],
+      milestone3Progress: 0.2,
+      overallMilestoneProgress: 0.7,
+      projectNumber: 'CCBC-010001',
+    },
+    _oldId: 1,
+  },
+};
+
 function FormDataMock() {
   this.append = jest.fn();
 }
@@ -143,7 +250,10 @@ describe('The Milestone excel import api route', () => {
       };
     });
 
-    mocked(performQuery).mockImplementation(async () => {
+    let queryInput = null;
+
+    mocked(performQuery).mockImplementation(async (_mutation, input) => {
+      queryInput = input;
       return {
         data: {
           applicationByRowId: {
@@ -151,13 +261,7 @@ describe('The Milestone excel import api route', () => {
               nodes: [
                 {
                   jsonData: {
-                    milestone1DateOfReception: '2021-01-01T00:00:00.000Z',
-                    milestone1Progress: '100%',
-                    milestone2DateOfReception: '2021-01-01T00:00:00.000Z',
-                    milestone2Progress: '100%',
-                    milestone3Progress: '100%',
-                    overallMilestoneProgress: '100%',
-                    projectNumber: 'CCBC-010001',
+                    ...expectedQueryInput.input._jsonData,
                   },
                   rowId: 1,
                 },
@@ -178,30 +282,7 @@ describe('The Milestone excel import api route', () => {
 
     expect(response.status).toBe(200);
 
-    expect(response.body).toEqual({
-      result: {
-        data: {
-          applicationByRowId: {
-            applicationMilestoneExcelDataByApplicationId: {
-              nodes: [
-                {
-                  jsonData: {
-                    milestone1DateOfReception: '2021-01-01T00:00:00.000Z',
-                    milestone1Progress: '100%',
-                    milestone2DateOfReception: '2021-01-01T00:00:00.000Z',
-                    milestone2Progress: '100%',
-                    milestone3Progress: '100%',
-                    overallMilestoneProgress: '100%',
-                    projectNumber: 'CCBC-010001',
-                  },
-                  rowId: 1,
-                },
-              ],
-            },
-          },
-        },
-      },
-    });
+    expect(queryInput).toEqual(expectedQueryInput);
   });
 
   afterEach(async () => {
