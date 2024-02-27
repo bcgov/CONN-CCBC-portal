fb8d0f3c275186f8ab67126d3825a51f79bfae3a
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2024-02-27 13:06:05 -0800

    test: send email tests

---
+++ app/tests/backend/lib/ches/sendEmail.test.ts
@@ -0,0 +1,54 @@
+import sendEmail from 'backend/lib/ches/sendEmail';
+
+describe('getAccessToken', () => {
+  it('should return a token when the request is successful', async () => {
+    // Mock the fetch response
+    // @ts-ignore
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () =>
+          Promise.resolve({
+            messages: [
+              {
+                msgId: 'mock_message_id',
+                to: ['mock_email@gov.bc.ca'],
+              },
+            ],
+            txId: 'mock_tx_id',
+          }),
+      })
+    );
+
+    const email = await sendEmail(
+      'test_token',
+      'test_body',
+      'test_subject',
+      'mock_email@gov.bc.ca',
+      'test_tag'
+    );
+    expect(email).toBe('mock_message_id');
+  });
+
+  it('should throw an error when the request fails', async () => {
+    // Mock the fetch response
+    // @ts-ignore
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        ok: false,
+        status: 422,
+      })
+    );
+
+    await expect(
+      sendEmail(
+        'test_token',
+        'test_body',
+        'test_subject',
+        'mock_email@gov.bc.ca',
+        'test_tag'
+      )
+    ).rejects.toThrow('Error sending email with status: 422');
+  });
+});
