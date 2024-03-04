d9af5b9b954197c23769bd7273532853f27b2207
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2024-03-04 13:59:02 -0800

    chore: set proper helm directory

--- lib/app_deploy_feature.sh
+++ lib/app_deploy_feature.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 echo "Deploying application to openshift $*"
 
-cd helm/app
+cd helm/feature
 helm dep up
 helm upgrade --install --atomic -f ./values.yaml "$@" . --timeout=20m0s
