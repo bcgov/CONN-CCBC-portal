717f7a5ab9ece421e836438f503246057b6cc24f
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2024-03-08 13:34:33 -0800

    chore: fake sqitch change

---
+++ db/deploy/computed_columns/fake.sql
@@ -0,0 +1,7 @@
+-- Deploy ccbc:computed_columns/fake to pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ db/revert/computed_columns/fake.sql
@@ -0,0 +1,7 @@
+-- Revert ccbc:computed_columns/fake from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
