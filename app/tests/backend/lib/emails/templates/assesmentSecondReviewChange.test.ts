3a476b9c44ccdaf7026499aeead71dc186a7462d
R Ranathunga <rumesha.ranathunga@gov.bc.ca>
2024-04-09 14:45:49 -0700

    test: assessment type based email content

--- app/tests/backend/lib/emails/templates/assesmentSecondReviewChange.test.ts
+++ app/tests/backend/lib/emails/templates/assesmentSecondReviewChange.test.ts
@@ -27,4 +27,75 @@ describe('assesmentSecondReviewChange template', () => {
       `<a href='http://mock_host.ca/analyst/application/1/assessments/screening'>Click here</a>`
     );
   });
+
+  it('should generates the correct body and subject based on the assessment type provided', () => {
+    const applicationId = '1';
+    const url = 'http://mock_host.ca';
+    const initiator = 'CCBC User';
+
+    const emailTemplatePM = assesmentSecondReviewChange(
+      applicationId,
+      url,
+      initiator,
+      { ccbcNumber: 'ABC', assessmentType: 'projectManagement' }
+    );
+    expect(emailTemplatePM.subject).toEqual(
+      `CCBC User has requested a 2nd Review for Eligibility Project Management - ABC`
+    );
+    expect(emailTemplatePM.body).toContain(
+      `<a href='http://mock_host.ca/analyst/application/1/assessments/project-management'>Click here</a>`
+    );
+
+    const emailTemplatePT = assesmentSecondReviewChange(
+      applicationId,
+      url,
+      initiator,
+      { ccbcNumber: 'ABC', assessmentType: 'permitting' }
+    );
+    expect(emailTemplatePT.subject).toEqual(
+      `CCBC User has requested a 2nd Review for Eligibility Permitting - ABC`
+    );
+    expect(emailTemplatePT.body).toContain(
+      `<a href='http://mock_host.ca/analyst/application/1/assessments/permitting'>Click here</a>`
+    );
+
+    const emailTemplateTech = assesmentSecondReviewChange(
+      applicationId,
+      url,
+      initiator,
+      { ccbcNumber: 'ABC', assessmentType: 'technical' }
+    );
+    expect(emailTemplateTech.subject).toEqual(
+      `CCBC User has requested a 2nd Review for Eligibility Technical - ABC`
+    );
+    expect(emailTemplateTech.body).toContain(
+      `<a href='http://mock_host.ca/analyst/application/1/assessments/technical'>Click here</a>`
+    );
+
+    const emailTemplateGis = assesmentSecondReviewChange(
+      applicationId,
+      url,
+      initiator,
+      { ccbcNumber: 'ABC', assessmentType: 'gis' }
+    );
+    expect(emailTemplateGis.subject).toEqual(
+      `CCBC User has requested a 2nd Review for Eligibility GIS - ABC`
+    );
+    expect(emailTemplateGis.body).toContain(
+      `<a href='http://mock_host.ca/analyst/application/1/assessments/gis'>Click here</a>`
+    );
+
+    const emailTemplateFR = assesmentSecondReviewChange(
+      applicationId,
+      url,
+      initiator,
+      { ccbcNumber: 'ABC', assessmentType: 'financialRisk' }
+    );
+    expect(emailTemplateFR.subject).toEqual(
+      `CCBC User has requested a 2nd Review for Eligibility Financial Risk - ABC`
+    );
+    expect(emailTemplateFR.body).toContain(
+      `<a href='http://mock_host.ca/analyst/application/1/assessments/financial-risk'>Click here</a>`
+    );
+  });
 });
