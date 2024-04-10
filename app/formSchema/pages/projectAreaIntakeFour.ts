8c92c0e2c1ef30e1bd9e2e568accb57e6cc09db9
Anthony Bushara <anthony@button.is>
2024-04-10 16:20:32 -0400

    chore: remove duplication

--- app/formSchema/pages/projectAreaIntakeFour.ts
+++ app/formSchema/pages/projectAreaIntakeFour.ts
@@ -3,47 +3,12 @@ import projectArea from './projectArea';
 
 const projectAreaIntakeFour: Record<string, RJSFSchema> = {
   projectArea: {
-    title: 'Project area',
-    type: 'object',
+    ...projectArea.projectArea,
     required: [
       'geographicArea',
       'projectSpanMultipleLocations',
       'firstNationsLed',
     ],
-    properties: {
-      ...projectArea.projectArea.properties,
-    },
-    dependencies: {
-      projectSpanMultipleLocations: {
-        oneOf: [
-          {
-            properties: {
-              projectSpanMultipleLocations: {
-                enum: [false],
-              },
-            },
-          },
-          {
-            properties: {
-              projectSpanMultipleLocations: {
-                enum: [true],
-              },
-              provincesTerritories: {
-                title:
-                  'If yes, select the provinces or territories (check all that apply):',
-                type: 'array',
-                items: {
-                  type: 'string',
-                  enum: ['Alberta', 'Northwest Territories', 'Yukon'],
-                },
-                uniqueItems: true,
-              },
-            },
-            required: ['provincesTerritories'],
-          },
-        ],
-      },
-    },
   },
 };
 
--- app/formSchema/uiSchema/pages/projectAreaIntakeFour.ts
+++ app/formSchema/uiSchema/pages/projectAreaIntakeFour.ts
@@ -1,72 +1,13 @@
-import { GeographicArea } from '../../../components/Form/CustomTitles';
+import projectArea from './projectArea';
 
 const projectAreaIntakeFour = {
-  'ui:order': [
-    'acceptedGeographicArea',
-    'firstNationsLed',
-    'geographicArea',
-    'projectZoneMap',
-    'projectSpanMultipleLocations',
-    'provincesTerritories',
-  ],
-  'ui:title': '',
+  ...projectArea,
   acceptedGeographicArea: {
     'ui:widget': 'ReadOnlyProjectAreaWidgetIntakeFour',
     'ui:options': {
       label: false,
     },
   },
-  firstNationsLed: {
-    'ui:widget': 'RadioWidget',
-  },
-  geographicArea: {
-    'ui:widget': 'CheckboxesWidget',
-    'ui:options': {
-      maxLength: 150,
-      singleSelection: true,
-      customTitle: GeographicArea,
-      kmzLink: true,
-    },
-  },
-  projectZoneMap: {
-    'ui:widget': 'ZoneMapWidget',
-    'ui:options': {
-      customTitle: true,
-    },
-  },
-  projectSpanMultipleLocations: {
-    'ui:widget': 'RadioWidget',
-  },
-  provincesTerritories: {
-    'ui:widget': 'CheckboxesWidget',
-  },
-  'ui:inline': [
-    {
-      columns: 1,
-      acceptedGeographicArea: 1,
-    },
-    {
-      columns: 2,
-      firstNationsLed: '1 / 2',
-      geographicArea: '1 / 2',
-      projectZoneMap: '2 / 3',
-      // only if you need to manupulate row spans within a grid row
-      rowspans: {
-        firstNationsLed: '1 / 2',
-        geographicArea: '2 / 3',
-        projectZoneMap: '1 / 3',
-      },
-    },
-  ],
-  'ui:inline:sm': [
-    {
-      columns: 1,
-      acceptedGeographicArea: '1 / 2',
-      firstNationsLed: '1 / 2',
-      geographicArea: '1 / 2',
-      projectZoneMap: '1 / 2',
-    },
-  ],
 };
 
 export default projectAreaIntakeFour;
