37e6acc7f12273a940874aba7dcd37497b0a6f85
Anthony Bushara <anthony@button.is>
2024-04-11 15:00:31 -0400

    chore: update kmz link for all zones map

--- app/data/externalConstants.ts
+++ app/data/externalConstants.ts
@@ -1,7 +1,9 @@
 export const ZONE_MAP_URL =
   'https://catalogue.data.gov.bc.ca/dataset/8fbfc57f-4381-4a10-a4e8-0f335c6fe39a/resource/e12440ae-0b5d-40dc-af4f-2098f0b9374f/download/internet-zones.kmz';
 
-export const ZONE_MAP_URL_INTAKE_4 = '';
+// the zones are the same, keeping this as an added variable in case there are changes in the future
+export const ZONE_MAP_URL_INTAKE_4 =
+  'https://catalogue.data.gov.bc.ca/dataset/8fbfc57f-4381-4a10-a4e8-0f335c6fe39a/resource/e12440ae-0b5d-40dc-af4f-2098f0b9374f/download/internet-zones.kmz';
 
 export const INTAKE_3_AREAS_OF_INTEREST =
   'https://catalogue.data.gov.bc.ca/dataset/connecting-communities-bc-intake-3-areas-of-interest';
