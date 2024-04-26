4db93edbc59c25f972552b3f4273e6398fea0944
Anthony Bushara <anthony@button.is>
2024-04-26 15:51:41 -0400

    refactor: refactor to use latest major version

--- app/backend/lib/express-helper.ts
+++ app/backend/lib/express-helper.ts
@@ -1,26 +1,38 @@
 import { NextFunction, Request, Response } from 'express';
+import formidable from 'formidable';
+import IncomingForm from 'formidable/Formidable';
 
-type AnyObject = Record<string, any>
-type TypedRequest<ReqBody = AnyObject & Request, QueryString = AnyObject> = Request<AnyObject, AnyObject, ReqBody, Partial<QueryString>>
+type AnyObject = Record<string, any>;
+type TypedRequest<
+  ReqBody = AnyObject & Request,
+  QueryString = AnyObject
+> = Request<AnyObject, AnyObject, ReqBody, Partial<QueryString>>;
 
-export type ExpressMiddleware<ReqBody = AnyObject, Res = AnyObject | string, QueryString = AnyObject> = (
+export type ExpressMiddleware<
+  ReqBody = AnyObject,
+  Res = AnyObject | string,
+  QueryString = AnyObject
+> = (
   req: TypedRequest<ReqBody, QueryString>,
   res: Response<Res>,
-  next: NextFunction,
+  next: NextFunction
 ) =>
   | Promise<void | Response<Res>>
   | Promise<void>
   | ExpressMiddleware<ReqBody, Res, QueryString>
   | Response<any, Record<string, any>>
-  | void
+  | void;
 
-  export const parseForm = (form, req) => {
-    return new Promise((resolve, reject) => {
-      form.parse(req, (err, fields, files) => { 
-        if (err) { 
-          return reject(err);
-        } 
-        return resolve(files);
-      });
+export const parseForm = (
+  form: IncomingForm,
+  req
+): Promise<formidable.Files<string>> => {
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(files);
     });
-  }
+  });
+};
