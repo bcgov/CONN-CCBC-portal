ab8602455401587d74029e9cff6d2ff925d84151
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2024-05-01 08:38:43 -0700

    test: updat test data for cbc projects

--- app/tests/pages/analyst/dashboard.test.tsx
+++ app/tests/pages/analyst/dashboard.test.tsx
@@ -92,6 +92,59 @@ const mockQueryPayload = {
           },
         ],
       },
+      allCbcProjects: {
+        nodes: [
+          {
+            jsonData: [
+              {
+                phase: 2,
+                intake: 1,
+                errorLog: [],
+                highwayKm: null,
+                projectType: 'Transport',
+                reviewNotes: 'Qtrly Report: Progress 0.39 -> 0.38',
+                transportKm: 124,
+                lastReviewed: '2023-07-11T00:00:00.000Z',
+                otherFunding: 265000,
+                projectTitle: 'Project 1',
+                dateAnnounced: '2019-07-02T00:00:00.000Z',
+                projectNumber: 5555,
+                projectStatus: 'Reporting Complete',
+                federalFunding: 555555,
+                householdCount: null,
+                applicantAmount: 555555,
+                bcFundingRequest: 5555555,
+                projectLocations: 'Location 1',
+                milestoneComments: 'Requested extension to March 31, 2024',
+                proposedStartDate: '2020-07-01T00:00:00.000Z',
+                primaryNewsRelease:
+                  'https://www.canada.ca/en/innovation-science-economic-development/news/2019/07/rural-communities-in-british-columbia-will-benefit-from-faster-internet.html',
+                projectDescription: 'Description 1',
+                totalProjectBudget: 5555555,
+                announcedByProvince: 'YES',
+                dateAgreementSigned: '2021-02-24T00:00:00.000Z',
+                changeRequestPending: 'No',
+                currentOperatingName: 'Internet company 1',
+                federalFundingSource: 'ISED-CTI',
+                transportProjectType: 'Fibre',
+                indigenousCommunities: 5,
+                proposedCompletionDate: '2023-03-31T00:00:00.000Z',
+                constructionCompletedOn: null,
+                dateApplicationReceived: null,
+                reportingCompletionDate: null,
+                applicantContractualName: 'Internet company 1',
+                dateConditionallyApproved: '2019-06-26T00:00:00.000Z',
+                eightThirtyMillionFunding: 'No',
+                projectMilestoneCompleted: 0.5,
+                communitiesAndLocalesCount: 5,
+                connectedCoastNetworkDependant: 'NO',
+                nditConditionalApprovalLetterSent: 'YES',
+                bindingAgreementSignedNditRecipient: 'YES',
+              },
+            ],
+          },
+        ],
+      },
     };
   },
 };
@@ -197,7 +250,7 @@ describe('The index page', () => {
     pageTestingHelper.loadQuery();
     pageTestingHelper.renderPage();
 
-    expect(screen.getByText('CCBC ID')).toBeInTheDocument();
+    expect(screen.getByText('Project ID')).toBeInTheDocument();
     expect(screen.getByText('Internal Status')).toBeInTheDocument();
     expect(screen.getByText('External Status')).toBeInTheDocument();
     expect(screen.getByText('Project title')).toBeInTheDocument();
@@ -249,7 +302,7 @@ describe('The index page', () => {
     pageTestingHelper.loadQuery();
     pageTestingHelper.renderPage();
 
-    const countRows = screen.getAllByText(/Showing 4 of 4 rows/i);
+    const countRows = screen.getAllByText(/Showing 5 of 5 rows/i);
     expect(countRows).toHaveLength(2);
   });
 
