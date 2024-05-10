3f582d41a2789562d5628740daebe7631807fdcd
Anthony Bushara <anthony@button.is>
2024-05-10 13:41:12 -0400

    chore: update names for table

--- db/deploy/tables/cbc_projects_001_permissions.sql
+++ db/deploy/tables/cbc_projects_001_permissions.sql
@@ -1,14 +1,15 @@
 -- Deploy ccbc:tables/cbc_projects_001_permissions to pg
 begin;
 do $grant$
+  perform ccbc_private.grant_permissions('select', 'cbc_data', 'cbc_admin');
 
-  perform ccbc_private.grant_permissions('select', 'cbc_projects', 'cbc_admin');
-  perform ccbc_private.grant_permissions('select', 'cbc_projects', 'ccbc_admin');
-  perform ccbc_private.grant_permissions('select', 'cbc_projects', 'ccbc_analyst');
+  perform ccbc_private.grant_permissions('select', 'cbc_data', 'ccbc_admin');
 
+  perform ccbc_private.grant_permissions('select', 'cbc_data', 'ccbc_analyst');
 
-  perform ccbc_private.grant_permissions('update', 'cbc_projects', 'cbc_admin');
-  perform ccbc_private.grant_permissions('insert', 'cbc_projects', 'cbc_admin');
+  perform ccbc_private.grant_permissions('update', 'cbc_data', 'cbc_admin');
+
+  perform ccbc_private.grant_permissions('insert', 'cbc_data', 'cbc_admin');
 end
 $grant$;
 commit;
--- db/revert/tables/cbc_projects_001_permissions.sql
+++ db/revert/tables/cbc_projects_001_permissions.sql
@@ -1,23 +1,23 @@
--- Revert ccbc:tables/cbc_projects_001_permissions from pg
+-- Revert ccbc:tables/cbc_data_001_permissions from pg
 
 begin;
 
 
-  -- perform ccbc_private.grant_permissions('select', 'cbc_projects', 'cbc_admin');
-  -- perform ccbc_private.grant_permissions('select', 'cbc_projects', 'ccbc_admin');
-  -- perform ccbc_private.grant_permissions('select', 'cbc_projects', 'ccbc_analyst');
+  -- perform ccbc_private.grant_permissions('select', 'cbc_data', 'cbc_admin');
+  -- perform ccbc_private.grant_permissions('select', 'cbc_data', 'ccbc_admin');
+  -- perform ccbc_private.grant_permissions('select', 'cbc_data', 'ccbc_analyst');
 
 
-  -- perform ccbc_private.grant_permissions('update', 'cbc_projects', 'cbc_admin');
-  -- perform ccbc_private.grant_permissions('insert', 'cbc_projects', 'cbc_admin');
+  -- perform ccbc_private.grant_permissions('update', 'cbc_data', 'cbc_admin');
+  -- perform ccbc_private.grant_permissions('insert', 'cbc_data', 'cbc_admin');
 
-  revoke select on ccbc_public.cbc_projects from ccbc_admin;
-  revoke select on ccbc_public.cbc_projects from ccbc_analyst;
-  revoke select on ccbc_public.cbc_projects from cbc_admin;
+  revoke select on ccbc_public.cbc_data from ccbc_admin;
+  revoke select on ccbc_public.cbc_data from ccbc_analyst;
+  revoke select on ccbc_public.cbc_data from cbc_admin;
 
 
-  revoke update on ccbc_public.cbc_projects from cbc_admin;
-  revoke insert on ccbc_public.cbc_projects from cbc_admin;
+  revoke update on ccbc_public.cbc_data from cbc_admin;
+  revoke insert on ccbc_public.cbc_data from cbc_admin;
 
 
 commit;
