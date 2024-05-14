792bc6a133f5c58a6c88d428812834649e5f5be1
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2024-05-14 15:40:11 -0700

    chore: properly escape background

--- app/components/Analyst/CBC/CbcChangeStatus.tsx
+++ app/components/Analyst/CBC/CbcChangeStatus.tsx
@@ -21,12 +21,13 @@ const StyledDropdown = styled.select<DropdownProps>`
   padding: 6px 12px;
   height: 32px;
   width: ${(props) => props.statusStyles?.pillWidth};
-  background: ${(props) => props.statusStyles?.backgroundColor}
-    url("data:image/svg+xml;utf8,<svg viewBox='0 0 140 140' width='24' height='24' xmlns='http://www.w3.org/2000/svg'>
-    <g><path d='m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z' fill='${(
-      props
-    ) => props.statusStyles?.primary.replace('#', '%23')}'/></g></svg>")
-    no-repeat;
+  background: ${(props) => `
+  ${props.statusStyles?.backgroundColor} url("data:image/svg+xml;utf8,
+  <svg viewBox='0 0 140 140' width='24' height='24' xmlns='http://www.w3.org/2000/svg'>
+    <g><path d='m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z' fill='${props.statusStyles?.primary.replace('#', '%23')}'/></g>
+  </svg>")
+  no-repeat;
+`};
   background-position: right 5px top 5px;
 
   :focus {
