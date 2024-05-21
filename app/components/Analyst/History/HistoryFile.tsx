78e2f55d0f615bf8aec1d41d570ace5e3c8364a4
R Ranathunga <rumesha.ranathunga@gov.bc.ca>
2024-05-21 14:17:48 -0700

    fix: history files not showing properly

--- app/components/Analyst/History/HistoryFile.tsx
+++ app/components/Analyst/History/HistoryFile.tsx
@@ -32,103 +32,105 @@ const HistoryFile = ({
     keepUnchangedValues: true,
     full: true,
   });
-  return filesDiff && filesDiff?.length > 0
-    ? (filesDiff[0][0] === '+' ||
-        filesDiff[0][0] === '-' ||
-        filesDiff[0][0] === '~') && (
-        <StyledTable>
-          <thead style={{ borderBottom: '2px solid #CCC' }}>
-            {tableTitle && (
-              <tr>
-                <th>Files</th>
-              </tr>
-            )}
-          </thead>
-          <tbody>
-            <tr data-testid={testId}>
-              <td style={tableTitle ? { paddingTop: '8px' } : {}}>{title}</td>
-              <td style={tableTitle ? { paddingTop: '8px' } : {}}>
-                <div>
-                  {filesDiff &&
-                    filesDiff.map((file) => {
-                      // A file was added
-                      if (file[0] === '+') {
-                        return (
-                          <div key={file[1].uuid}>
-                            Added file{' '}
+
+  const hasFileChanges =
+    filesDiff?.filter(
+      (file) => file[0] === '+' || file[0] === '-' || file[0] === '~'
+    ).length > 0;
+
+  return hasFileChanges ? (
+    <StyledTable>
+      <thead style={{ borderBottom: '2px solid #CCC' }}>
+        {tableTitle && (
+          <tr>
+            <th>Files</th>
+          </tr>
+        )}
+      </thead>
+      <tbody>
+        <tr data-testid={testId}>
+          <td style={tableTitle ? { paddingTop: '8px' } : {}}>{title}</td>
+          <td style={tableTitle ? { paddingTop: '8px' } : {}}>
+            <div>
+              {filesDiff &&
+                filesDiff.map((file) => {
+                  // A file was added
+                  if (file[0] === '+') {
+                    return (
+                      <div key={file[1].uuid}>
+                        Added file{' '}
+                        <DownloadLink
+                          uuid={file[1].uuid}
+                          fileName={file[1].name}
+                        />
+                      </div>
+                    );
+                  }
+                  // A file was removed
+                  if (file[0] === '-') {
+                    return (
+                      <div key={file[1].uuid}>
+                        Deleted file{' '}
+                        <del>
+                          <DownloadLink
+                            uuid={file[1].uuid}
+                            fileName={file[1].name}
+                          />
+                        </del>
+                      </div>
+                    );
+                  }
+                  // The object was modified (file replacement)
+                  if (file[0] === '~') {
+                    if (filesDiff.length === 1) {
+                      return (
+                        <div key={file[1].uuid}>
+                          Replaced file{' '}
+                          <del>
                             <DownloadLink
-                              uuid={file[1].uuid}
-                              fileName={file[1].name}
+                              uuid={file[1].uuid.__old}
+                              fileName={file[1].name.__old || file[1].name}
                             />
-                          </div>
-                        );
-                      }
-                      // A file was removed
-                      if (file[0] === '-') {
-                        return (
-                          <div key={file[1].uuid}>
-                            Deleted file{' '}
-                            <del>
-                              <DownloadLink
-                                uuid={file[1].uuid}
-                                fileName={file[1].name}
-                              />
-                            </del>
-                          </div>
-                        );
-                      }
-                      // The object was modified (file replacement)
-                      if (file[0] === '~') {
-                        if (filesDiff.length === 1) {
-                          return (
-                            <div key={file[1].uuid}>
-                              Replaced file{' '}
-                              <del>
-                                <DownloadLink
-                                  uuid={file[1].uuid.__old}
-                                  fileName={file[1].name.__old || file[1].name}
-                                />
-                              </del>{' '}
-                              with file{' '}
-                              <DownloadLink
-                                uuid={file[1].uuid.__new}
-                                fileName={file[1].name.__new || file[1].name}
-                              />
-                            </div>
-                          );
-                        }
-                        return (
-                          <>
-                            <div key={file[1].uuid}>
-                              Deleted file{' '}
-                              <del>
-                                <DownloadLink
-                                  uuid={file[1].uuid.__old}
-                                  fileName={file[1].name.__old}
-                                />
-                              </del>{' '}
-                            </div>
-                            <div key={file[1].uuid}>
-                              Added file{' '}
-                              <del>
-                                <DownloadLink
-                                  uuid={file[1].uuid.__new}
-                                  fileName={file[1].name.__new}
-                                />
-                              </del>{' '}
-                            </div>
-                          </>
-                        );
-                      }
-                      return null;
-                    })}
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </StyledTable>
-      )
-    : null;
+                          </del>{' '}
+                          with file{' '}
+                          <DownloadLink
+                            uuid={file[1].uuid.__new}
+                            fileName={file[1].name.__new || file[1].name}
+                          />
+                        </div>
+                      );
+                    }
+                    return (
+                      <>
+                        <div key={file[1].uuid}>
+                          Deleted file{' '}
+                          <del>
+                            <DownloadLink
+                              uuid={file[1].uuid.__old}
+                              fileName={file[1].name.__old}
+                            />
+                          </del>{' '}
+                        </div>
+                        <div key={file[1].uuid}>
+                          Added file{' '}
+                          <del>
+                            <DownloadLink
+                              uuid={file[1].uuid.__new}
+                              fileName={file[1].name.__new}
+                            />
+                          </del>{' '}
+                        </div>
+                      </>
+                    );
+                  }
+                  return null;
+                })}
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </StyledTable>
+  ) : null;
 };
 
 export default HistoryFile;
