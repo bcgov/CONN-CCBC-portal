518c774ecb5a61ad0cd6e13ad6954da7f8f3a08e
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2024-05-27 10:32:24 -0700

    chore: sonarcloud

--- app/components/Analyst/CBC/CbcForm.tsx
+++ app/components/Analyst/CBC/CbcForm.tsx
@@ -5,6 +5,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 import { RJSFSchema } from '@rjsf/utils';
 import Button from '@button-inc/bcgov-theme/Button';
 import ProjectTheme from '../Project/ProjectTheme';
+import { ProjectFormProps } from '../Project/ProjectFormProps';
 
 const LoadingContainer = styled.div`
   display: block;
@@ -43,41 +44,7 @@ const StyledBtn = styled(Button)`
   }
 `;
 
-interface Props {
-  additionalContext?: any;
-  before?: React.ReactNode;
-  children?: React.ReactNode;
-  formAnimationHeight?: number;
-  formAnimationHeightOffset?: number;
-  formData: any;
-  formHeader?: string | React.ReactNode | JSX.Element;
-  handleChange: any;
-  showEditBtn?: boolean;
-  /** The hidden submit button's ref, used to enforce validation on the form
-   *  (the red-outline we see on widgets) */
-  hiddenSubmitRef?: any;
-  isExpanded?: boolean;
-  isFormEditMode: boolean;
-  isFormAnimated?: boolean;
-  liveValidate?: boolean;
-  onSubmit: any;
-  resetFormData: any;
-  saveBtnText?: string;
-  saveBtnDisabled?: boolean;
-  cancelBtnDisabled?: boolean;
-  schema: RJSFSchema;
-  setFormData?: any;
-  setIsFormEditMode: any;
-  submitting?: boolean;
-  submittingText?: string;
-  theme?: any;
-  title: string;
-  uiSchema?: any;
-  saveDataTestId?: string;
-  validate?: any;
-}
-
-const CbcForm: React.FC<Props> = ({
+const CbcForm: React.FC<ProjectFormProps> = ({
   additionalContext,
   before,
   children,
