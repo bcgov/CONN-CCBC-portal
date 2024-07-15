341b68f0876a96cf1526f1985fe7884e51a55c72
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2024-07-15 13:50:50 -0700

    feat: edit cbc project number, use current operating name

---
+++ app/backend/lib/validation.ts
@@ -0,0 +1,46 @@
+import { Router } from 'express';
+import RateLimit from 'express-rate-limit';
+import getAuthRole from '../../utils/getAuthRole';
+import { performQuery } from './graphql';
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 2000,
+});
+
+const validation = Router();
+
+const getProjectNumberQuery = `
+  query GetProjectNumber($projectNumber: Int!) {
+    cbcByProjectNumber(projectNumber: $projectNumber) {
+      rowId
+      projectNumber
+    }
+  }
+`;
+
+validation.post(
+  '/api/validation/project-number-unique',
+  limiter,
+  async (req, res) => {
+    const authRole = getAuthRole(req);
+    const isRoleAuthorized = authRole?.pgRole === 'cbc_admin';
+    if (!isRoleAuthorized) {
+      return res.status(404).end();
+    }
+    const { projectNumber } = req.body;
+    const getProjectNumber = await performQuery(
+      getProjectNumberQuery,
+      {
+        projectNumber: parseInt(projectNumber, 10),
+      },
+      req
+    );
+    if (getProjectNumber.data.cbcByProjectNumber) {
+      return res.send(false);
+    }
+    return res.send(true);
+  }
+);
+
+export default validation;
---
+++ db/revert/tables/cbc_add_fk_update_constraint.sql
@@ -0,0 +1,7 @@
+-- Revert ccbc:tables/cbc_add_fk_update_constraint from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
