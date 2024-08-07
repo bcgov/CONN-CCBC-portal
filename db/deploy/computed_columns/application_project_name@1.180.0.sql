7cbfe492dfd0ceac38531ae124108c6886831bd1
R Ranathunga <rumesha.ranathunga@gov.bc.ca>
2024-08-07 11:32:46 -0700

    fix: project name computed column scripts

--- db/deploy/computed_columns/application_project_name@1.180.0.sql
+++ db/deploy/computed_columns/application_project_name@1.180.0.sql
@@ -3,11 +3,7 @@
 begin;
 
 create or replace function ccbc_public.application_project_name(application ccbc_public.application) returns text as $$
-  select coalesce(asd.json_data ->>'projectTitle', afd.json_data -> 'projectInformation' ->> 'projectTitle') from ccbc_public.application_form_data(application) as afd
-  left join
-  ccbc_public.application_sow_data as asd on asd.application_id = application.id
-  order by asd.id desc
-  limit 1;
+  select json_data  -> 'projectInformation' ->> 'projectTitle' from ccbc_public.application_form_data(application);
 $$ language sql stable;
 
 grant execute on function ccbc_public.application_project_name to ccbc_auth_user;
