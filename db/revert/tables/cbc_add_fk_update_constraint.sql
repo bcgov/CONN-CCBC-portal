a11c7cac9458881fd6986598e781e93405cb2288
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2024-08-09 10:58:49 -0700

    chore: proper revert syntax

--- db/revert/tables/cbc_add_fk_update_constraint.sql
+++ db/revert/tables/cbc_add_fk_update_constraint.sql
@@ -10,6 +10,6 @@ DROP CONSTRAINT fk_cbc_data;
 ALTER TABLE ccbc_public.cbc_data
 ADD CONSTRAINT cbc_data_project_number_fkey
 FOREIGN KEY (project_number)
-REFERENCES ccbc_public.cbc(project_number)
+REFERENCES ccbc_public.cbc(project_number);
 
 COMMIT;
