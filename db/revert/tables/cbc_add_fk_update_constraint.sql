ded9ba3f67c5a0a0d31dee110bc71804cc0dc5b1
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2024-08-09 10:57:20 -0700

    chore: add missing revert

--- db/revert/tables/cbc_add_fk_update_constraint.sql
+++ db/revert/tables/cbc_add_fk_update_constraint.sql
@@ -2,6 +2,14 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+BEGIN;
+
+ALTER TABLE ccbc_public.cbc_data
+DROP CONSTRAINT fk_cbc_data;
+
+ALTER TABLE ccbc_public.cbc_data
+ADD CONSTRAINT cbc_data_project_number_fkey
+FOREIGN KEY (project_number)
+REFERENCES ccbc_public.cbc(project_number)
 
 COMMIT;
