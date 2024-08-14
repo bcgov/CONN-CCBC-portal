2cef172553b1fb832d28409d245167b6ddc76419
R Ranathunga <rumesha.ranathunga@gov.bc.ca>
2024-08-14 15:48:03 -0700

    fix: sort order for status ccbc and cbc combined

--- app/components/AnalystDashboard/AssessmentAssignmentTable.tsx
+++ app/components/AnalystDashboard/AssessmentAssignmentTable.tsx
@@ -76,6 +76,17 @@ export const filterZones = (row, id, filterValue) => {
   return zones.some((zone) => parseInt(zone, 10) === parseInt(filterValue, 10));
 };
 
+export const sortStatus = (rowA, rowB, columnId) => {
+  const sortColumn =
+    columnId === 'analystStatus'
+      ? 'internalStatusOrder'
+      : 'externalStatusOrder';
+  const statusA = rowA.original?.[sortColumn] as number;
+  const statusB = rowB.original?.[sortColumn] as number;
+
+  return statusA - statusB;
+};
+
 export const sortZones = (rowA, rowB, columnId) => {
   const valueA = rowA.getValue(columnId) as Array<string>;
   const valueB = rowB.getValue(columnId) as Array<string>;
