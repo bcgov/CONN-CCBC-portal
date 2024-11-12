1ec087eadcf89968a214874bd7641f70dcb6fc67
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2024-11-12 15:40:32 -0800

    feat: move application dependencies to technical assessments

--- app/components/Analyst/History/HistoryContent.tsx
+++ app/components/Analyst/History/HistoryContent.tsx
@@ -91,7 +91,14 @@ const HistoryContent = ({
   const showHistoryDetails = useFeature('show_history_details').value;
   const isAnalyst = sessionSub.includes('idir') || externalAnalyst;
   const fullName = `${givenName} ${familyName}`;
-  const displayName = isAnalyst ? fullName : 'The applicant';
+  let displayName;
+  if (isAnalyst) {
+    displayName = fullName;
+  } else if (record?.archived_by === null) {
+    displayName = 'The system';
+  } else {
+    displayName = 'The applicant';
+  }
   const reasonForChange = record.reason_for_change || record.change_reason;
   const createdAtFormatted =
     op === 'UPDATE'
