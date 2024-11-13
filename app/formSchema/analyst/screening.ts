885d91f956b0abe07a8f63af08d2a5bc9e77b5ec
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2024-11-13 13:36:13 -0800

    chore: update screening schema

--- app/formSchema/analyst/screening.ts
+++ app/formSchema/analyst/screening.ts
@@ -47,14 +47,6 @@ const screening: RJSFSchema = {
       ],
       default: 'No decision',
     },
-    connectedCoastNetworkDependent: {
-      title: 'Connected Coast Network Dependent',
-      type: 'boolean',
-    },
-    crtcProjectDependent: {
-      title: 'CRTC Project Dependent',
-      type: 'boolean',
-    },
     contestingMap: {
       title: 'Contesting Map',
       type: 'array',
