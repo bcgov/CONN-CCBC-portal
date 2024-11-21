331b8fed05a690e9679d0220b5f2c3cfda6b0b1d
Anthony Bushara <anthony@button.is>
2024-11-21 10:56:35 -0500

    chore: increase testing for assessments e2e

--- app/cypress/e2e/analyst/application/[applicationId]/assessments/setup.cy.js
+++ app/cypress/e2e/analyst/application/[applicationId]/assessments/setup.cy.js
@@ -11,4 +11,43 @@ const assessmentsSetup = () => {
   cy.intercept('POST', '/graphql').as('graphql');
 };
 
+export const assessmentsSetupLoginAdmin = () => {
+  cy.mockLogin('ccbc_admin');
+  const mockedDateString = '2022-10-10';
+  const mockedDate = new Date(mockedDateString);
+  cy.useMockedTime(mockedDate);
+  cy.sqlFixture('e2e/reset_db');
+  cy.sqlFixture('e2e/001_intake');
+  cy.sqlFixture('e2e/001_application');
+  cy.sqlFixture('e2e/001_application_received');
+  cy.sqlFixture('e2e/001_analyst');
+  cy.intercept('POST', '/graphql').as('graphql');
+};
+
+export const assessmentsSetupLoginSuperAdmin = () => {
+  cy.mockLogin('super_admin');
+  const mockedDateString = '2022-10-10';
+  const mockedDate = new Date(mockedDateString);
+  cy.useMockedTime(mockedDate);
+  cy.sqlFixture('e2e/reset_db');
+  cy.sqlFixture('e2e/001_intake');
+  cy.sqlFixture('e2e/001_application');
+  cy.sqlFixture('e2e/001_application_received');
+  cy.sqlFixture('e2e/001_analyst');
+  cy.intercept('POST', '/graphql').as('graphql');
+};
+
+export const assessmentsSetupLoginCbcAdmin = () => {
+  cy.mockLogin('cbc_admin');
+  const mockedDateString = '2022-10-10';
+  const mockedDate = new Date(mockedDateString);
+  cy.useMockedTime(mockedDate);
+  cy.sqlFixture('e2e/reset_db');
+  cy.sqlFixture('e2e/001_intake');
+  cy.sqlFixture('e2e/001_application');
+  cy.sqlFixture('e2e/001_application_received');
+  cy.sqlFixture('e2e/001_analyst');
+  cy.intercept('POST', '/graphql').as('graphql');
+};
+
 export default assessmentsSetup;
