1a3e90da5cc807ddb7cc62512a96dee4ef99dfc5
R Ranathunga <rumesha.ranathunga@gov.bc.ca>
2024-11-25 06:28:36 -0800

    chore: filter package by null

--- app/components/AnalystDashboard/AllDashboard.tsx
+++ app/components/AnalystDashboard/AllDashboard.tsx
@@ -157,6 +157,9 @@ const ApplicantStatusCell = ({ cell }) => {
 
 const filterOutNullishs = (val) => val !== undefined && val !== null;
 
+const toLabelValuePair = (value) =>
+  value ? { label: value, value } : { label: 'Unassigned', value: ' ' };
+
 const accessorFunctionGeneratorInjectsEmptyString = (accessorKey) => {
   return (row) => row[accessorKey] ?? '';
 };
@@ -609,8 +612,8 @@ const AllDashboardTable: React.FC<Props> = ({ query }) => {
         allApplications.edges.map((edge) => edge.node.package?.toString())
       ),
     ]
-      .filter(filterOutNullishs)
-      .sort((a, b) => Number(a) - Number(b));
+      .map(toLabelValuePair)
+      .toSorted((a, b) => Number(a.value) - Number(b.value));
 
     return [
       {
