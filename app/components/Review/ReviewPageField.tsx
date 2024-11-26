9d3bc8bbcc2f70682ec29a386dc90b36a6278714
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2024-11-26 09:00:39 -0800

    chore: revert rjsf/utils

--- app/components/Review/ReviewPageField.tsx
+++ app/components/Review/ReviewPageField.tsx
@@ -93,7 +93,7 @@ const ReviewPageField: React.FC<FieldProps> = (props) => {
           required
           schema={schema.properties.acknowledgeIncomplete as RJSFSchema}
           uiSchema={uiSchema}
-          idSchema={idSchema.acknowledgeIncomplete as any}
+          idSchema={idSchema.acknowledgeIncomplete}
           formData={formData?.acknowledgeIncomplete}
           autofocus={false}
           disabled={!formContext.isEditable}
