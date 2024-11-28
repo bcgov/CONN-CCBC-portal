b2ee4a2a6bbe94399d7aed36880fee5e111540dc
Anthony Bushara <anthony@button.is>
2024-11-27 20:34:22 -0500

    test(e2e): extra testing for roles on other pages

--- app/cypress/e2e/analyst/cbc/[cbcId].cy.js
+++ app/cypress/e2e/analyst/cbc/[cbcId].cy.js
@@ -1,3 +1,16 @@
+const loadTest = (role) => {
+  cy.visit('/analyst/cbc/1');
+  cy.contains('h1', 'Project 1');
+  cy.contains('h2', 'Tombstone');
+  cy.contains('h2', 'Project type');
+  cy.contains('h2', 'Locations');
+  cy.contains('h2', 'Counts');
+  cy.contains('h2', 'Funding');
+  cy.contains('h2', 'Events and dates');
+  cy.contains('h2', 'Miscellaneous');
+  cy.contains('h2', 'Project data reviews');
+  cy.get('body').happoScreenshot({ component: `CBC project view - ${role}` });
+};
 describe('The cbc project view', () => {
   beforeEach(() => {
     cy.mockLogin('cbc_admin');
@@ -10,26 +23,29 @@ describe('The cbc project view', () => {
     cy.sqlFixture('e2e/001_application_received');
     cy.sqlFixture('e2e/001_analyst');
   });
-
-  it('loads', () => {
-    cy.visit('/analyst/cbc/1');
-    cy.contains('h1', 'Project 1');
-    cy.contains('h2', 'Tombstone');
-    cy.contains('h2', 'Project type');
-    cy.contains('h2', 'Locations');
-    cy.contains('h2', 'Counts');
-    cy.contains('h2', 'Funding');
-    cy.contains('h2', 'Events and dates');
-    cy.contains('h2', 'Miscellaneous');
-    cy.contains('h2', 'Project data reviews');
-    cy.get('body').happoScreenshot({ component: 'CBC project view' });
+  describe('cbc load tests for each role', () => {
+    const roles = ['ccbc_analyst', 'ccbc_admin', 'super_admin', 'cbc_admin'];
+    roles.forEach((role) => {
+      it(`loads for ${role}`, () => {
+        loadTest(role);
+      });
+    });
   });
 
-  it('triggers quick edit', () => {
+  it('triggers quick edit cbc_admin', () => {
     cy.visit('/analyst/cbc/1');
     cy.get('button').contains('Quick edit').click();
     cy.get('body').happoScreenshot({
-      component: 'CBC project view - quick edit',
+      component: 'CBC project view - quick edit cbc_admin',
+    });
+  });
+
+  it('triggers quick edit cbc_admin', () => {
+    cy.mockLogin('super_admin');
+    cy.visit('/analyst/cbc/1');
+    cy.get('button').contains('Quick edit').click();
+    cy.get('body').happoScreenshot({
+      component: 'CBC project view - quick edit super_admin',
     });
   });
 });
