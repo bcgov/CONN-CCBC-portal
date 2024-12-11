6fc3349614217fcbfa1314f67c0d95358fb09aeb
Anthony Bushara <anthony@button.is>
2024-12-11 11:33:49 -0500

    feat: sow data is traversed for amendments

--- app/pages/analyst/application/[applicationId]/summary.tsx
+++ app/pages/analyst/application/[applicationId]/summary.tsx
@@ -94,10 +94,11 @@ const getSummaryQuery = graphql`
     # Cannot run it inside the above due to conflict of filter with header
     allApplicationSowData(
       filter: { applicationId: { equalTo: $rowId } }
+      orderBy: AMENDMENT_NUMBER_DESC
       condition: { archivedAt: null }
-      last: 1
     ) {
       nodes {
+        amendmentNumber
         rowId
         jsonData
         sowTab1SBySowId {
