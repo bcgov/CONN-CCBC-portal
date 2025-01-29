cc667240b2f13474ac304fdef9f273cd08a9e5ce
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2025-01-29 10:28:13 -0800

    chore: handle azureidir as idir

--- db/deploy/functions/session.sql
+++ db/deploy/functions/session.sql
@@ -9,10 +9,27 @@ create or replace function ccbc_public.session()
 $function$
 declare
   _sub text := current_setting('jwt.claims.sub', true);
+  _preferred_username text := current_setting('jwt.claims.preferred_username', true);
+  _identity_provider text := current_setting('jwt.claims.identity_provider', true);
 begin
   if ((coalesce(trim(_sub), '') = '') is not false) then
     return null; -- ensure null, empty and whitespace _sub claims are filtered out
   end if;
+
+  -- Replace @azureidir with @idir in sub and preferred_username
+  if _sub like '%@azureidir' then
+    _sub := replace(_sub, '@azureidir', '@idir');
+  end if;
+
+  if _preferred_username like '%@azureidir' then
+    _preferred_username := replace(_preferred_username, '@azureidir', '@idir');
+  end if;
+
+  -- Set identity_provider to idir if it is azureidir
+  if _identity_provider = 'azureidir' then
+    _identity_provider := 'idir';
+  end if;
+
   return (
     select row (
       current_setting('jwt.claims.jti', true),
@@ -21,7 +38,7 @@ begin
       current_setting('jwt.claims.iat', true),
       current_setting('jwt.claims.iss', true),
       current_setting('jwt.claims.aud', true),
-      current_setting('jwt.claims.sub', true),
+      _sub,
       current_setting('jwt.claims.typ', true),
       current_setting('jwt.claims.azp', true),
       current_setting('jwt.claims.auth_time', true),
@@ -29,13 +46,13 @@ begin
       current_setting('jwt.claims.acr', true),
       current_setting('jwt.claims.email_verified', true),
       current_setting('jwt.claims.name', true),
-      current_setting('jwt.claims.preferred_username', true),
+      _preferred_username,
       current_setting('jwt.claims.given_name', true),
       current_setting('jwt.claims.family_name', true),
       current_setting('jwt.claims.email', true),
       current_setting('jwt.claims.broker_session_id', true),
       current_setting('jwt.claims.priority_group', true),
-      current_setting('jwt.claims.identity_provider', true),
+      _identity_provider,
       (select string_to_array(current_setting('jwt.claims.user_groups', true), ',')),
       current_setting('role', true)
     )::ccbc_public.keycloak_jwt
