f52362a9f20ff1056c0f163f00108e28a38d59a6
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2025-01-29 11:20:16 -0800

    chore: handle azureidir

--- app/data/ssoConstants.ts
+++ app/data/ssoConstants.ts
@@ -1,6 +1,6 @@
 export const IDP_HINTS = {
   'Basic BCeID': 'bceidbasic',
   'Business BCeID': 'bceidbusiness',
-  IDIR: 'idir',
+  IDIR: 'azureidir',
 };
 export const IDP_HINT_PARAM = 'kc_idp_hint';
