9d321b832572f11ac70af77b1305a419b27d69fb
Anthony Bushara <anthony@button.is>
2025-02-04 18:03:27 -0500

    chore: work done so far

--- app/pages/applicantportal/form/[id]/rfi/[applicantRfiId].tsx
+++ app/pages/applicantportal/form/[id]/rfi/[applicantRfiId].tsx
@@ -15,9 +15,11 @@ import { useRouter } from 'next/router';
 import FormDiv from 'components/FormDiv';
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
-import { useCreateNewFormDataMutation } from 'schema/mutations/application/createNewFormData';
 import useEmailNotification from 'lib/helpers/useEmailNotification';
 import useRfiCoverageMapKmzUploadedEmail from 'lib/helpers/useRfiCoverageMapKmzUploadedEmail';
+import { useUpdateRfiAndCreateTemplateNineDataMutation } from 'schema/mutations/application/updateRfiAndCreateTemplateNineDataMutation';
+import { useUpdateRfiAndFormDataMutation } from 'schema/mutations/application/updateRfiAndFormDataMutation';
+import { useUpdateFormRfiAndCreateTemplateNineDataMutation } from 'schema/mutations/application/updateFormRfiAndCreateTemplateNineDataMutation';
 
 const Flex = styled('header')`
   display: flex;
@@ -64,14 +66,26 @@ const ApplicantRfiPage = ({
   const { session, rfiDataByRowId, applicationByRowId } = query;
   const { rfiNumber } = rfiDataByRowId;
   const [updateRfi] = useUpdateWithTrackingRfiMutation();
+  const [updateRfiAndCreateTemplateNineData] =
+    useUpdateRfiAndCreateTemplateNineDataMutation();
+  const [updateFormRfiAndCreateTemplateNineData] =
+    useUpdateFormRfiAndCreateTemplateNineDataMutation();
+  const [updateRfiAndFormData] = useUpdateRfiAndFormDataMutation();
   const router = useRouter();
   const formJsonData = applicationByRowId?.formData?.jsonData;
   const applicationId = router.query.id as string;
   const formSchemaId = applicationByRowId?.formData?.formSchemaId;
   const ccbcNumber = applicationByRowId?.ccbcNumber;
   const [newFormData, setNewFormData] = useState(formJsonData);
-  const [createNewFormData] = useCreateNewFormDataMutation();
+  const [hasApplicationFormDataUpdated, setHasApplicationFormDataUpdated] =
+    useState(false);
+  const [templateNineData, setTemplateNineData] = useState(null);
   const [templateData, setTemplateData] = useState(null);
+  const [templatesUpdated, setTemplatesUpdated] = useState({
+    one: false,
+    two: false,
+    nine: false,
+  });
   const [formData, setFormData] = useState(rfiDataByRowId.jsonData);
   const { notifyHHCountUpdate } = useEmailNotification();
   const { notifyRfiCoverageMapKmzUploaded } =
@@ -89,6 +103,10 @@ const ApplicantRfiPage = ({
         },
       };
       setNewFormData(newFormDataWithTemplateOne);
+      setTemplatesUpdated((prevTemplatesUpdated) => {
+        return { ...prevTemplatesUpdated, one: true };
+      });
+      setHasApplicationFormDataUpdated(true);
     } else if (templateData?.templateNumber === 2 && !templateData.error) {
       const newFormDataWithTemplateTwo = {
         ...newFormData,
@@ -99,7 +117,21 @@ const ApplicantRfiPage = ({
         },
       };
       setNewFormData(newFormDataWithTemplateTwo);
-    } else if (templateData?.error && templateData?.templateNumber === 1) {
+      setTemplatesUpdated((prevTemplatesUpdated) => {
+        return { ...prevTemplatesUpdated, two: true };
+      });
+      setHasApplicationFormDataUpdated(true);
+    } else if (templateData?.templateNumber === 9 && !templateData.error) {
+      setTemplatesUpdated((prevTemplatesUpdated) => {
+        return { ...prevTemplatesUpdated, nine: true };
+      });
+      setTemplateNineData({ ...templateData });
+    } else if (
+      templateData?.error &&
+      (templateData?.templateNumber === 1 ||
+        templateData?.templateNumber === 2 ||
+        templateData?.templateNumber === 9)
+    ) {
       const fileArrayLength =
         newFormData.templateUploads?.eligibilityAndImpactsCalculator?.length;
       fetch(`/api/email/notifyFailedReadOfTemplateData`, {
@@ -119,66 +151,115 @@ const ApplicantRfiPage = ({
           },
         }),
       });
-    } else if (templateData?.error && templateData?.templateNumber === 2) {
-      const fileArrayLength =
-        newFormData.templateUploads?.detailedBudget?.length;
-      fetch(`/api/email/notifyFailedReadOfTemplateData`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          applicationId,
-          host: window.location.origin,
-          params: {
-            templateNumber: templateData.templateNumber,
-            uuid: newFormData.templateUploads?.detailedBudget?.[
-              fileArrayLength - 1
-            ]?.uuid,
-            uploadedAt:
-              newFormData.templateUploads?.detailedBudget?.[fileArrayLength - 1]
-                ?.uploadedAt,
-          },
-        }),
-      });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [templateData]);
 
   const handleSubmit = (e: IChangeEvent<any>) => {
-    updateRfi({
-      variables: {
-        input: {
-          jsonData: e.formData,
-          rfiRowId: rfiDataByRowId.rowId,
-        },
-      },
-      onCompleted: () => {
-        if (e.formData?.rfiAdditionalFiles?.geographicCoverageMap?.length > 0) {
-          notifyRfiCoverageMapKmzUploaded(
-            rfiDataByRowId,
-            e.formData,
-            applicationId,
+    const getTemplateNineUUID = () => {
+      // can be wrong source if there are multiple uploads
+      return e.formData?.rfiAdditionalFiles?.geographicNames?.[0]?.uuid;
+    };
+
+    const checkAndNotifyRfiCoverage = async () => {
+      if (e.formData?.rfiAdditionalFiles?.geographicCoverageMap?.length > 0) {
+        return notifyRfiCoverageMapKmzUploaded(
+          rfiDataByRowId,
+          e.formData,
+          applicationId,
+          ccbcNumber,
+          rfiNumber,
+          applicationByRowId.organizationName
+        );
+      }
+      return Promise.resolve();
+    };
+
+    const checkAndNotifyHHCount = async () => {
+      if (templatesUpdated?.one) {
+        return notifyHHCountUpdate(
+          newFormData.benefits,
+          formJsonData.benefits,
+          applicationId,
+          {
             ccbcNumber,
+            timestamp: new Date().toLocaleString(),
+            manualUpdate: false,
             rfiNumber,
-            applicationByRowId.organizationName
-          );
-        }
-        if (!templateData) {
-          router.push(`/applicantportal/dashboard`);
-        }
-      },
-      onError: (err) => {
-        // eslint-disable-next-line no-console
-        console.log('Error updating RFI', err);
-      },
-    });
-    if (templateData) {
-      createNewFormData({
+            organizationName: applicationByRowId.organizationName,
+          }
+        );
+      }
+      return Promise.resolve();
+    };
+
+    if (!hasApplicationFormDataUpdated && !templatesUpdated.nine) {
+      // form data not updated and template nine not updated
+      // only update rfi
+      updateRfi({
         variables: {
           input: {
+            jsonData: e.formData,
+            rfiRowId: rfiDataByRowId.rowId,
+          },
+        },
+        onCompleted: () => {
+          checkAndNotifyRfiCoverage().then(() => {
+            // wait until email is sent before redirecting
+            router.push(`/applicantportal/dashboard`);
+          });
+        },
+        onError: (err) => {
+          // eslint-disable-next-line no-console
+          console.log('Error updating RFI', err);
+        },
+      });
+    } else if (!hasApplicationFormDataUpdated && templatesUpdated.nine) {
+      // form data not updated but template nine updated, update rfi and create template nine record
+      updateRfiAndCreateTemplateNineData({
+        variables: {
+          rfiInput: {
+            jsonData: e.formData,
+            rfiRowId: rfiDataByRowId.rowId,
+          },
+          templateNineInput: {
+            applicationFormTemplate9Data: {
+              applicationId: Number(applicationId),
+              jsonData: templateNineData.data,
+              source: {
+                source: 'RFI',
+                uuid: getTemplateNineUUID(),
+              },
+            },
+          },
+        },
+        onError: (err) => {
+          // eslint-disable-next-line no-console
+          console.log(
+            'Error updating RFI and creating template nine data',
+            err
+          );
+        },
+        onCompleted: () => {
+          checkAndNotifyRfiCoverage().then(() => {
+            // wait until email(s) is sent before redirecting
+            router.push(`/applicantportal/dashboard`);
+          });
+        },
+      });
+    } else if (hasApplicationFormDataUpdated && !templatesUpdated.nine) {
+      // only update rfi and form data since no template nine data
+      updateRfiAndFormData({
+        variables: {
+          formInput: {
             applicationRowId: Number(applicationId),
             jsonData: newFormData,
             reasonForChange: `Auto updated from upload for RFI: ${rfiNumber}`,
             formSchemaId,
+          },
+          rfiInput: {
+            jsonData: e.formData,
+            rfiRowId: rfiDataByRowId.rowId,
           },
         },
         onError: (err) => {
@@ -186,22 +267,54 @@ const ApplicantRfiPage = ({
           console.log('Error creating new form data', err);
         },
         onCompleted: () => {
-          if (templateData?.templateNumber === 1) {
-            notifyHHCountUpdate(
-              newFormData.benefits,
-              formJsonData.benefits,
-              applicationId,
-              {
-                ccbcNumber,
-                timestamp: new Date().toLocaleString(),
-                manualUpdate: false,
-                rfiNumber,
-                organizationName: applicationByRowId.organizationName,
-              }
-            );
-          }
           setTemplateData(null);
-          router.push(`/applicantportal/dashboard`);
+          checkAndNotifyRfiCoverage().then(() => {
+            checkAndNotifyHHCount().then(() => {
+              // wait until email is sent before redirecting
+              router.push(`/applicantportal/dashboard`);
+            });
+          });
+        },
+      });
+    } else if (hasApplicationFormDataUpdated && templatesUpdated.nine) {
+      // update rfi, form data, and template nine data (all three)
+      updateFormRfiAndCreateTemplateNineData({
+        variables: {
+          formInput: {
+            applicationRowId: Number(applicationId),
+            jsonData: newFormData,
+            reasonForChange: `Auto updated from upload for RFI: ${rfiNumber}`,
+            formSchemaId,
+          },
+          rfiInput: {
+            jsonData: e.formData,
+            rfiRowId: rfiDataByRowId.rowId,
+          },
+          templateNineInput: {
+            applicationFormTemplate9Data: {
+              applicationId: Number(applicationId),
+              jsonData: templateNineData.data,
+              source: {
+                source: 'RFI',
+                uuid: getTemplateNineUUID(),
+              },
+            },
+          },
+        },
+        onError: (err) => {
+          // eslint-disable-next-line no-console
+          console.log(
+            'Error updating RFI, form data, and template nine data',
+            err
+          );
+        },
+        onCompleted: () => {
+          checkAndNotifyHHCount().then(() => {
+            checkAndNotifyRfiCoverage().then(() => {
+              // wait until email(s) is sent before redirecting
+              router.push(`/applicantportal/dashboard`);
+            });
+          });
         },
       });
     }
