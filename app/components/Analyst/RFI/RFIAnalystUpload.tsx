ba8b286bb5c85d133497a000eb9d112db23c39dc
Anthony Bushara <anthony@button.is>
2025-02-05 12:51:06 -0500

    chore: add backend functionality

--- app/components/Analyst/RFI/RFIAnalystUpload.tsx
+++ app/components/Analyst/RFI/RFIAnalystUpload.tsx
@@ -9,13 +9,16 @@ import { rfiAnalystUiSchema } from 'formSchema/uiSchema/analyst/rfiUiSchema';
 import { useRouter } from 'next/router';
 import { useUpdateWithTrackingRfiMutation } from 'schema/mutations/application/updateWithTrackingRfiMutation';
 import styled from 'styled-components';
+import { DateTime } from 'luxon';
 
-import { useCreateNewFormDataMutation } from 'schema/mutations/application/createNewFormData';
 import useEmailNotification from 'lib/helpers/useEmailNotification';
 import useRfiCoverageMapKmzUploadedEmail from 'lib/helpers/useRfiCoverageMapKmzUploadedEmail';
 import { useToast } from 'components/AppProvider';
 import Link from 'next/link';
 import joinWithAnd from 'utils/formatArray';
+import { useUpdateRfiAndCreateTemplateNineDataMutation } from 'schema/mutations/application/updateRfiAndCreateTemplateNineDataMutation';
+import { useUpdateFormRfiAndCreateTemplateNineDataMutation } from 'schema/mutations/application/updateFormRfiAndCreateTemplateNineDataMutation';
+import { useUpdateRfiAndFormDataMutation } from 'schema/mutations/application/updateRfiAndFormDataMutation';
 
 const Flex = styled('header')`
   display: flex;
@@ -39,6 +42,13 @@ const RfiAnalystUpload = ({ query }) => {
             formSchemaId
             jsonData
           }
+          applicationFormTemplate9DataByApplicationId(
+            filter: { archivedAt: { isNull: true } }
+          ) {
+            nodes {
+              rowId
+            }
+          }
           ...RfiFormStatus_application
         }
         rfiDataByRowId(rowId: $rfiId) {
@@ -58,18 +68,33 @@ const RfiAnalystUpload = ({ query }) => {
     formData: { formSchemaId, jsonData },
     rowId: applicationId,
     ccbcNumber,
+    applicationFormTemplate9DataByApplicationId,
   } = applicationByRowId;
   const { showToast, hideToast } = useToast();
 
   const { rfiNumber } = rfiDataByRowId;
+  const applicationFormTemplate9DataId =
+    applicationFormTemplate9DataByApplicationId?.nodes?.[0]?.rowId;
 
-  const [createNewFormData] = useCreateNewFormDataMutation();
   const [updateRfi] = useUpdateWithTrackingRfiMutation();
+  const [updateRfiAndCreateTemplateNineData] =
+    useUpdateRfiAndCreateTemplateNineDataMutation();
+  const [updateFormRfiAndCreateTemplateNineData] =
+    useUpdateFormRfiAndCreateTemplateNineDataMutation();
+  const [updateRfiAndFormData] = useUpdateRfiAndFormDataMutation();
   const [rfiFormData, setRfiFormData] = useState(rfiDataByRowId?.jsonData);
   const [newFormData, setNewFormData] = useState(jsonData);
   const [templateData, setTemplateData] = useState(null);
+  const [templateNineData, setTemplateNineData] = useState(null);
   const [excelImportFields, setExcelImportFields] = useState([]);
   const [excelImportFiles, setExcelImportFiles] = useState([]);
+  const [hasApplicationFormDataUpdated, setHasApplicationFormDataUpdated] =
+    useState(false);
+  const [templatesUpdated, setTemplatesUpdated] = useState({
+    one: false,
+    two: false,
+    nine: false,
+  });
   const router = useRouter();
   const { notifyHHCountUpdate, notifyDocumentUpload } = useEmailNotification();
   const { notifyRfiCoverageMapKmzUploaded } =
@@ -89,6 +114,11 @@ const RfiAnalystUpload = ({ query }) => {
         },
       };
       setNewFormData(newFormDataWithTemplateOne);
+      setTemplatesUpdated((prevTemplatesUpdated) => {
+        return { ...prevTemplatesUpdated, one: true };
+      });
+      setTemplateData(null);
+      setHasApplicationFormDataUpdated(true);
     } else if (templateData?.templateNumber === 2 && !templateData?.error) {
       setExcelImportFields([...excelImportFields, 'Template 2']);
       setExcelImportFiles([...excelImportFiles, templateData?.templateName]);
@@ -101,101 +131,157 @@ const RfiAnalystUpload = ({ query }) => {
         },
       };
       setNewFormData(newFormDataWithTemplateTwo);
+      setTemplatesUpdated((prevTemplatesUpdated) => {
+        return { ...prevTemplatesUpdated, two: true };
+      });
+      setTemplateData(null);
+      setHasApplicationFormDataUpdated(true);
     } else if (templateData?.templateNumber === 9 && !templateData?.error) {
       setExcelImportFields([...excelImportFields, 'Template 9']);
       setExcelImportFiles([...excelImportFiles, templateData?.templateName]);
+      setTemplateNineData(templateData);
+      setTemplatesUpdated((prevTemplatesUpdated) => {
+        return { ...prevTemplatesUpdated, nine: true };
+      });
+      setTemplateData(null);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [templateData]);
 
-  const getToastMessage = () => {
-    return (
-      <>
-        {' '}
-        Template {templateData?.templateNumber} data changed successfully, new
-        values for{' '}
-        {templateData?.templateNumber === 1
-          ? 'Total Households and Indigenous Households'
-          : 'Total eligible costs and Total project costs'}{' '}
-        data in the application now reflect template uploads. Please see{' '}
-        <StyledLink
-          href={`/analyst/application/${router.query.applicationId}/history`}
-        >
-          history page
-        </StyledLink>{' '}
-        for details.
-      </>
-    );
+  const getToastMessage = (templateNumber) => (
+    <>
+      Template {templateNumber} data changed successfully, new values for{' '}
+      {templateNumber === 1
+        ? 'Total Households and Indigenous Households'
+        : 'Total eligible costs and Total project costs'}{' '}
+      data in the application now reflect template uploads. Please see{' '}
+      <StyledLink
+        href={`/analyst/application/${router.query.applicationId}/history`}
+      >
+        history page
+      </StyledLink>{' '}
+      for details.
+      <br />
+    </>
+  );
+
+  const processUpload = async (mutation, payload) => {
+    const successCallback = () => {
+      setTemplateData(null);
+      // show toast messages for uploaded/updated data
+      if (
+        templatesUpdated?.one ||
+        templatesUpdated?.two ||
+        templatesUpdated?.nine
+      ) {
+        const message = (
+          <>
+            {templatesUpdated.nine && (
+              <>
+                Template 9 processing successful, geographic names have been
+                updated for this application.
+                <br />
+              </>
+            )}
+            {templatesUpdated.one && getToastMessage(1)}
+            {templatesUpdated.two && getToastMessage(2)}
+          </>
+        );
+        showToast(message, 'success', 100000000);
+      }
+      notifyDocumentUpload(applicationId, {
+        ccbcNumber,
+        documentType: joinWithAnd(excelImportFields),
+        documentNames: excelImportFiles,
+      });
+      if (rfiFormData?.rfiAdditionalFiles?.geographicCoverageMap?.length > 0) {
+        notifyRfiCoverageMapKmzUploaded(
+          rfiDataByRowId,
+          rfiFormData,
+          applicationId,
+          ccbcNumber,
+          rfiNumber
+        );
+      }
+      if (templatesUpdated?.one) {
+        notifyHHCountUpdate(
+          newFormData.benefits,
+          jsonData.benefits,
+          applicationId,
+          {
+            ccbcNumber,
+            timestamp: new Date().toLocaleString(),
+            manualUpdate: false,
+            rfiNumber,
+          }
+        );
+      }
+      router.push(`/analyst/application/${router.query.applicationId}/rfi`);
+    };
+
+    const errorCallback = (err) => {
+      // eslint-disable-next-line no-console
+      console.log('Error updating RFI', err);
+    };
+
+    // call mutation based on documents updated
+    mutation({
+      variables: payload,
+      onCompleted: successCallback,
+      onError: errorCallback,
+    });
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
+    hideToast();
     const updatedExcelFields = excelImportFields.join(', ');
     const reasonForChange = `Auto updated from upload of ${updatedExcelFields} for RFI: ${rfiNumber}`;
-    hideToast();
-    updateRfi({
-      variables: {
-        input: {
-          jsonData: rfiFormData,
-          rfiRowId: rfiDataByRowId.rowId,
-        },
+    const payloadRfi = {
+      jsonData: rfiFormData,
+      rfiRowId: rfiDataByRowId.rowId,
+    };
+    const payloadFormData = {
+      applicationRowId: Number(applicationId),
+      jsonData: newFormData,
+      reasonForChange,
+      formSchemaId,
+    };
+    const payloadTemplateNine = {
+      _applicationId: Number(applicationId),
+      _jsonData: templateNineData?.data,
+      _previousTemplate9Id: applicationFormTemplate9DataId,
+      _source: {
+        source: 'rfi',
+        rfiNumber: rfiNumber || null,
+        fileName: templateNineData?.data?.originalFilename,
+        date: DateTime.now().toISO(),
       },
-      onCompleted: () => {
-        if (excelImportFields.length > 0) {
-          createNewFormData({
-            variables: {
-              input: {
-                applicationRowId: Number(applicationId),
-                jsonData: newFormData,
-                reasonForChange,
-                formSchemaId,
-              },
-            },
-            onCompleted: () => {
-              if (templateData?.templateNumber === 1) {
-                notifyHHCountUpdate(
-                  newFormData.benefits,
-                  jsonData.benefits,
-                  applicationId,
-                  {
-                    ccbcNumber,
-                    timestamp: new Date().toLocaleString(),
-                    manualUpdate: false,
-                    rfiNumber,
-                  }
-                );
-              }
-              if (
-                templateData?.templateNumber === 1 ||
-                templateData?.templateNumber === 2
-              ) {
-                showToast(getToastMessage(), 'success', 100000000);
-              }
-              notifyDocumentUpload(applicationId, {
-                ccbcNumber,
-                documentType: joinWithAnd(excelImportFields),
-                documentNames: excelImportFiles,
-              });
-            },
-          });
-        }
-        if (
-          rfiFormData?.rfiAdditionalFiles?.geographicCoverageMap?.length > 0
-        ) {
-          notifyRfiCoverageMapKmzUploaded(
-            rfiDataByRowId,
-            rfiFormData,
-            applicationId,
-            ccbcNumber,
-            rfiNumber
-          );
-        }
-        router.push(`/analyst/application/${router.query.applicationId}/rfi`);
-      },
-      onError: (err) => {
-        // eslint-disable-next-line no-console
-        console.log('Error updating RFI', err);
-      },
-    });
+      _errors: templateNineData?.data?.errors || {},
+    };
+
+    if (!hasApplicationFormDataUpdated && !templatesUpdated.nine) {
+      // only update rfi
+      processUpload(updateRfi, { rfiInput: payloadRfi });
+    } else if (!hasApplicationFormDataUpdated && templatesUpdated.nine) {
+      // form data not updated but template nine updated, update rfi and create template nine record
+      processUpload(updateRfiAndCreateTemplateNineData, {
+        rfiInput: payloadRfi,
+        templateNineInput: payloadTemplateNine,
+      });
+    } else if (hasApplicationFormDataUpdated && !templatesUpdated.nine) {
+      // only update rfi and form data since no template nine data
+      processUpload(updateRfiAndFormData, {
+        formInput: payloadFormData,
+        rfiInput: payloadRfi,
+      });
+    } else if (hasApplicationFormDataUpdated && templatesUpdated.nine) {
+      // update rfi, form data, and template nine data (all three)
+      processUpload(updateFormRfiAndCreateTemplateNineData, {
+        formInput: payloadFormData,
+        rfiInput: payloadRfi,
+        templateNineInput: payloadTemplateNine,
+      });
+    }
   };
 
   return (
