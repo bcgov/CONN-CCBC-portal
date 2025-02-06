9e403ab8efec5cdc0a1f8696656210307df9a8da
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2025-02-06 13:35:32 -0800

    chore: handle sqitch

--- .bin/check-db-changes.sh
+++ .bin/check-db-changes.sh
@@ -5,5 +5,7 @@ if git diff --quiet HEAD^ HEAD -- db; then
   echo "No changes in the db folder. Skipping sqitch tag."
 else
   echo "Changes detected in the db folder. Running sqitch tag."
-  sqitch --chdir db tag "$1" -m "release v$1"
+  docker pull sqitch/sqitch
+  curl -L https://git.io/JJKCn -o sqitch && chmod +x sqitch
+  ./sqitch --chdir db tag "$1" -m "release v$1"
 fi
