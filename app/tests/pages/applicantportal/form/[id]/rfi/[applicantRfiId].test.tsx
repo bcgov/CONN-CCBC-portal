e14adc33e26d34910030007364df0ba2b8dbf1bf
Anthony Bushara <anthony@button.is>
2025-02-06 11:19:01 -0500

    test: update testing for applicantRfiId

--- app/tests/pages/applicantportal/form/[id]/rfi/[applicantRfiId].test.tsx
+++ app/tests/pages/applicantportal/form/[id]/rfi/[applicantRfiId].test.tsx
@@ -12,6 +12,7 @@ const mockQueryPayload = {
         jsonData: {
           rfiDueBy: '2022-12-22',
           rfiAdditionalFiles: {
+            geographicNamesRfi: true,
             detailedBudgetRfi: true,
             eligibilityAndImpactsCalculatorRfi: true,
             geographicCoverageMapRfi: true,
@@ -69,6 +70,7 @@ describe('The applicantRfiId Page', () => {
     pageTestingHelper.reinit();
     pageTestingHelper.setMockRouterValues({
       query: { applicantRfiId: '1', id: '1' },
+      pathname: '/applicantportal/form/1/rfi/1',
     });
   });
 
@@ -101,6 +103,7 @@ describe('The applicantRfiId Page', () => {
           jsonData: {
             rfiType: [],
             rfiAdditionalFiles: {
+              geographicNamesRfi: true,
               eligibilityAndImpactsCalculatorRfi: true,
               detailedBudgetRfi: true,
               geographicCoverageMapRfi: true,
@@ -226,6 +229,7 @@ describe('The applicantRfiId Page', () => {
         jsonData: {
           rfiType: [],
           rfiAdditionalFiles: {
+            geographicNamesRfi: true,
             detailedBudgetRfi: true,
             eligibilityAndImpactsCalculatorRfi: true,
             geographicCoverageMapRfi: true,
@@ -391,6 +395,7 @@ describe('The applicantRfiId Page', () => {
           jsonData: {
             rfiType: [],
             rfiAdditionalFiles: {
+              geographicNamesRfi: true,
               eligibilityAndImpactsCalculatorRfi: true,
               detailedBudgetRfi: true,
               eligibilityAndImpactsCalculator: [
@@ -434,6 +439,328 @@ describe('The applicantRfiId Page', () => {
             rfiDueBy: '2022-12-22',
           },
           rfiRowId: 1,
+        },
+      }
+    );
+  });
+  it('uses template 9 data and  creates new record', async () => {
+    // load page test
+    // mock fetch for template 9
+    // upload fake template 9
+    // click save
+    // expect update rfi and create new record mutation to be called
+
+    pageTestingHelper.loadQuery();
+    pageTestingHelper.renderPage();
+
+    const mockSuccessResponseTemplateNine = {
+      errors: [],
+      projectZone: 'zone',
+      geoName: 'geoName',
+    };
+
+    const mockFetchPromiseTemplateNine = Promise.resolve({
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve({ ...mockSuccessResponseTemplateNine }),
+    });
+
+    global.fetch = jest.fn((url) => {
+      if (url.includes('template-nine')) return mockFetchPromiseTemplateNine;
+      return Promise.resolve({ ok: true, status: 200 });
+    });
+
+    const file = new File([new ArrayBuffer(1)], 'file.xlsx', {
+      type: 'application/vnd.ms-excel',
+    });
+    // grab the third file input since that is the one for template 9
+    const inputFile = screen.getAllByTestId('file-test')[2];
+
+    await act(async () => {
+      fireEvent.change(inputFile, { target: { files: [file] } });
+    });
+
+    await act(async () => {
+      pageTestingHelper.environment.mock.resolveMostRecentOperation({
+        data: {
+          createAttachment: {
+            attachment: {
+              rowId: 3,
+              file: 'UUIDstring',
+            },
+          },
+        },
+      });
+    });
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    expect(global.fetch).toHaveBeenCalledOnce();
+    expect(global.fetch).toHaveBeenCalledWith(
+      '/api/template-nine/rfi/applicant/1/CCBC-01001-01',
+      {
+        body: formData,
+        method: 'POST',
+      }
+    );
+
+    await act(async () => {
+      fireEvent.click(screen.getByRole('button', { name: 'Save' }));
+    });
+
+    pageTestingHelper.expectMutationToBeCalled(
+      'updateRfiAndCreateTemplateNineDataMutation',
+      {
+        rfiInput: {
+          jsonData: {
+            rfiType: [],
+            rfiAdditionalFiles: {
+              geographicNamesRfi: true,
+              detailedBudgetRfi: true,
+              eligibilityAndImpactsCalculatorRfi: true,
+              geographicCoverageMapRfi: true,
+              geographicCoverageMap: [
+                {
+                  uuid: 1,
+                  name: '1.kmz',
+                  size: 0,
+                  type: '',
+                  uploadedAt: '2024-05-31T14:05:03.509-07:00',
+                },
+                {
+                  uuid: 2,
+                  name: '2.kmz',
+                  size: 0,
+                  type: '',
+                  uploadedAt: '2024-05-31T14:05:03.509-07:00',
+                },
+              ],
+              geographicNames: [
+                {
+                  id: 3,
+                  uuid: 'UUIDstring',
+                  name: 'file.xlsx',
+                  size: 1,
+                  type: 'application/vnd.ms-excel',
+                  uploadedAt: expect.anything(),
+                },
+              ],
+            },
+            rfiDueBy: '2022-12-22',
+          },
+          rfiRowId: 1,
+        },
+        templateNineInput: {
+          applicationFormTemplate9Data: {
+            applicationId: 1,
+            jsonData: mockSuccessResponseTemplateNine,
+            source: {
+              source: 'RFI',
+              uuid: 'UUIDstring',
+            },
+          },
+        },
+      }
+    );
+  });
+
+  it('uses template 1, 2, and 9 data to update form, rfi data, and create new record', async () => {
+    // load page test
+    // mock fetch for template 1, 2, and 9
+    // upload fake template 1, 2, and 9
+    // click save
+    // expect update rfi, form, and create new record mutation to be called
+    pageTestingHelper.loadQuery();
+    pageTestingHelper.renderPage();
+    const mockSuccessResponseTemplateOne = {
+      totalNumberHouseholdsImpacted: 60,
+      finalEligibleHouseholds: 4,
+    };
+    const mockFetchPromiseTemplateOne = Promise.resolve({
+      ok: false,
+      status: 200,
+      json: () => Promise.resolve({ result: mockSuccessResponseTemplateOne }),
+    });
+
+    const mockSuccessResponseTemplateTwo = {
+      totalEligibleCosts: 92455,
+      totalProjectCosts: 101230,
+    };
+    const mockFetchPromiseTemplateTwo = Promise.resolve({
+      ok: false,
+      status: 200,
+      json: () => Promise.resolve({ result: mockSuccessResponseTemplateTwo }),
+    });
+
+    const mockSuccessResponseTemplateNine = {
+      errors: [],
+      projectZone: 'zone',
+      geoName: 'geoName',
+    };
+
+    const mockFetchPromiseTemplateNine = Promise.resolve({
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve({ ...mockSuccessResponseTemplateNine }),
+    });
+
+    global.fetch = jest.fn((url) => {
+      if (url.includes('templateNumber=1')) return mockFetchPromiseTemplateOne;
+      if (url.includes('templateNumber=2')) return mockFetchPromiseTemplateTwo;
+      return mockFetchPromiseTemplateNine;
+    });
+
+    const file = new File([new ArrayBuffer(1)], 'file.xlsx', {
+      type: 'application/vnd.ms-excel',
+    });
+
+    const templateOneInputFile = screen.getAllByTestId('file-test')[0];
+    const templateTwoInputFile = screen.getAllByTestId('file-test')[1];
+    const templateNineInputFile = screen.getAllByTestId('file-test')[2];
+    // upload template 1
+    await act(async () => {
+      fireEvent.change(templateOneInputFile, { target: { files: [file] } });
+    });
+    // resolve template 1 upload
+    await act(async () => {
+      pageTestingHelper.environment.mock.resolveMostRecentOperation({
+        data: {
+          createAttachment: {
+            attachment: {
+              rowId: 1,
+              file: 'UUIDTemplateOne',
+            },
+          },
+        },
+      });
+    });
+    // upload template 2
+    await act(async () => {
+      fireEvent.change(templateTwoInputFile, { target: { files: [file] } });
+    });
+    // resolve template 2 upload
+    await act(async () => {
+      pageTestingHelper.environment.mock.resolveMostRecentOperation({
+        data: {
+          createAttachment: {
+            attachment: {
+              rowId: 2,
+              file: 'UUIDTemplateTwo',
+            },
+          },
+        },
+      });
+    });
+    // upload template 9
+    await act(async () => {
+      fireEvent.change(templateNineInputFile, { target: { files: [file] } });
+    });
+    // resolve template 9 upload
+    await act(async () => {
+      pageTestingHelper.environment.mock.resolveMostRecentOperation({
+        data: {
+          createAttachment: {
+            attachment: {
+              rowId: 3,
+              file: 'UUIDTemplateNine',
+            },
+          },
+        },
+      });
+    });
+
+    expect(global.fetch).toHaveBeenCalledTimes(5);
+    expect(global.fetch).toHaveBeenCalledWith(
+      '/api/applicant/template?templateNumber=1',
+      { body: expect.any(FormData), method: 'POST' }
+    );
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      '/api/applicant/template?templateNumber=2',
+      { body: expect.any(FormData), method: 'POST' }
+    );
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      '/api/template-nine/rfi/applicant/1/CCBC-01001-01',
+      { body: expect.any(FormData), method: 'POST' }
+    );
+
+    await act(async () => {
+      fireEvent.click(screen.getByRole('button', { name: 'Save' }));
+    });
+
+    pageTestingHelper.expectMutationToBeCalled(
+      'updateRfiAndCreateTemplateNineDataMutation',
+      {
+        rfiInput: {
+          jsonData: {
+            rfiType: [],
+            rfiAdditionalFiles: {
+              geographicNamesRfi: true,
+              detailedBudgetRfi: true,
+              eligibilityAndImpactsCalculatorRfi: true,
+              geographicCoverageMapRfi: true,
+              geographicCoverageMap: [
+                {
+                  uuid: 1,
+                  name: '1.kmz',
+                  size: 0,
+                  type: '',
+                  uploadedAt: '2024-05-31T14:05:03.509-07:00',
+                },
+                {
+                  uuid: 2,
+                  name: '2.kmz',
+                  size: 0,
+                  type: '',
+                  uploadedAt: '2024-05-31T14:05:03.509-07:00',
+                },
+              ],
+              eligibilityAndImpactsCalculator: [
+                {
+                  id: 1,
+                  uuid: 'UUIDTemplateOne',
+                  name: 'file.xlsx',
+                  size: 1,
+                  type: 'application/vnd.ms-excel',
+                  uploadedAt: expect.anything(),
+                },
+              ],
+              detailedBudget: [
+                {
+                  id: 2,
+                  uuid: 'UUIDTemplateTwo',
+                  name: 'file.xlsx',
+                  size: 1,
+                  type: 'application/vnd.ms-excel',
+                  uploadedAt: expect.anything(),
+                },
+              ],
+              geographicNames: [
+                {
+                  id: 3,
+                  uuid: 'UUIDTemplateNine',
+                  name: 'file.xlsx',
+                  size: 1,
+                  type: 'application/vnd.ms-excel',
+                  uploadedAt: expect.anything(),
+                },
+              ],
+            },
+            rfiDueBy: '2022-12-22',
+          },
+          rfiRowId: 1,
+        },
+        templateNineInput: {
+          applicationFormTemplate9Data: {
+            applicationId: 1,
+            jsonData: mockSuccessResponseTemplateNine,
+            source: {
+              source: 'RFI',
+              uuid: 'UUIDTemplateNine',
+            },
+          },
         },
       }
     );
