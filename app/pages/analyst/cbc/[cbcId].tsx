f19aba523fd7c4b518b3e14b713c79692e48bc85
R Ranathunga <rumesha.ranathunga@gov.bc.ca>
2025-03-11 08:36:34 -0700

    fix: cbc data save issue

--- app/pages/analyst/cbc/[cbcId].tsx
+++ app/pages/analyst/cbc/[cbcId].tsx
@@ -267,6 +267,7 @@ const Cbc = ({
           rowId: query?.cbcByRowId?.cbcDataByCbcId?.edges[0].node.rowId || null,
           cbcDataPatch: {
             jsonData: {
+              ...query?.cbcByRowId?.cbcDataByCbcId?.edges[0].node.jsonData,
               ...formData.tombstone,
               ...formData.projectType,
               projectLocations,
