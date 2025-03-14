a3a715127a0c1b51274b9ab7014d8080ea743965
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2025-03-14 12:01:37 -0700

    feat: handle single line strings in placemarks

--- app/backend/lib/map/utils.ts
+++ app/backend/lib/map/utils.ts
@@ -133,7 +133,6 @@ const parseKML = (
 ): ParsedKML => {
   const kml = new DOMParser().parseFromString(kmlContent, 'text/xml');
   const geoJson = toGeoJSON.kml(kml as any); // Converts to GeoJSON
-
   let hasLineStrings = false;
 
   const polygons: PolygonData[] = [];
@@ -257,6 +256,32 @@ const parseKML = (
       if (multiGeometry) {
         const lineStringElements =
           multiGeometry.getElementsByTagName('LineString');
+        const coordinates = [];
+        for (let j = 0; j < lineStringElements.length; j++) {
+          const lineString = lineStringElements[j];
+          const coordinatesText =
+            lineString.getElementsByTagName('coordinates')[0]?.textContent ||
+            '';
+          const cords = coordinatesText
+            .trim()
+            .split(/\s+/)
+            .map((pair) => {
+              const [lng, lat] = pair.split(',').map(Number);
+              return [lat, lng];
+            });
+          coordinates.push(cords);
+        }
+        lineStrings.push({
+          coordinates,
+          name,
+          description,
+          style: styleData,
+          fileName,
+          source,
+        });
+        // there is no MultiGeometry, just a LineString or multiple
+      } else {
+        const lineStringElements = placemark.getElementsByTagName('LineString');
         const coordinates = [];
         for (let j = 0; j < lineStringElements.length; j++) {
           const lineString = lineStringElements[j];
