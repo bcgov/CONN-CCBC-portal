41d39d7b564c3bb16bf90cfda268ee3233f0aefd
Oleksandr Sokolov <oleksandr.sokolov@gov.bc.ca>
2025-04-29 10:57:14 -0500

    chore: Fixed markup and logic for original project number

--- app/components/AnalystDashboard/AllDashboardDetailPanel.tsx
+++ app/components/AnalystDashboard/AllDashboardDetailPanel.tsx
@@ -49,6 +49,8 @@ const HighlightFilterMatch = ({ text, filterValue }) => {
 
 const AllDashboardDetailPanel: React.FC<Props> = ({ row, filterValue }) => {
   const communities = (row.original.communities as any[]) || [];
+  console.log("AAAAAAA")
+  console.log(row.original)
   return (
     <>
       <StyledSpan>Communities</StyledSpan>
@@ -72,20 +74,7 @@ const AllDashboardDetailPanel: React.FC<Props> = ({ row, filterValue }) => {
         <StyledSpan>N/A</StyledSpan>
       )}
       <StyledSpan>Original Project Number</StyledSpan>
-      {row.original.projectNumber ? (
-        <StyledMapLink
-          href={row.original.projectNumber.mapLink}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <HighlightFilterMatch
-            text={row.original.projectNumber.geoName}
-            filterValue={filterValue}
-          />
-        </StyledMapLink>
-      ) : (
-        <StyledSpan>N/A</StyledSpan>
-      )}
+      <StyledSpan>{row.original.projectNumber || "N/A"}</StyledSpan>
     </>
   );
 };
