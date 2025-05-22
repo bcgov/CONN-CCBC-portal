0eed41d241c89b4d65625092e8d4643a7f107b97
RRanath <rumesha.ranathunga@gov.bc.ca>
2025-05-22 08:25:40 -0700

    feat: add cbc change log to analyst dashboard

--- app/components/AnalystDashboard/AdditionalFilters.tsx
+++ app/components/AnalystDashboard/AdditionalFilters.tsx
@@ -29,9 +29,12 @@ export const additionalFilterColumns = [
   },
 ];
 
-const AdditionalFilters = ({ filters, setFilters }) => {
+const AdditionalFilters = ({ filters, setFilters, disabledFilters = [] }) => {
   const programFilters =
     filters.filter((f) => f.id === 'program')?.[0]?.value ?? [];
+
+  const disabledProgramFilters =
+    disabledFilters.filter((f) => f.id === 'program')?.[0]?.value ?? [];
 
   const handleProgramFilterChange = (program) => {
     const newFilters = programFilters.includes(program)
@@ -50,6 +53,7 @@ const AdditionalFilters = ({ filters, setFilters }) => {
         type="checkbox"
         data-testid="programFilterCcbc"
         checked={programFilters.includes('CCBC')}
+        disabled={disabledProgramFilters.includes('CCBC')}
         onChange={() => handleProgramFilterChange('CCBC')}
       />
       CCBC
@@ -57,6 +61,7 @@ const AdditionalFilters = ({ filters, setFilters }) => {
         type="checkbox"
         data-testid="programFilterCbc"
         checked={programFilters.includes('CBC')}
+        disabled={disabledProgramFilters.includes('CBC')}
         onChange={() => handleProgramFilterChange('CBC')}
       />
       CBC
@@ -64,6 +69,7 @@ const AdditionalFilters = ({ filters, setFilters }) => {
         type="checkbox"
         data-testid="programFilterOther"
         checked={programFilters.includes('OTHER')}
+        disabled={disabledProgramFilters.includes('OTHER')}
         onChange={() => handleProgramFilterChange('OTHER')}
       />
       OTHER
