6282dca9443f41aee672cf10ee7d2c50992370ed
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2025-05-28 15:57:41 -0700

    chore: debug 15 minutes modal display

--- app/components/SessionExpiryHandler.tsx
+++ app/components/SessionExpiryHandler.tsx
@@ -55,7 +55,7 @@ const SessionExpiryHandler: React.FC = () => {
     if (hasSession)
       return (
         <SessionTimeoutHandler
-          modalDisplaySecondsBeforeLogout={120}
+          modalDisplaySecondsBeforeLogout={900}
           onSessionExpired={handleSessionExpired}
           resetOnChange={[]}
           extendSessionOnEvents={{
