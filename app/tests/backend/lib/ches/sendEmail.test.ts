18429dc8c113f6c7294186bf9c87d5924da0990d
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2025-06-26 13:12:39 -0700

    test: email record functionality on send email

--- app/tests/backend/lib/ches/sendEmail.test.ts
+++ app/tests/backend/lib/ches/sendEmail.test.ts
@@ -1,12 +1,14 @@
 import sendEmail from 'backend/lib/ches/sendEmail';
 
+import * as emailRecordModule from 'backend/lib/emails/utils/emailRecord';
+
 jest.mock('backend/lib/emails/utils/emailRecord', () => ({
   __esModule: true,
-  default: jest.fn(),
+  recordEmailRecord: jest.fn(),
 }));
 
-describe('getAccessToken', () => {
-  it('should return a token when the request is successful', async () => {
+describe('send email', () => {
+  it('should return a message id when the request is successful', async () => {
     // Mock the fetch response
     // @ts-ignore
     global.fetch = jest.fn(() =>
@@ -55,5 +57,45 @@ describe('getAccessToken', () => {
         'test_tag'
       )
     ).rejects.toThrow('Error sending email with status: 422');
+  });
+
+  it('should process email record when req and applicationId are provided', async () => {
+    // Mock the fetch response
+    // @ts-ignore
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () =>
+          Promise.resolve({
+            messages: [
+              {
+                msgId: 'mock_message_id',
+                to: ['mock_email@gov.bc.ca'],
+              },
+            ],
+            txId: 'mock_tx_id',
+          }),
+      })
+    );
+    // Mock the email record processing
+    (emailRecordModule.recordEmailRecord as jest.Mock).mockImplementation(() =>
+      Promise.resolve()
+    );
+    const req = { body: { someData: 'test' } };
+    const applicationId = 1;
+    const email = await sendEmail(
+      'test_token',
+      'test_body',
+      'test_subject',
+      'mock_email@gov.bc.ca',
+      'test_tag',
+      [],
+      [],
+      0,
+      req,
+      applicationId
+    );
+    expect(email).toBe('mock_message_id');
   });
 });
