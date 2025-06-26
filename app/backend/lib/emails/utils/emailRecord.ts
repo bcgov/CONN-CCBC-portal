52d4a453c3cb2556d24037db7e7a49faaeccb8a8
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2025-06-26 10:24:43 -0700

    feat: record all single emails

---
+++ app/backend/lib/emails/utils/emailRecord.ts
@@ -0,0 +1,26 @@
+import { performQuery } from '../../graphql';
+
+const emailRecordMutation = `
+  mutation emailRecordMutation($input: CreateEmailRecordInput!) {
+    createEmailRecord(input: $input) {
+      emailRecord {
+        id
+      }
+      clientMutationId
+    }
+  }
+`;
+
+const recordEmailRecord = async (input, req) => {
+  try {
+    const result = await performQuery(emailRecordMutation, { input }, req);
+    console.log('Email record result:', result);
+    return result;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error recording email record:', error);
+    throw error;
+  }
+};
+
+export default recordEmailRecord;
