6b553af15504ae05394821c23d65aea585cdde41
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2025-07-15 15:28:23 -0700

    fix: sow replacements not importing data

--- db/deploy/mutations/create_application_sow_data.sql
+++ db/deploy/mutations/create_application_sow_data.sql
@@ -33,24 +33,24 @@ begin
 
   -- archive the old sow data
   if exists (select * from ccbc_public.application_sow_data where id = old_application_sow_data_id)
-    then update ccbc_public.application_sow_data set archived_at = now(), history_operation = _history_operation where id = old_application_sow_data_id;
+    then update ccbc_public.application_sow_data set archived_at = now(), history_operation = _history_operation where id = old_application_sow_data_id and archived_at is null;
   end if;
 
  -- archive all the sow tabs related to the old sow id
   if exists (select * from ccbc_public.sow_tab_1 where sow_id = old_application_sow_data_id)
-    then update ccbc_public.sow_tab_1 set archived_at = now();
+    then update ccbc_public.sow_tab_1 set archived_at = now() where sow_id = old_application_sow_data_id and archived_at is null;
   end if;
 
   if exists (select * from ccbc_public.sow_tab_2 where sow_id = old_application_sow_data_id)
-    then update ccbc_public.sow_tab_2 set archived_at = now();
+    then update ccbc_public.sow_tab_2 set archived_at = now() where sow_id = old_application_sow_data_id and archived_at is null;
   end if;
 
   if exists (select * from ccbc_public.sow_tab_7 where sow_id = old_application_sow_data_id)
-    then update ccbc_public.sow_tab_7 set archived_at = now();
+    then update ccbc_public.sow_tab_7 set archived_at = now() where sow_id = old_application_sow_data_id and archived_at is null;
   end if;
 
   if exists (select * from ccbc_public.sow_tab_8 where sow_id = old_application_sow_data_id)
-    then update ccbc_public.sow_tab_8 set archived_at = now();
+    then update ccbc_public.sow_tab_8 set archived_at = now() where sow_id = old_application_sow_data_id and archived_at is null;
   end if;
 
   return (select row(ccbc_public.application_sow_data.*) from ccbc_public.application_sow_data where id = new_application_sow_data_id);
--- db/revert/mutations/create_application_sow_data@1.259.0.sql
+++ db/revert/mutations/create_application_sow_data@1.259.0.sql
@@ -4,7 +4,7 @@ begin;
 
 drop function if exists ccbc_public.create_application_sow_data;
 
-create or replace function ccbc_public.create_application_sow_data(_application_id int, _json_data jsonb, _amendment_number int, _history_operation text)
+create or replace function ccbc_public.create_application_sow_data(_application_id int, _json_data jsonb, _amendment_number int)
 returns ccbc_public.application_sow_data as $$
 declare
 new_application_sow_data_id int;
@@ -28,29 +28,29 @@ begin
   and archived_at is null
   order by asd.id desc limit 1;
 
-  insert into ccbc_public.application_sow_data (application_id, json_data, amendment_number, is_amendment, history_operation)
-    values (_application_id, _json_data, sow_amendment_number, sow_is_amendment, _history_operation) returning id into new_application_sow_data_id;
+  insert into ccbc_public.application_sow_data (application_id, json_data, amendment_number, is_amendment)
+    values (_application_id, _json_data, sow_amendment_number, sow_is_amendment) returning id into new_application_sow_data_id;
 
   -- archive the old sow data
   if exists (select * from ccbc_public.application_sow_data where id = old_application_sow_data_id)
-    then update ccbc_public.application_sow_data set archived_at = now(), history_operation = _history_operation where id = old_application_sow_data_id;
+    then update ccbc_public.application_sow_data set archived_at = now() where id = old_application_sow_data_id;
   end if;
 
  -- archive all the sow tabs related to the old sow id
   if exists (select * from ccbc_public.sow_tab_1 where sow_id = old_application_sow_data_id)
-    then update ccbc_public.sow_tab_1 set archived_at = now();
+    then update ccbc_public.sow_tab_1 set archived_at = now() where sow_id = old_application_sow_data_id;
   end if;
 
   if exists (select * from ccbc_public.sow_tab_2 where sow_id = old_application_sow_data_id)
-    then update ccbc_public.sow_tab_2 set archived_at = now();
+    then update ccbc_public.sow_tab_2 set archived_at = now() where sow_id = old_application_sow_data_id;
   end if;
 
   if exists (select * from ccbc_public.sow_tab_7 where sow_id = old_application_sow_data_id)
-    then update ccbc_public.sow_tab_7 set archived_at = now();
+    then update ccbc_public.sow_tab_7 set archived_at = now() where sow_id = old_application_sow_data_id;
   end if;
 
   if exists (select * from ccbc_public.sow_tab_8 where sow_id = old_application_sow_data_id)
-    then update ccbc_public.sow_tab_8 set archived_at = now();
+    then update ccbc_public.sow_tab_8 set archived_at = now() where sow_id = old_application_sow_data_id;
   end if;
 
   return (select row(ccbc_public.application_sow_data.*) from ccbc_public.application_sow_data where id = new_application_sow_data_id);
