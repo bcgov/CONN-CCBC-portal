4e81dc97c1281dc93b2c658beeedc22dc3c72134
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2025-11-19 15:42:05 -0800

    chore: e2e testing match form id

--- db/data/e2e/001_history.sql
+++ db/data/e2e/001_history.sql
@@ -44,17 +44,19 @@ insert into ccbc_public.ccbc_user
     $${"id": 1, "json_data": {"dateApproved": "2023-08-01", "dateRequested": "2023-08-01", "amendmentNumber": 1, "levelOfAmendment": "Major Amendment", "descriptionOfChanges": "test changes", "statementOfWorkUpload": [{"id": 1, "name": "CCBC-010001-sow_excel.xlsx", "size": 4232056, "type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "uuid": "648c1c7c-c498-4c58-9da3-bec51eb76a62"}]}, "created_at": "2023-08-28T22:15:57.950376+00:00", "created_by": null, "updated_at": "2023-08-28T22:15:57.950376+00:00", "updated_by": null, "archived_at": null, "archived_by": null, "application_id": 1, "amendment_number": 1}$$),
     -- Edit application
     ('09990a12-547b-5fd4-a59b-e31ff2dd9d5e', 'INSERT', 123, 'ccbc_public', 'form_data', 2, now(),
-    $${"id": 2, "json_data": {"submission": {"submissionDate": "2022-09-15", "submissionTitle": "Test title", "submissionCompletedBy": "Mr Test", "submissionCompletedFor": "Testing Incorporated"}, "projectInformation": {"projectTitle": "Test title"}, "organizationProfile": {"organizationName": "org name received"}}, "created_at": "2023-08-29T18:12:54.02826+00:00", "created_by": null, "updated_at": "2023-08-29T18:12:54.02826+00:00", "updated_by": null, "archived_at": null, "archived_by": null, "form_schema_id": 1, "last_edited_page": null, "reason_for_change": "e2e testing", "form_data_status_type_id": "pending", "application_id": 1}$$);
+    $${"id": 1, "json_data": {"submission": {"submissionDate": "2022-09-15", "submissionTitle": "Test title", "submissionCompletedBy": "Mr Test", "submissionCompletedFor": "Testing Incorporated"}, "projectInformation": {"projectTitle": "Test title"}, "organizationProfile": {"organizationName": "org name received"}}, "created_at": "2023-08-29T18:12:54.02826+00:00", "created_by": null, "updated_at": "2023-08-29T18:12:54.02826+00:00", "updated_by": null, "archived_at": null, "archived_by": null, "form_schema_id": 1, "last_edited_page": null, "reason_for_change": "e2e testing", "form_data_status_type_id": "pending", "application_id": 1}$$),
+    ('09990a12-547b-5fd5-a59b-e31ff2dd9d5e', 'INSERT', 20043, 'ccbc_public', 'form_data', 2, now(),
+    $${"id": 1, "json_data": {}, "created_at": "2023-08-29T18:12:54.02826+00:00", "created_by": 2, "updated_at": "2023-08-29T18:12:54.02826+00:00", "updated_by": 2, "archived_at": null, "archived_by": null, "form_schema_id": 1, "last_edited_page": null, "reason_for_change": "e2e testing", "form_data_status_type_id": "pending", "application_id": 1}$$);
 
 
   -- Form data form edit application
-  insert into ccbc_public.form_data
-  (id, json_data, last_edited_page, form_data_status_type_id, created_by, created_at, updated_by, updated_at, form_schema_id)
-  overriding system value
-  values (2, '{}', 'projectInformation', 'pending', 1, '2022-10-17 16:28:11.006719-07', 1, '2022-10-17 16:28:26.105206-07', 1);
+  -- insert into ccbc_public.form_data
+  -- (id, json_data, last_edited_page, form_data_status_type_id, created_by, created_at, updated_by, updated_at, form_schema_id)
+  -- overriding system value
+  -- values (2, '{}', 'projectInformation', 'pending', 2, '2022-10-17 16:28:11.006719-07', 2, '2022-10-17 16:28:26.105206-07', 1);
 
-  insert into ccbc_public.application_form_data
-  (form_data_id, application_id)
-  values (2,1);
+  -- insert into ccbc_public.application_form_data
+  -- (form_data_id, application_id)
+  -- values (2,1);
 
 commit;
