e52e28a3b97ec3ae89881c310e78f3019c1f240c
RRanath <rumesha.ranathunga@gov.bc.ca>
2025-12-01 12:13:29 -0800

    chore: format money figures in funding summary and accordion

--- app/formSchema/uiSchema/summary/reviewUiSchema.ts
+++ app/formSchema/uiSchema/summary/reviewUiSchema.ts
@@ -14,34 +14,73 @@ const reviewUiSchema = {
     bcFundingRequested: {
       ...fundingUiSchema.bcFundingRequested,
       'ui:widget': 'ReadOnlyMoneyWidget',
+      'ui:options': {
+        ...fundingUiSchema.bcFundingRequested?.['ui:options'],
+        decimals: 0,
+      },
+    },
+    fnhaContribution: {
+      ...fundingUiSchema.fnhaContribution,
+      'ui:options': {
+        ...fundingUiSchema.fnhaContribution?.['ui:options'],
+        decimals: 0,
+      },
     },
     federalFunding: {
       ...fundingUiSchema.federalFunding,
       'ui:widget': 'ReadOnlyMoneyWidget',
+      'ui:options': {
+        ...fundingUiSchema.federalFunding?.['ui:options'],
+        decimals: 0,
+      },
     },
     fundingRequestedCcbc: {
       ...fundingUiSchema.fundingRequestedCcbc,
       'ui:widget': 'ReadOnlyMoneyWidget',
+      'ui:options': {
+        ...fundingUiSchema.fundingRequestedCcbc?.['ui:options'],
+        decimals: 0,
+      },
     },
     applicantAmount: {
       ...fundingUiSchema.applicantAmount,
       'ui:widget': 'ReadOnlyMoneyWidget',
+      'ui:options': {
+        ...fundingUiSchema.applicantAmount?.['ui:options'],
+        decimals: 0,
+      },
     },
     otherFunding: {
       ...fundingUiSchema.otherFunding,
       'ui:widget': 'ReadOnlyMoneyWidget',
+      'ui:options': {
+        ...fundingUiSchema.otherFunding?.['ui:options'],
+        decimals: 0,
+      },
     },
     cibFunding: {
       ...fundingUiSchema.cibFunding,
       'ui:widget': 'ReadOnlyMoneyWidget',
+      'ui:options': {
+        ...fundingUiSchema.cibFunding?.['ui:options'],
+        decimals: 0,
+      },
     },
     fnhaFunding: {
       ...fundingUiSchema.fnhaFunding,
       'ui:widget': 'ReadOnlyMoneyWidget',
+      'ui:options': {
+        ...fundingUiSchema.fnhaFunding?.['ui:options'],
+        decimals: 0,
+      },
     },
     totalProjectBudget: {
       ...fundingUiSchema.totalProjectBudget,
       'ui:widget': 'ReadOnlyMoneyWidget',
+      'ui:options': {
+        ...fundingUiSchema.totalProjectBudget?.['ui:options'],
+        decimals: 0,
+      },
     },
     'ui:options': {
       allowAnalystEdit: true,
