b59d45f43969eb304235e78dc4fbc50768fd9f48
RRanath <rumesha.ranathunga@gov.bc.ca>
2025-12-01 12:13:29 -0800

    chore: format money figures in funding summary and accordion

--- app/components/Review/widgets/MoneyWidget.tsx
+++ app/components/Review/widgets/MoneyWidget.tsx
@@ -2,9 +2,14 @@ import { WidgetProps } from '@rjsf/utils';
 import formatMoney from 'utils/formatMoney';
 import { StyledSourceSpan } from './DefaultWidget';
 
-const MoneyWidget: React.FC<WidgetProps> = ({ value, formContext, name }) => (
+const MoneyWidget: React.FC<WidgetProps> = ({
+  value,
+  formContext,
+  name,
+  options,
+}) => (
   <>
-    {formatMoney(value)}
+    {formatMoney(value, options?.decimals as number)}
     {formContext?.formDataSource?.[name] &&
       value !== null &&
       typeof value !== 'undefined' &&
