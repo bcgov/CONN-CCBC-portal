95670f0acc0262ae3c339b5afbbbbb9f96fabbb6
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2025-12-02 08:49:31 -0800

    chore: internal notes history schema

---
+++ app/formSchema/uiSchema/history/internalNotes.ts
@@ -0,0 +1,12 @@
+const internalNotes = {
+  applicationInternalNotes: {
+    properties: {
+      note: {
+        title: 'Internal Notes',
+        type: 'string',
+      },
+    },
+  },
+};
+
+export default internalNotes;
