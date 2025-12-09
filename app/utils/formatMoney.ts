a0256d090fdf50990f07aeebaf74368efa441d97
RRanath <rumesha.ranathunga@gov.bc.ca>
2025-12-09 09:53:08 -0800

    chore: replace number format function

--- app/utils/formatMoney.ts
+++ app/utils/formatMoney.ts
@@ -1,13 +1,15 @@
 const formatMoney = (number: number, decimals?: number) => {
   if (!number) return;
+
   let value = Number(number);
+
   if (decimals !== undefined) {
     value = Number(value.toFixed(decimals));
   }
 
   const [intPart, decimalPart] = value.toString().split('.');
 
-  const formattedInt = intPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  const formattedInt = Number(intPart).toLocaleString('en-US');
 
   // eslint-disable-next-line consistent-return
   return decimalPart !== undefined
