be0b2c20f48a2c13954ba197774b19fcf889623e
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2025-12-30 13:22:04 -0800

    test: coverage

--- app/tests/backend/lib/reporting/gcpe.test.ts
+++ app/tests/backend/lib/reporting/gcpe.test.ts
@@ -82,4 +82,136 @@ describe('Gcpe reporting functions', () => {
     const blob = await compareGcpeReports(1, 2, null);
     expect(blob).toBeDefined();
   });
+
+  it('should return BC for CCBC with Agreement Signed status and BC funding only', async () => {
+    const modifiedCcbcDataQueryResult = {
+      ...ccbcDataQueryResult,
+      data: {
+        ...ccbcDataQueryResult.data,
+        allApplications: {
+          ...ccbcDataQueryResult.data.allApplications,
+          edges: ccbcDataQueryResult.data.allApplications.edges.map((edge) => ({
+            ...edge,
+            node: {
+              ...edge.node,
+              analystStatus: 'approved',
+              applicationSowDataByApplicationId: {
+                nodes: [
+                  {
+                    sowTab7SBySowId: {
+                      nodes: [
+                        {
+                          jsonData: {
+                            summaryTable: {
+                              amountRequestedFromProvince: 100000,
+                              amountRequestedFromFederalGovernment: 0,
+                            },
+                          },
+                        },
+                      ],
+                      totalCount: 1,
+                    },
+                  },
+                ],
+                totalCount: 1,
+              },
+            },
+          })),
+        },
+      },
+    };
+    mocked(performQuery).mockImplementation(async () => {
+      return modifiedCcbcDataQueryResult;
+    });
+    const result = await generateGcpeReport(null);
+    expect(result.blob).toBeDefined();
+  });
+
+  it('should return ISED for CCBC with Agreement Signed status and ISED funding only', async () => {
+    const modifiedCcbcDataQueryResult = {
+      ...ccbcDataQueryResult,
+      data: {
+        ...ccbcDataQueryResult.data,
+        allApplications: {
+          ...ccbcDataQueryResult.data.allApplications,
+          edges: ccbcDataQueryResult.data.allApplications.edges.map((edge) => ({
+            ...edge,
+            node: {
+              ...edge.node,
+              analystStatus: 'approved',
+              applicationSowDataByApplicationId: {
+                nodes: [
+                  {
+                    sowTab7SBySowId: {
+                      nodes: [
+                        {
+                          jsonData: {
+                            summaryTable: {
+                              amountRequestedFromProvince: 0,
+                              amountRequestedFromFederalGovernment: 150000,
+                            },
+                          },
+                        },
+                      ],
+                      totalCount: 1,
+                    },
+                  },
+                ],
+                totalCount: 1,
+              },
+            },
+          })),
+        },
+      },
+    };
+    mocked(performQuery).mockImplementation(async () => {
+      return modifiedCcbcDataQueryResult;
+    });
+    const result = await generateGcpeReport(null);
+    expect(result.blob).toBeDefined();
+  });
+
+  it('should return BC & ISED for CCBC with Agreement Signed status and both BC and ISED funding', async () => {
+    const modifiedCcbcDataQueryResult = {
+      ...ccbcDataQueryResult,
+      data: {
+        ...ccbcDataQueryResult.data,
+        allApplications: {
+          ...ccbcDataQueryResult.data.allApplications,
+          edges: ccbcDataQueryResult.data.allApplications.edges.map((edge) => ({
+            ...edge,
+            node: {
+              ...edge.node,
+              analystStatus: 'approved',
+              applicationSowDataByApplicationId: {
+                nodes: [
+                  {
+                    sowTab7SBySowId: {
+                      nodes: [
+                        {
+                          jsonData: {
+                            summaryTable: {
+                              amountRequestedFromProvince: 100000,
+                              amountRequestedFromFederalGovernment: 150000,
+                            },
+                          },
+                        },
+                      ],
+                      totalCount: 1,
+                    },
+                  },
+                ],
+                totalCount: 1,
+              },
+            },
+          })),
+        },
+      },
+    };
+    mocked(performQuery).mockImplementation(async () => {
+      return modifiedCcbcDataQueryResult;
+    });
+    const result = await generateGcpeReport(null);
+    expect(result.blob).toBeDefined();
+  });
 });
