e2c3ca6048e79b2fc7704fcb2ed5fe5d8c1a9e06
RRanath <rumesha.ranathunga@gov.bc.ca>
2026-01-13 08:10:30 -0800

    chore: add announcement details to history

--- app/components/Analyst/History/HistoryRow.tsx
+++ app/components/Analyst/History/HistoryRow.tsx
@@ -30,6 +30,7 @@ const HistoryRow = ({
   originalProjectTitle,
   recordWithOrgChange,
   recordWithTitleChange,
+  announcements,
 }) => {
   const { tableName, user } = historyItem;
 
@@ -59,6 +60,7 @@ const HistoryRow = ({
             originalProjectTitle={originalProjectTitle}
             recordWithOrgChange={recordWithOrgChange}
             recordWithTitleChange={recordWithTitleChange}
+            announcements={announcements}
           />
         </StyledCell>
       </tr>
