ba6061c079c0f195fd65f604a15dbb8d9721d940
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2026-02-02 13:13:39 -0800

    feat: all dashboard extra icons screen readable

--- app/components/AnalystDashboard/DownloadIcon.tsx
+++ app/components/AnalystDashboard/DownloadIcon.tsx
@@ -1,3 +1,4 @@
+import { CSSProperties } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileDownload } from '@fortawesome/free-solid-svg-icons';
 import styled from 'styled-components';
@@ -7,36 +8,58 @@ const StyledFontAwesome = styled(FontAwesomeIcon)`
   margin-left: 4px;
 `;
 
-const DownloadIcon = ({ handleClick, isLoading }) => {
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter' || event.key === ' ') {
-      handleClick();
-    }
-  };
+const StyledButton = styled.button`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background: transparent;
+  border: none;
+  padding: 0;
+  cursor: pointer;
 
+  &[aria-disabled='true'] {
+    cursor: default;
+  }
+`;
+
+const visuallyHidden: CSSProperties = {
+  border: 0,
+  clip: 'rect(0 0 0 0)',
+  height: '1px',
+  margin: '-1px',
+  overflow: 'hidden',
+  padding: 0,
+  position: 'absolute',
+  width: '1px',
+  whiteSpace: 'nowrap',
+};
+
+const DownloadIcon = ({ handleClick, isLoading }) => {
   return (
-    <>
+    <StyledButton
+      type="button"
+      onClick={isLoading ? undefined : handleClick}
+      aria-label="Download XLS file"
+      aria-busy={isLoading}
+      aria-disabled={isLoading}
+      data-testid="download-dashboard-icon"
+    >
       {isLoading ? (
         <LoadingSpinner color="#345FA9" />
       ) : (
-        <div
-          role="button"
-          tabIndex={0}
-          onClick={!isLoading ? handleClick : null}
-          onKeyDown={!isLoading ? handleKeyDown : null}
-          aria-labelledby="Download Excel of current projects"
-          style={{ cursor: 'pointer' }}
-          data-testid="download-dashboard-icon"
-        >
-          <StyledFontAwesome
-            icon={faFileDownload}
-            fixedWidth
-            size="lg"
-            color="#345FA9"
-          />
-        </div>
+        <StyledFontAwesome
+          icon={faFileDownload}
+          fixedWidth
+          size="lg"
+          color="#345FA9"
+        />
       )}
-    </>
+      {isLoading ? (
+        <span role="status" aria-live="polite" style={visuallyHidden}>
+          Downloading XLS file
+        </span>
+      ) : null}
+    </StyledButton>
   );
 };
 
