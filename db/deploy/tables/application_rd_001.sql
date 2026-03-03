24219ee13c42b6adaf217fe0eb01853104c264a5
Rafael Solorzano <61289255+rafasdc@users.noreply.github.com>
2026-03-03 13:39:46 -0800

    chore: use if not exists

--- db/deploy/tables/application_rd_001.sql
+++ db/deploy/tables/application_rd_001.sql
@@ -2,10 +2,25 @@
 
 begin;
 
+do $$
+begin
+  if not exists (
+    select 1 from information_schema.columns
+    where table_schema = 'ccbc_public' and table_name = 'application_rd' and column_name = 'id'
+  ) then
+    alter table ccbc_public.application_rd add column id serial primary key;
+  end if;
+
+  if not exists (
+    select 1 from information_schema.columns
+    where table_schema = 'ccbc_public' and table_name = 'application_rd' and column_name = 'er'
+  ) then
+    alter table ccbc_public.application_rd add column er text;
+  end if;
+end $$;
+
 alter table ccbc_public.application_rd
-  add column id serial primary key,
   alter column ccbc_number type text,
-  alter column rd type text,
-  add column er text;
+  alter column rd type text;
 
 commit;
